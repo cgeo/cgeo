e9ff8452457816da5bf98cb6f5c6ef8a8d9c6d0a
bananeweizen <bananeweizen@gmx.de>
2011-08-14 15:51:18 +0200

    make date comparator no longer differentiate between event and non event caches. this can be done with filters anyway.
    fixes #148

--- src/cgeo/geocaching/sorting/DateComparator.java
+++ src/cgeo/geocaching/sorting/DateComparator.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import cgeo.geocaching.cgCache;
 
 /**
- * compares event caches by date
+ * compares caches by date
  * @author bananeweizen
  *
  */
@@ -18,24 +18,17 @@ public class DateComparator extends AbstractCacheComparator {
 
 	@Override
 	protected int compareCaches(cgCache cache1, cgCache cache2) {
-		Date event1 = null;
-		Date event2 = null;
-		if (cache1.isEventCache()) {
-			event1 = cache1.hidden;
+		Date date1 = cache1.hidden;
+		Date date2 = cache2.hidden;
+		if (date1 != null && date2 != null) {
+			return date1.compareTo(date2);
 		}
-		if (cache2.isEventCache()) {
-			event2 = cache2.hidden;
-		}
-		if (event1 != null && event2 != null) {
-			return event1.compareTo(event2);
-		}
-		if (event1 != null) {
+		if (date1 != null) {
 			return -1;
 		}
-		if (event2 != null) {
+		if (date2 != null) {
 			return 1;
 		}
 		return 0;
 	}
-
 }
