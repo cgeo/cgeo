8ac2fcc921ef5037c354eeefb3c87198faeca528
rsudev <rasch@munin-soft.de>
2011-08-22 14:02:12 +0800

    Fixing #198, map switch changes zoom Implemented proper limits check for mapsforge

--- src/cgeo/geocaching/mapsforge/mfMapView.java
+++ src/cgeo/geocaching/mapsforge/mfMapView.java
@@ -47,7 +47,7 @@ public class mfMapView extends MapView implements MapViewImpl {
 
 	@Override
 	public MapControllerImpl getMapController() {
-		return new mfMapController(getController());
+		return new mfMapController(getController(), getMaxZoomLevel());
 	}
 
 	@Override
