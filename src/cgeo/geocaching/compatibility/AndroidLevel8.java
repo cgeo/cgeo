0b4865651abf0c56f538c34cf82cd3b0a1ddd293
blafoo <github@blafoo.de>
2011-09-04 10:36:22 +0200

    Changed accessing static method

--- src/cgeo/geocaching/compatibility/AndroidLevel8.java
+++ src/cgeo/geocaching/compatibility/AndroidLevel8.java
@@ -22,6 +22,6 @@ public class AndroidLevel8 {
 	}
 
 	public int getRotation(Activity activity) {
-		return internal.getRotation(activity);
+		return AndroidLevel8Internal.getRotation(activity);
 	}
 }
