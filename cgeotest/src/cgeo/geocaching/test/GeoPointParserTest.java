853d0fecf124a3278f4ae41f8e905e7b87b782b9
Samuel Tardieu <sam@rfc1149.net>
2011-09-16 14:22:06 +0200

    Add more tests for GeopointParser

--- cgeotest/src/cgeo/geocaching/test/GeoPointParserTest.java
+++ cgeotest/src/cgeo/geocaching/test/GeoPointParserTest.java
@@ -1,18 +1,39 @@
 package cgeo.geocaching.test;
 
+import cgeo.geocaching.geopoint.Geopoint;
 import cgeo.geocaching.geopoint.GeopointParser;
 import junit.framework.Assert;
 import android.test.AndroidTestCase;
 
 public class GeoPointParserTest extends AndroidTestCase {
 
-    public void testParseLatitude() throws Throwable {
-    	
-    	Assert.assertEquals(49.0 + 56.031 / 60.0, GeopointParser.parseLatitude("N 49° 56.031"));
-    }
+	private static final double refLongitude = 8.0 + 38.564 / 60.0;
+	private static final double refLatitude = 49.0 + 56.031 / 60.0;
 
-    public void testParseLongitude() throws Throwable {
-    	
-    	Assert.assertEquals(8.0 + 38.564 / 60.0, GeopointParser.parseLongitude("E 8° 38.564"));
-    }
+	public void testParseLatitude() throws Throwable {
+
+		Assert.assertEquals(refLatitude, GeopointParser.parseLatitude("N 49° 56.031"), 1e-8);
+	}
+
+	public void testParseLongitude() throws Throwable {
+
+		Assert.assertEquals(refLongitude, GeopointParser.parseLongitude("E 8° 38.564"), 1e-8);
+	}
+
+	public void testFullCoordinates() throws Throwable {
+		final Geopoint goal = new Geopoint(refLatitude, refLongitude);
+		Assert.assertTrue(goal.isEqualTo(GeopointParser.parse("N 49° 56.031 | E 8° 38.564"), 1e-6));
+	}
+
+	public void testSouth() throws Throwable {
+		Assert.assertEquals(-refLatitude, GeopointParser.parseLatitude("S 49° 56.031"), 1e-8);
+	}
+
+	public void testWest() throws Throwable {
+		Assert.assertEquals(-refLongitude, GeopointParser.parseLongitude("W 8° 38.564"), 1e-8);
+	}
+
+	public void testLowerCase() throws Throwable {
+		Assert.assertEquals(refLongitude, GeopointParser.parseLongitude("e 8° 38.564"), 1e-8);
+	}
 }
