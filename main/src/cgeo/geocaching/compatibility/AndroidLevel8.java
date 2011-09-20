6b2f63e0efb3c5a2f09af12455fa86ab347b63f3
Samuel Tardieu <sam@rfc1149.net>
2011-09-20 09:54:43 +0200

    Do not use an intermediate AndroidLevel8Internal class
    It looks like going through one level of indirection is
    enough, as the verifier won't work recursively.
    
    Tested on Android 1.5 emulator and on SDK API >= 8.

--- main/src/cgeo/geocaching/compatibility/AndroidLevel8.java
+++ main/src/cgeo/geocaching/compatibility/AndroidLevel8.java
@@ -1,24 +1,14 @@
 package cgeo.geocaching.compatibility;
 
 import android.app.Activity;
+import android.view.Display;
 
 public class AndroidLevel8 {
-    static {
-        try {
-            Class.forName("cgeo.geocaching.compatibility.AndroidLevel8Internal");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void check() {
-        // nothing
-    }
-
-    public AndroidLevel8() {
-    }
 
     public int getRotation(Activity activity) {
-        return AndroidLevel8Internal.getRotation(activity);
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        return display.getRotation();
+    }
+
     }
 }
