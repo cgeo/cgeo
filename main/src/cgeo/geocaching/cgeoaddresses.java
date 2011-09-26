9e4aaa2c387e3f7c2771f3fc03aa8be0952b6e36
Samuel Tardieu <sam@rfc1149.net>
2011-09-26 23:28:43 +0200

    Use an AsyncTask instead of hand-coded Handler for address lookups

--- main/src/cgeo/geocaching/cgeoaddresses.java
+++ main/src/cgeo/geocaching/cgeoaddresses.java
@@ -6,86 +6,22 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
 public class cgeoaddresses extends AbstractActivity {
-    private final List<Address> addresses = new ArrayList<Address>();
     private String keyword = null;
     private LayoutInflater inflater = null;
     private LinearLayout addList = null;
     private ProgressDialog waitDialog = null;
-    private Handler loadPlacesHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            try {
-                if (addList == null) {
-                    addList = (LinearLayout) findViewById(R.id.address_list);
-                }
-
-                if (addresses.isEmpty()) {
-                    if (waitDialog != null) {
-                        waitDialog.dismiss();
-                    }
-
-                    showToast(res.getString(R.string.err_search_address_no_match));
-
-                    finish();
-                    return;
-                } else {
-                    LinearLayout oneAddPre = null;
-                    for (Address address : addresses) {
-                        oneAddPre = (LinearLayout) inflater.inflate(R.layout.address_button, null);
-
-                        Button oneAdd = (Button) oneAddPre.findViewById(R.id.button);
-                        int index = 0;
-                        StringBuilder allAdd = new StringBuilder();
-                        StringBuilder allAddLine = new StringBuilder();
-
-                        while (address.getAddressLine(index) != null) {
-                            if (allAdd.length() > 0) {
-                                allAdd.append('\n');
-                            }
-                            if (allAddLine.length() > 0) {
-                                allAddLine.append("; ");
-                            }
-
-                            allAdd.append(address.getAddressLine(index));
-                            allAddLine.append(address.getAddressLine(index));
-
-                            index++;
-                        }
-
-                        oneAdd.setText(allAdd.toString());
-                        oneAdd.setLines(allAdd.toString().split("\n").length);
-                        oneAdd.setClickable(true);
-                        oneAdd.setOnClickListener(new buttonListener(address.getLatitude(), address.getLongitude(), allAddLine.toString()));
-                        addList.addView(oneAddPre);
-                    }
-                }
-
-                if (waitDialog != null) {
-                    waitDialog.dismiss();
-                }
-            } catch (Exception e) {
-                if (waitDialog != null) {
-                    waitDialog.dismiss();
-                }
-                Log.e(cgSettings.tag, "cgeoaddresses.loadCachesHandler: " + e.toString());
-            }
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -115,7 +51,68 @@ public class cgeoaddresses extends AbstractActivity {
         waitDialog = ProgressDialog.show(this, res.getString(R.string.search_address_started), keyword, true);
         waitDialog.setCancelable(true);
 
-        (new loadPlaces()).start();
+        new AsyncTask<Void, Void, List<Address>>() {
+
+            @Override
+            protected List<Address> doInBackground(Void... params) {
+                final Geocoder geocoder = new Geocoder(cgeoaddresses.this, Locale.getDefault());
+                try {
+                    return geocoder.getFromLocationName(keyword, 20);
+                } catch (Exception e) {
+                    Log.e(cgSettings.tag, "cgeoaddresses.doInBackground", e);
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(final List<Address> addresses) {
+                waitDialog.dismiss();
+                try {
+                    if (addList == null) {
+                        addList = (LinearLayout) findViewById(R.id.address_list);
+                    }
+
+                    if (addresses == null || addresses.isEmpty()) {
+                        showToast(res.getString(R.string.err_search_address_no_match));
+                        finish();
+                        return;
+                    } else {
+                        LinearLayout oneAddPre = null;
+                        for (Address address : addresses) {
+                            oneAddPre = (LinearLayout) inflater.inflate(R.layout.address_button, null);
+
+                            Button oneAdd = (Button) oneAddPre.findViewById(R.id.button);
+                            int index = 0;
+                            StringBuilder allAdd = new StringBuilder();
+                            StringBuilder allAddLine = new StringBuilder();
+
+                            while (address.getAddressLine(index) != null) {
+                                if (allAdd.length() > 0) {
+                                    allAdd.append('\n');
+                                }
+                                if (allAddLine.length() > 0) {
+                                    allAddLine.append("; ");
+                                }
+
+                                allAdd.append(address.getAddressLine(index));
+                                allAddLine.append(address.getAddressLine(index));
+
+                                index++;
+                            }
+
+                            oneAdd.setText(allAdd.toString());
+                            oneAdd.setLines(allAdd.toString().split("\n").length);
+                            oneAdd.setClickable(true);
+                            oneAdd.setOnClickListener(new buttonListener(address.getLatitude(), address.getLongitude(), allAddLine.toString()));
+                            addList.addView(oneAddPre);
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e(cgSettings.tag, "cgeoaddresses.onPostExecute", e);
+                }
+            }
+
+        }.execute();
     }
 
     @Override
@@ -123,29 +120,6 @@ public class cgeoaddresses extends AbstractActivity {
         super.onResume();
 
         settings.load();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-
-    private class loadPlaces extends Thread {
-
-        @Override
-        public void run() {
-            Geocoder geocoder = new Geocoder(cgeoaddresses.this, Locale.getDefault());
-            try {
-                List<Address> knownLocations = geocoder.getFromLocationName(keyword, 20);
-
-                addresses.clear();
-                addresses.addAll(knownLocations);
-            } catch (Exception e) {
-                Log.e(cgSettings.tag, "cgeoaddresses.loadPlaces.run", e);
-            }
-
-            loadPlacesHandler.sendMessage(new Message());
-        }
     }
 
     private class buttonListener implements View.OnClickListener {
