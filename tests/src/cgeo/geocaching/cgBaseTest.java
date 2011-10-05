29f705cb5b3cd7708c2364cf0d78b128dd9a3b33
Samuel Tardieu <sam@rfc1149.net>
2011-10-05 11:33:30 +0200

    New elevation test

--- tests/src/cgeo/geocaching/cgBaseTest.java
+++ tests/src/cgeo/geocaching/cgBaseTest.java
@@ -1,6 +1,6 @@
 package cgeo.geocaching;
 
-import cgeo.geocaching.cgBase;
+import cgeo.geocaching.geopoint.Geopoint;
 
 import android.test.AndroidTestCase;
 
@@ -11,6 +11,10 @@ public class cgBaseTest extends AndroidTestCase {
 
     public void testReplaceWhitespaces() {
         Assert.assertEquals("foo bar baz ", cgBase.replaceWhitespace(new String("  foo\n\tbar   \r   baz  ")));
+    }
+
+    public void testElevation() {
+        Assert.assertEquals(125.663703918457, cgBase.getElevation(new Geopoint(48.0, 2.0)), 0.1);
     }
 
 }
