6c48f49b21e0c90329f22a4e6cbd5e3654a9c09f
Bananeweizen <bananeweizen@gmx.de>
2011-10-05 19:19:48 +0200

    code cleanup: remove another static methods warning

--- tests/src/cgeo/geocaching/cgBaseTest.java
+++ tests/src/cgeo/geocaching/cgBaseTest.java
@@ -6,14 +6,13 @@ import android.test.AndroidTestCase;
 
 import junit.framework.Assert;
 
-@SuppressWarnings("static-method")
 public class cgBaseTest extends AndroidTestCase {
 
-    public void testReplaceWhitespaces() {
+    public static void testReplaceWhitespaces() {
         Assert.assertEquals("foo bar baz ", cgBase.replaceWhitespace(new String("  foo\n\tbar   \r   baz  ")));
     }
 
-    public void testElevation() {
+    public static void testElevation() {
         Assert.assertEquals(125.663703918457, cgBase.getElevation(new Geopoint(48.0, 2.0)), 0.1);
     }
 
