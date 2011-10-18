a330f6ac9c68f089f95e6d97e85aba5f54681c1e
Samuel Tardieu <sam@rfc1149.net>
2011-10-18 22:33:29 +0200

    Synchronize access to Progress methods
    This is part of #670.

--- main/src/cgeo/geocaching/activity/Progress.java
+++ main/src/cgeo/geocaching/activity/Progress.java
@@ -10,26 +10,26 @@ public class Progress {
 
     private static ProgressDialog dialog;
 
-    public static void dismiss() {
+    public static synchronized void dismiss() {
         if (dialog != null && dialog.isShowing()) {
             dialog.dismiss();
         }
         dialog = null;
     }
 
-    public static void show(Context context, String title, String message, boolean indeterminate, boolean cancelable) {
+    public static synchronized void show(Context context, String title, String message, boolean indeterminate, boolean cancelable) {
         if (dialog == null) {
             dialog = ProgressDialog.show(context, title, message, indeterminate, cancelable);
         }
     }
 
-    public static void setMessage(final String message) {
+    public static synchronized void setMessage(final String message) {
         if (dialog != null && dialog.isShowing()) {
             dialog.setMessage(message);
         }
     }
 
-    public static boolean isShowing() {
+    public static synchronized boolean isShowing() {
         return dialog != null && dialog.isShowing();
     }
 
