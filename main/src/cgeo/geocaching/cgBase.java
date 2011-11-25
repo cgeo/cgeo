cgeo/cgeo

commit 9f1678c693937f603c9981320b3f34aa3d499a16

Fix #800: Refresh not stored caches
Also add progress status for refresh and store (but only works for refreshing not stored caches; should be redone completely, maybe with interface).

main/src/cgeo/geocaching/cgBase.java
@@ -2746,7 +2746,7 @@ public class cgBase {
                     cache = origCache;
                 }
             } else if (StringUtils.isNotBlank(geocode)) {
-                final cgSearch search = searchByGeocode(geocode, null, listId, false, null);
+                final cgSearch search = searchByGeocode(geocode, null, listId, false, handler);
                 cache = app.getCache(search);
             } else {
                 cache = null;

main/src/cgeo/geocaching/CacheDetailActivity.java
@@ -1248,34 +1248,54 @@ public class CacheDetailActivity extends AbstractActivity {
         private class StoreCacheHandler extends CancellableHandler {
             @Override
             public void handleRegularMessage(Message msg) {
-                storeThread = null;
+                if (cgBase.UPDATE_LOAD_PROGRESS_DETAIL == msg.what && msg.obj instanceof String) {
+                    updateStatusMsg((String) msg.obj);
+                } else {
+                    storeThread = null;
 
-                try {
-                    cache = app.getCache(search); // reload cache details
-                } catch (Exception e) {
-                    showToast(res.getString(R.string.err_store_failed));
+                    try {
+                        cache = app.getCache(search); // reload cache details
+                    } catch (Exception e) {
+                        showToast(res.getString(R.string.err_store_failed));
 
-                    Log.e(Settings.tag, "CacheDetailActivity.storeCacheHandler: " + e.toString());
+                        Log.e(Settings.tag, "CacheDetailActivity.storeCacheHandler: " + e.toString());
+                    }
+
+                    CacheDetailActivity.this.notifyDataSetChanged();
                 }
+            }
 
-                CacheDetailActivity.this.notifyDataSetChanged();
+            private void updateStatusMsg(final String msg) {
+                progress.setMessage(res.getString(R.string.cache_dialog_offline_save_message)
+                        + "\n\n"
+                        + msg);
             }
         }
 
         private class RefreshCacheHandler extends CancellableHandler {
             @Override
             public void handleRegularMessage(Message msg) {
-                refreshThread = null;
+                if (cgBase.UPDATE_LOAD_PROGRESS_DETAIL == msg.what && msg.obj instanceof String) {
+                    updateStatusMsg((String) msg.obj);
+                } else {
+                    refreshThread = null;
 
-                try {
-                    cache = app.getCache(search); // reload cache details
-                } catch (Exception e) {
-                    showToast(res.getString(R.string.err_refresh_failed));
+                    try {
+                        cache = app.getCache(search); // reload cache details
+                    } catch (Exception e) {
+                        showToast(res.getString(R.string.err_refresh_failed));
 
-                    Log.e(Settings.tag, "CacheDetailActivity.refreshCacheHandler: " + e.toString());
+                        Log.e(Settings.tag, "CacheDetailActivity.refreshCacheHandler: " + e.toString());
+                    }
+
+                    CacheDetailActivity.this.notifyDataSetChanged();
                 }
+            }
 
-                CacheDetailActivity.this.notifyDataSetChanged();
+            private void updateStatusMsg(final String msg) {
+                progress.setMessage(res.getString(R.string.cache_dialog_refresh_message)
+                        + "\n\n"
+                        + msg);
             }
         }
 

commit 6fde65ea65599330db070b18dd980c4e733aafa2

Fix bug introduced in 9f1678c693937f603c9981320b3f34aa3d499a16
Sometimes we can't use the handler-what-field for differentiate between handler-tasks because it is used for other progress/id-stuff.

main/src/cgeo/geocaching/cgBase.java
@@ -2746,7 +2746,7 @@ public class cgBase {
                     cache = origCache;
                 }
             } else if (StringUtils.isNotBlank(geocode)) {
-                final cgSearch search = searchByGeocode(geocode, null, listId, false, handler);
+                final cgSearch search = searchByGeocode(geocode, null, listId, false, null);
                 cache = app.getCache(search);
             } else {
                 cache = null;