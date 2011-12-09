079064a4194d69d08ccafbfd932785db84211d4f
blafoo <github@blafoo.de>
2011-12-09 21:41:30 +0100

    Additonal test cases

--- tests/src/cgeo/geocaching/enumerations/CacheSizeTest.java
+++ tests/src/cgeo/geocaching/enumerations/CacheSizeTest.java
@@ -3,9 +3,18 @@ package cgeo.geocaching.enumerations;
 import android.test.AndroidTestCase;
 
 public class CacheSizeTest extends AndroidTestCase {
+
     public static void testOrder() {
         assertTrue(CacheSize.MICRO.comparable < CacheSize.SMALL.comparable);
         assertTrue(CacheSize.SMALL.comparable < CacheSize.REGULAR.comparable);
         assertTrue(CacheSize.REGULAR.comparable < CacheSize.LARGE.comparable);
     }
+
+    public static void testGetById() {
+        assertEquals(CacheSize.UNKNOWN, CacheSize.getById(""));
+        assertEquals(CacheSize.UNKNOWN, CacheSize.getById(null));
+        assertEquals(CacheSize.UNKNOWN, CacheSize.getById("random garbage"));
+        assertEquals(CacheSize.LARGE, CacheSize.getById("large"));
+    }
+
 }
--- tests/src/cgeo/geocaching/enumerations/CacheTypeTest.java
+++ tests/src/cgeo/geocaching/enumerations/CacheTypeTest.java
@@ -8,6 +8,14 @@ public class CacheTypeTest extends AndroidTestCase {
         assertEquals(CacheType.UNKNOWN, CacheType.getById(""));
         assertEquals(CacheType.UNKNOWN, CacheType.getById(null));
         assertEquals(CacheType.UNKNOWN, CacheType.getById("random garbage"));
+        assertEquals(CacheType.WHERIGO, CacheType.getById("wherigo"));
+    }
+
+    public static void testGetByPattern() {
+        assertEquals(CacheType.UNKNOWN, CacheType.getByPattern(""));
+        assertEquals(CacheType.UNKNOWN, CacheType.getByPattern(null));
+        assertEquals(CacheType.UNKNOWN, CacheType.getByPattern("random garbage"));
+        assertEquals(CacheType.CITO, CacheType.getByPattern("cache in trash out event"));
     }
 
 }
