84f2034065378baf1ede7e8b71f1d9031ce8e8ed
Bananeweizen <bananeweizen@gmx.de>
2011-12-19 20:28:06 +0100

    fix #888: creating calendar entry on event date still doesn't work

--- main/src/cgeo/geocaching/cgCache.java
+++ main/src/cgeo/geocaching/cgCache.java
@@ -242,7 +242,7 @@ public class cgCache implements ICache {
         // is not in the past?
         final Calendar cal = Calendar.getInstance();
         cal.setTime(new Date());
-        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         cal.set(Calendar.MILLISECOND, 0);
