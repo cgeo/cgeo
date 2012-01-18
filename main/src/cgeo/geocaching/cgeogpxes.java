bed7d5c1a0009b2f15e9d6255341dc8ac7407c8d
Torsten Keil <github@torsten-keil.net>
2012-01-18 20:57:05 +0100

    Improved filename filter for file list on gpx import.

--- main/src/cgeo/geocaching/cgeogpxes.java
+++ main/src/cgeo/geocaching/cgeogpxes.java
@@ -71,7 +71,7 @@ public class cgeogpxes extends FileList<GPXListAdapter> {
                 return false;
             }
             // filter out waypoint files
-            return !StringUtils.endsWithIgnoreCase(filename, GPXImporter.WAYPOINTS_FILE_SUFFIX_AND_EXTENSION);
+            return StringUtils.lastIndexOfIgnoreCase(filename, GPXImporter.WAYPOINTS_FILE_SUFFIX) == -1;
         }
         return false;
     }
