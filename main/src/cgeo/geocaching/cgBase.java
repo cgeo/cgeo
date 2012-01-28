48cdb411cdd9b55e96c069204c422dd62a389c0d
rsudev <rasch@munin-soft.de>
2012-01-28 22:01:14 +0100

    New live-map reliability check

--- main/src/cgeo/geocaching/cgBase.java
+++ main/src/cgeo/geocaching/cgBase.java
@@ -737,10 +737,9 @@ public class cgBase {
             final JSONObject extra = dataJSON.getJSONObject("cs");
             if (extra != null && extra.length() > 0) {
                 int count = extra.getInt("count");
-                // unused, meaning not clear boolean li = extra.getBoolean("li");
-                // expected meaning pm=premium member
-                boolean pm = extra.getBoolean("pm");
-                if (Settings.isPremiumMember() && !pm) {
+                // check login status (presumably meaning of the 'li' member
+                boolean li = extra.getBoolean("li");
+                if (!li) {
                     parseResult.error = StatusCode.NOT_LOGGED_IN;
                 }
 
@@ -756,8 +755,8 @@ public class cgBase {
 
                             final cgCache cacheToAdd = new cgCache();
                             cacheToAdd.setDetailed(false);
-                            // coords could be reliable but we only can detect that for premium members
-                            cacheToAdd.setReliableLatLon(pm);
+                            // coords are reliable if we are logged in
+                            cacheToAdd.setReliableLatLon(li);
                             cacheToAdd.setGeocode(oneCache.getString("gc"));
                             cacheToAdd.setCoords(new Geopoint(oneCache.getDouble("lat"), oneCache.getDouble("lon")));
                             cacheToAdd.setName(oneCache.getString("nn"));
