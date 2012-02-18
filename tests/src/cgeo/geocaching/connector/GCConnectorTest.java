8035e5c16fd013a18d398ab14532a8fff482a459
blafoo <github@blafoo.de>
2012-02-18 23:39:12 +0100

    Positions in grid evaluated for caches

--- tests/src/cgeo/geocaching/connector/GCConnectorTest.java
+++ tests/src/cgeo/geocaching/connector/GCConnectorTest.java
@@ -1,6 +1,7 @@
 package cgeo.geocaching.connector;
 
 import cgeo.geocaching.SearchResult;
+import cgeo.geocaching.connector.gc.GCBase;
 import cgeo.geocaching.geopoint.Viewport;
 import cgeo.geocaching.test.mock.GC2JVEH;
 
@@ -10,11 +11,22 @@ public class GCConnectorTest extends AndroidTestCase {
 
     @SuppressWarnings("null")
     public static void testGetViewport() {
+
         GC2JVEH cache = new GC2JVEH();
         final Viewport viewport = new Viewport(cache.getCoords(), 1.0, 1.0);
-        SearchResult searchResult = GCConnectorImpl.searchByViewport(viewport);
+        SearchResult searchResult = GCBase.searchByViewport(viewport);
         assertTrue(searchResult != null);
-        assertTrue(searchResult.getGeocodes().contains(cache.getGeocode()));
+        assertEquals(3, searchResult.getCount());
+        assertTrue(searchResult.getGeocodes().contains("GC211WG"));
+    }
+
+    public static void testBaseCodings() {
+        assertEquals(2045702, GCBase.newidToGCId("CpLB"));
+        assertEquals("CpLB", GCBase.gcidToNewId(2045702));
+        assertEquals(2045702, GCBase.gccodeToGCId("GC2MEGA"));
+        assertEquals("GC2MEGA", GCBase.gcidToGCCode(2045702));
+
+        assertEquals("GC211WG", GCBase.newidToGeocode("gEaR"));
     }
 
 }
