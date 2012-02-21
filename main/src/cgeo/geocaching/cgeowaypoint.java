009c9575e84379612a710955436f703c7a27bdbc
rsudev <rasch@munin-soft.de>
2012-02-21 22:01:24 +0100

    Get geocode from wayoint fro clearing CachesCache if necessary, fixes #1154

--- main/src/cgeo/geocaching/cgeowaypoint.java
+++ main/src/cgeo/geocaching/cgeowaypoint.java
@@ -312,8 +312,14 @@ public class cgeowaypoint extends AbstractActivity {
 
         public void onClick(View arg0) {
             if (app.deleteWaypoint(id)) {
-                StaticMapsProvider.removeWpStaticMaps(id, geocode);
-                app.removeCache(geocode, EnumSet.of(RemoveFlag.REMOVE_CACHE));
+                String tmpCode = geocode;
+                if (StringUtils.isEmpty(tmpCode)) {
+                    tmpCode = waypoint.getGeocode();
+                }
+                StaticMapsProvider.removeWpStaticMaps(id, tmpCode);
+                if (!StringUtils.isEmpty(tmpCode)) {
+                    app.removeCache(tmpCode, EnumSet.of(RemoveFlag.REMOVE_CACHE));
+                }
 
                 finish();
                 return;
