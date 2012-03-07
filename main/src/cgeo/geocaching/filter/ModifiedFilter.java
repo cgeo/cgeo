734d52565d401abb9701f5e5efbb278745021e03
Bananeweizen <bananeweizen@gmx.de>
2012-03-07 07:25:45 +0100

    refactor #1002: use only flags available at cache

--- main/src/cgeo/geocaching/filter/ModifiedFilter.java
+++ main/src/cgeo/geocaching/filter/ModifiedFilter.java
@@ -1,7 +1,6 @@
 package cgeo.geocaching.filter;
 
 import cgeo.geocaching.cgCache;
-import cgeo.geocaching.cgWaypoint;
 
 public class ModifiedFilter extends AbstractFilter {
 
@@ -12,17 +11,6 @@ public class ModifiedFilter extends AbstractFilter {
     @Override
     public boolean accepts(final cgCache cache) {
         // modified on GC
-        if (cache.hasUserModifiedCoords()) {
-            return true;
-        }
-
-        // or with added/modified waypoints
-        for (cgWaypoint waypoint : cache.getWaypoints()) {
-            if (waypoint.isUserDefined()) {
-                return true;
-            }
-        }
-
-        return false;
+        return cache.hasUserModifiedCoords() || cache.hasFinalDefined();
     }
 }
