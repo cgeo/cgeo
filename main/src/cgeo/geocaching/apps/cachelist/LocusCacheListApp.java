9c028c81ed9c89adbb9ea24a58aa1952f9ead6c8
jenda^^ <jenda.69@gmail.com>
2012-03-19 20:58:26 +0100

    Export caches from Stored to Locus (#1282)
    Signed-off-by: jenda^^ <jenda.69@gmail.com>

--- main/src/cgeo/geocaching/apps/cachelist/LocusCacheListApp.java
+++ main/src/cgeo/geocaching/apps/cachelist/LocusCacheListApp.java
@@ -1,5 +1,6 @@
 package cgeo.geocaching.apps.cachelist;
 
+import cgeo.geocaching.R;
 import cgeo.geocaching.SearchResult;
 import cgeo.geocaching.cgCache;
 import cgeo.geocaching.cgGeo;
@@ -8,10 +9,18 @@ import cgeo.geocaching.apps.AbstractLocusApp;
 import org.apache.commons.collections.CollectionUtils;
 
 import android.app.Activity;
+import android.content.Intent;
 
 import java.util.List;
 
 class LocusCacheListApp extends AbstractLocusApp implements CacheListApp {
+
+    private boolean export;
+
+    public LocusCacheListApp(boolean export) {
+        super(getString(export ? R.string.caches_map_locus_export : R.string.caches_map_locus), Intent.ACTION_VIEW);
+        this.export = export;
+    }
 
     /**
      * show caches in Locus
@@ -25,7 +34,7 @@ class LocusCacheListApp extends AbstractLocusApp implements CacheListApp {
             return false;
         }
 
-        showInLocus(cacheList, false, activity);
+        showInLocus(cacheList, false, export, activity);
 
         return true;
     }
