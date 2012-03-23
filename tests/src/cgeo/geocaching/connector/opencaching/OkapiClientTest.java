ed88574985d94d0680c9c84f42659bdba400ce89
campbeb <bpcampbell@gmail.com>
2012-03-23 19:55:16 +0900

    Store OC caches to database on load and mark as detailed

--- tests/src/cgeo/geocaching/connector/opencaching/OkapiClientTest.java
+++ tests/src/cgeo/geocaching/connector/opencaching/OkapiClientTest.java
@@ -1,6 +1,8 @@
 package cgeo.geocaching.connector.opencaching;
 
 import cgeo.geocaching.cgCache;
+import cgeo.geocaching.cgeoapplication;
+import cgeo.geocaching.enumerations.LoadFlags;
 
 import android.test.AndroidTestCase;
 
@@ -12,6 +14,14 @@ public class OkapiClientTest extends AndroidTestCase {
         assertNotNull(cache);
         assertEquals(geoCode, cache.getGeocode());
         assertEquals("Oshkosh Municipal Tank", cache.getName());
+        assertTrue(cache.isDetailed());
+
+        // cache should be stored to DB (to listID 0) when loaded above
+        cache = cgeoapplication.getInstance().loadCache(geoCode, LoadFlags.LOAD_ALL_DB_ONLY);
+        assertNotNull(cache);
+        assertEquals(geoCode, cache.getGeocode());
+        assertEquals("Oshkosh Municipal Tank", cache.getName());
+        assertTrue(cache.isDetailed());
     }
 
 }
