4a2b8c73fc1be9218a554073f1a2b0ed1e850d8d
Samuel Tardieu <sam@rfc1149.net>
2012-04-02 10:24:35 +0200

    Deactivate debug info in release mode
    See issue #1301 for a discussion about this.

--- main/src/cgeo/geocaching/utils/Log.java
+++ main/src/cgeo/geocaching/utils/Log.java
@@ -1,41 +1,48 @@
 package cgeo.geocaching.utils;
 
+import cgeo.geocaching.BuildConfig;
 import cgeo.geocaching.Settings;
 
 final public class Log {
 
+    // A warning may be issued when DEBUG is false
+    @SuppressWarnings("unused")
+    private static boolean debugEnabled() {
+        return BuildConfig.DEBUG && Settings.isDebug();
+    }
+
     public static void v(final String tag, final String msg) {
-        if (Settings.isDebug()) {
+        if (debugEnabled()) {
             android.util.Log.v(tag, msg);
         }
     }
 
     public static void v(final String tag, final String msg, final Throwable t) {
-        if (Settings.isDebug()) {
+        if (debugEnabled()) {
             android.util.Log.v(tag, msg, t);
         }
     }
 
     public static void d(final String tag, final String msg) {
-        if (Settings.isDebug()) {
+        if (debugEnabled()) {
             android.util.Log.d(tag, msg);
         }
     }
 
     public static void d(final String tag, final String msg, final Throwable t) {
-        if (Settings.isDebug()) {
+        if (debugEnabled()) {
             android.util.Log.d(tag, msg, t);
         }
     }
 
     public static void i(final String tag, final String msg) {
-        if (Settings.isDebug()) {
+        if (debugEnabled()) {
             android.util.Log.i(tag, msg);
         }
     }
 
     public static void i(final String tag, final String msg, final Throwable t) {
-        if (Settings.isDebug()) {
+        if (debugEnabled()) {
             android.util.Log.i(tag, msg, t);
         }
     }
