96c84535b6098dfe34e8ad2ffd1821469e4a06a2
Stephan Merker <merker.stephan@googlemail.com>
2012-04-19 22:59:48 +0200

    make Log independent of Settings
    - related to #1347: Unit tests fail on Intel image with VM
      acceleration

--- tests/src/cgeo/geocaching/HtmlPerformanceTest.java
+++ tests/src/cgeo/geocaching/HtmlPerformanceTest.java
@@ -1,12 +1,13 @@
 package cgeo.geocaching;
 
 
+import cgeo.geocaching.utils.Log;
+
 import org.apache.commons.lang3.StringEscapeUtils;
 
 import android.os.SystemClock;
 import android.test.AndroidTestCase;
 import android.text.Html;
-import android.util.Log;
 
 public class HtmlPerformanceTest extends AndroidTestCase {
     private String input;
@@ -56,7 +57,7 @@ public class HtmlPerformanceTest extends AndroidTestCase {
         final long start = SystemClock.elapsedRealtime();
         runnable.run();
         final long end = SystemClock.elapsedRealtime();
-        Log.d("cgeotest", label + ": " + (end - start) + " ms");
+        Log.d(label + ": " + (end - start) + " ms");
         return end - start;
     }
 }
