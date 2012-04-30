79aa242f2bcb72d52c27390c099b016bb277e267
Portree-Kid <keith.paterson@gmx.de>
2012-04-30 22:21:07 +0200

    Removed commented code

--- main/src/cgeo/geocaching/maps/CachesOverlay.java
+++ main/src/cgeo/geocaching/maps/CachesOverlay.java
@@ -229,7 +229,6 @@ public class CachesOverlay extends AbstractItemizedOverlay {
                 cgeopopup.startActivity(context, coordinate.getGeocode());
             } else if (coordinate.getCoordType() != null && coordinate.getCoordType().equalsIgnoreCase("waypoint") && coordinate.getId() > 0) {
                 CGeoMap.markCacheAsDirty(coordinate.getGeocode());
-                //CacheDetailActivity.startActivity(context, coordinate.getGeocode(), CacheDetailActivity.Page.WAYPOINTS.ordinal());
                 WaypointPopup.startActivity(context, coordinate.getId(), coordinate.getGeocode());
             } else {
                 waitDialog.dismiss();
