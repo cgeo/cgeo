d1399dadad3886d8b60eb271a7631699e61dd5ee
Bananeweizen <bananeweizen@gmx.de>
2012-06-30 09:13:46 +0200

    fix #1850: log summary starts with comma

--- main/src/cgeo/geocaching/CacheDetailActivity.java
+++ main/src/cgeo/geocaching/CacheDetailActivity.java
@@ -2109,43 +2109,33 @@ public class CacheDetailActivity extends AbstractActivity {
             view = (ListView) getLayoutInflater().inflate(R.layout.cacheview_logs, null);
 
             // log count
-            if (cache.getLogCounts() != null) {
-                final StringBuilder text = new StringBuilder(200);
-                text.append(res.getString(R.string.cache_log_types));
-                text.append(": ");
-
-                // sort the log counts by type id ascending. that way the FOUND, DNF log types are the first and most visible ones
-                final List<Entry<LogType, Integer>> sortedLogCounts = new ArrayList<Entry<LogType, Integer>>();
-                for (Entry<LogType, Integer> entry : cache.getLogCounts().entrySet()) {
-                    sortedLogCounts.add(entry); // don't add these entries using addAll(), the iterator in the EntrySet can go wrong (see Findbugs)
-                }
-
-                Collections.sort(sortedLogCounts, new Comparator<Entry<LogType, Integer>>() {
-
-                    @Override
-                    public int compare(Entry<LogType, Integer> logCountItem1, Entry<LogType, Integer> logCountItem2) {
-                        return logCountItem1.getKey().compareTo(logCountItem2.getKey());
-                    }
-                });
-
-                boolean showLogCounter = false;
-                for (Entry<LogType, Integer> pair : sortedLogCounts) {
-                    String logTypeLabel = pair.getKey().getL10n();
+            final Map<LogType, Integer> logCounts = cache.getLogCounts();
+            if (logCounts != null) {
+                final List<Entry<LogType, Integer>> sortedLogCounts = new ArrayList<Entry<LogType, Integer>>(logCounts.size());
+                for (Entry<LogType, Integer> entry : logCounts.entrySet()) {
                     // it may happen that the label is unknown -> then avoid any output for this type
-                    if (logTypeLabel != null && pair.getKey() != LogType.PUBLISH_LISTING) {
-                        if (showLogCounter) {
-                            text.append(", ");
-                        }
-                        text.append(pair.getValue().intValue());
-                        text.append("× ");
-                        text.append(logTypeLabel);
+                    if (entry.getKey() != LogType.PUBLISH_LISTING && entry.getKey().getL10n() != null) {
+                        sortedLogCounts.add(entry);
                     }
-                    showLogCounter = true;
                 }
 
-                if (showLogCounter) {
+                if (sortedLogCounts.size() > 0) {
+                    // sort the log counts by type id ascending. that way the FOUND, DNF log types are the first and most visible ones
+                    Collections.sort(sortedLogCounts, new Comparator<Entry<LogType, Integer>>() {
+
+                        @Override
+                        public int compare(Entry<LogType, Integer> logCountItem1, Entry<LogType, Integer> logCountItem2) {
+                            return logCountItem1.getKey().compareTo(logCountItem2.getKey());
+                        }
+                    });
+
+                    ArrayList<String> labels = new ArrayList<String>(sortedLogCounts.size());
+                    for (Entry<LogType, Integer> pair : sortedLogCounts) {
+                        labels.add(pair.getValue().intValue() + "× " + pair.getKey().getL10n());
+                    }
+
                     final TextView countView = new TextView(CacheDetailActivity.this);
-                    countView.setText(text.toString());
+                    countView.setText(res.getString(R.string.cache_log_types) + ": " + StringUtils.join(labels, ", "));
                     view.addHeaderView(countView, null, false);
                 }
             }
