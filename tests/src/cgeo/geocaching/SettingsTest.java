651965129fbc5704bc2ca2845efe0d182357ab65
Bananeweizen <bananeweizen@gmx.de>
2012-11-18 09:04:04 +0100

    wrong assertion in mapfile test
    * interface to settings changed, now there can be null for new devices

--- tests/src/cgeo/geocaching/SettingsTest.java
+++ tests/src/cgeo/geocaching/SettingsTest.java
@@ -24,9 +24,9 @@ public class SettingsTest extends ActivityInstrumentationTestCase2<cgeo> {
      */
     public static void testSettingsException() {
         final String mapFile = Settings.getMapFile();
-        assertNotNull(mapFile);
         // We just want to ensure that it does not throw any exception but we do not know anything about the result
         MapsforgeMapProvider.isValidMapFile(mapFile);
+        assertTrue(true);
     }
 
     public static void testSettings() {
