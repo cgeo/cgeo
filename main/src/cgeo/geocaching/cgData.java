6335693959709d1d1f6cd08379e70b726731809a
Portree-Kid <keith.paterson@gmx.de>
2013-01-22 21:25:52 +0100

    Keep the Waypoint Ids stable. Fixes #2341

--- main/src/cgeo/geocaching/cgData.java
+++ main/src/cgeo/geocaching/cgData.java
@@ -1160,7 +1160,6 @@ public class cgData {
 
     private void saveOriginalWaypointsWithoutTransaction(final cgCache cache) {
         String geocode = cache.getGeocode();
-        database.delete(dbTableWaypoints, "geocode = ? and type <> ? and own = 0", new String[]{geocode, "own"});
 
         List<cgWaypoint> waypoints = cache.getWaypoints();
         if (CollectionUtils.isNotEmpty(waypoints)) {
@@ -1183,8 +1182,12 @@ public class cgData {
                 values.put("note", oneWaypoint.getNote());
                 values.put("own", oneWaypoint.isUserDefined() ? 1 : 0);
 
-                final long rowId = database.insert(dbTableWaypoints, null, values);
-                oneWaypoint.setId((int) rowId);
+                if (oneWaypoint.getId() < 0) {
+                    final long rowId = database.insert(dbTableWaypoints, null, values);
+                    oneWaypoint.setId((int) rowId);
+                } else {
+                    database.update(dbTableWaypoints, values, "_id = ?", new String[] { Integer.toString(oneWaypoint.getId(), 10) });
+                }
             }
         }
     }
