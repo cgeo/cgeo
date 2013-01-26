2f541d3b40e67e03c540f4e10dccf0384854fd0e
Samuel Tardieu <sam@rfc1149.net>
2013-01-26 22:58:20 +0100

    Add documentation of what is an unset waypoint id

--- main/src/cgeo/geocaching/IWaypoint.java
+++ main/src/cgeo/geocaching/IWaypoint.java
@@ -1,12 +1,14 @@
-/**
- *
- */
 package cgeo.geocaching;
 
 import cgeo.geocaching.enumerations.WaypointType;
 
 public interface IWaypoint extends ILogable, ICoordinates {
 
+    /**
+     * Return an unique waypoint id.
+     *
+     * @return a non-negative id if set, -1 if unset
+     */
     public abstract int getId();
 
     public abstract WaypointType getWaypointType();
