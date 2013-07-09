cgeo/cgeo

commit dc53fa7be33f3052e39952112679c7b0e0b3b324

Implements #2900, Support logging with password (oc)
Extends db, geocache and OKAPI to retrieve and store the property
Extends logging UI and OKAPI to allow entering and sending it

main/src/cgeo/geocaching/cgData.java
@@ -67,8 +67,8 @@ public class cgData {
             "difficulty", "direction", "distance", "terrain", "latlon", "location", "elevation", "personal_note", "shortdesc",
             //    24             25       26       27         28          29         30         31         32
             "favourite_cnt", "rating", "votes", "myvote", "disabled", "archived", "members", "found", "favourite",
-            //        33               34              35                36            37           38              39         40          41              42
-            "inventoryunknown", "onWatchlist", "reliable_latlon", "coordsChanged", "latitude", "longitude",  "finalDefined", "_id", "inventorycoins", "inventorytags"
+            //        33               34              35                36            37           38              39         40          41              42                   43
+            "inventoryunknown", "onWatchlist", "reliable_latlon", "coordsChanged", "latitude", "longitude", "finalDefined", "_id", "inventorycoins", "inventorytags", "logPasswordRequired"
             // reason is replaced by listId in Geocache
     };
 
@@ -86,7 +86,7 @@ public class cgData {
     private static int[] cacheColumnIndex;
     private static CacheCache cacheCache = new CacheCache();
     private static SQLiteDatabase database = null;
-    private static final int dbVersion = 67;
+    private static final int dbVersion = 68;
     public static final int customListIdOffset = 10;
     private static final String dbName = "data";
     private static final String dbTableCaches = "cg_caches";
@@ -146,6 +146,7 @@ public class cgData {
             + "onWatchlist integer default 0, "
             + "coordsChanged integer default 0, "
             + "finalDefined integer default 0"
+            + "logPasswordRequired integer default 0"
             + "); ";
     private static final String dbCreateLists = ""
             + "create table " + dbTableLists + " ("
@@ -685,6 +686,15 @@ public class cgData {
 
                         }
                     }
+                    // Introduces logPasswordRequired on caches
+                    if (oldVersion < 68) {
+                        try {
+                            db.execSQL("alter table " + dbTableCaches + " add column logPasswordRequired integer default 0");
+                        } catch (Exception e) {
+                            Log.e("Failed to upgrade to ver. 68", e);
+
+                        }
+                    }
                 }
 
                 db.setTransactionSuccessful();
@@ -1052,6 +1062,7 @@ public class cgData {
         values.put("onWatchlist", cache.isOnWatchlist() ? 1 : 0);
         values.put("coordsChanged", cache.hasUserModifiedCoords() ? 1 : 0);
         values.put("finalDefined", cache.hasFinalDefined() ? 1 : 0);
+        values.put("logPasswordRequired", cache.isLogPasswordRequired() ? 1 : 0);
 
         init();
 
@@ -1643,6 +1654,7 @@ public class cgData {
         cache.setReliableLatLon(cursor.getInt(cacheColumnIndex[35]) > 0);
         cache.setUserModifiedCoords(cursor.getInt(cacheColumnIndex[36]) > 0);
         cache.setFinalDefined(cursor.getInt(cacheColumnIndex[39]) > 0);
+        cache.setLogPasswordRequired(cursor.getInt(cacheColumnIndex[43]) > 0);
 
         Log.d("Loading " + cache.toString() + " (" + cache.getListId() + ") from DB");
 

main/res/layout/logcache_activity.xml
@@ -58,6 +58,29 @@
                     android:singleLine="false" />
 
                 <LinearLayout
+                    android:id="@+id/log_password_box"
+                    android:layout_width="fill_parent"
+                    android:layout_height="wrap_content"
+                    android:layout_marginBottom="5dip"
+                    android:orientation="vertical"
+                    android:visibility="gone" >
+                    <TextView
+                        android:layout_width="fill_parent"
+                        android:layout_height="wrap_content"
+                        android:gravity="left"
+                        android:padding="10dip"
+                        android:text="@string/log_password_title"
+                        android:textColor="?text_color"
+                        android:textSize="22sp" />
+                    <EditText
+                        android:id="@+id/log_password"
+                        style="@style/edittext_full"
+                        android:hint="@string/log_hint_log_password"
+                        android:inputType="text"
+                        android:singleLine="true" />
+                </LinearLayout>
+
+                <LinearLayout
                     android:id="@+id/tweet_box"
                     android:layout_width="fill_parent"
                     android:layout_height="wrap_content"

main/res/values/strings.xml
@@ -127,6 +127,8 @@
     <string name="log_image_caption">Caption</string>
     <string name="log_image_description">Description</string>
     <string name="log_image_scale">Scaling</string>
+    <string name="log_password_title">Log Password:</string>
+    <string name="log_hint_log_password">Enter your log password</string>
 
     <string-array name="log_image_scales">
         <item>No scaling</item>

main/src/cgeo/geocaching/Geocache.java
@@ -135,6 +135,7 @@ public class Geocache implements ICache, IWaypoint {
     private String nameForSorting;
     private final EnumSet<StorageLocation> storageLocation = EnumSet.of(StorageLocation.HEAP);
     private boolean finalDefined = false;
+    private boolean logPasswordRequired = false;
     private int zoomlevel = Tile.ZOOMLEVEL_MAX + 1;
 
     private static final Pattern NUMBER_PATTERN = Pattern.compile("\\d+");
@@ -1769,5 +1770,13 @@ public class Geocache implements ICache, IWaypoint {
 
     public int getMapMarkerId() {
         return getConnector().getCacheMapMarkerId(isDisabled() || isArchived());
+    }
+
+    public boolean isLogPasswordRequired() {
+        return logPasswordRequired;
+    }
+
+    public void setLogPasswordRequired(boolean required) {
+        logPasswordRequired = required;
     }
 }

main/src/cgeo/geocaching/LogCacheActivity.java
@@ -71,6 +71,7 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
     private Button postButton = null;
     private CheckBox tweetCheck = null;
     private LinearLayout tweetBox = null;
+    private LinearLayout logPasswordBox = null;
     private boolean tbChanged = false;
     private SparseArray<TrackableLog> actionButtons;
 
@@ -252,6 +253,7 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
         postButton = (Button) findViewById(R.id.post);
         tweetBox = (LinearLayout) findViewById(R.id.tweet_box);
         tweetCheck = (CheckBox) findViewById(R.id.tweet);
+        logPasswordBox = (LinearLayout) findViewById(R.id.log_password_box);
 
         // initialize with default values
         setDefaultValues();
@@ -302,6 +304,7 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
 
         tweetCheck.setChecked(true);
         updateTweetBox(typeSelected);
+        updateLogPasswordBox(typeSelected);
 
         final Button imageButton = (Button) findViewById(R.id.image_btn);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -377,6 +380,8 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
 
         final EditText logView = (EditText) findViewById(R.id.log);
         logView.setText(StringUtils.EMPTY);
+        final EditText logPasswordView = (EditText) findViewById(R.id.log_password);
+        logPasswordView.setText(StringUtils.EMPTY);
 
         updateImageButton();
 
@@ -491,6 +496,14 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
         }
     }
 
+    private void updateLogPasswordBox(LogType type) {
+        if (type == LogType.FOUND_IT && cache.isLogPasswordRequired()) {
+            logPasswordBox.setVisibility(View.VISIBLE);
+        } else {
+            logPasswordBox.setVisibility(View.GONE);
+        }
+    }
+
     private class DateListener implements View.OnClickListener {
 
         @Override
@@ -507,7 +520,7 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
             final String message = res.getString(StringUtils.isBlank(imageUri.getPath()) ?
                     R.string.log_saving :
                     R.string.log_saving_and_uploading);
-            new Poster(LogCacheActivity.this, message).execute(currentLogText());
+            new Poster(LogCacheActivity.this, message).execute(currentLogText(), currentLogPassword());
         }
     }
 
@@ -520,8 +533,9 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
         @Override
         protected StatusCode doInBackgroundInternal(final String[] logTexts) {
             final String log = logTexts[0];
+            final String logPwd = logTexts.length > 1 ? logTexts[1] : null;
             try {
-                final LogResult logResult = loggingManager.postLog(cache, typeSelected, date, log, trackables);
+                final LogResult logResult = loggingManager.postLog(cache, typeSelected, date, log, logPwd, trackables);
 
                 if (logResult.getPostLogResult() == StatusCode.NO_ERROR) {
                     final LogEntry logNow = new LogEntry(date, typeSelected, log);
@@ -591,6 +605,10 @@ public class LogCacheActivity extends AbstractLoggingActivity implements DateDia
 
     private String currentLogText() {
         return ((EditText) findViewById(R.id.log)).getText().toString();
+    }
+
+    private String currentLogPassword() {
+        return ((EditText) findViewById(R.id.log_password)).getText().toString();
     }
 
     @Override

main/src/cgeo/geocaching/connector/ILoggingManager.java
@@ -11,10 +11,23 @@ import java.util.List;
 
 public interface ILoggingManager {
 
+    /**
+     * Post a log for a cache online
+     * 
+     * @param cache
+     * @param logType
+     * @param date
+     * @param log
+     * @param logPassword
+     *            optional, maybe null
+     * @param trackableLogs
+     * @return
+     */
     LogResult postLog(Geocache cache,
             LogType logType,
             Calendar date,
             String log,
+            String logPassword,
             List<TrackableLog> trackableLogs);
 
     ImageResult postLogImage(String logId,

main/src/cgeo/geocaching/connector/NoLoggingManager.java
@@ -19,7 +19,7 @@ public class NoLoggingManager implements ILoggingManager {
     }
 
     @Override
-    public LogResult postLog(Geocache cache, LogType logType, Calendar date, String log, List<TrackableLog> trackableLogs) {
+    public LogResult postLog(Geocache cache, LogType logType, Calendar date, String log, String logPassword, List<TrackableLog> trackableLogs) {
         return new LogResult(StatusCode.LOG_POST_ERROR, "");
     }
 

main/src/cgeo/geocaching/connector/gc/GCLoggingManager.java
@@ -1,10 +1,10 @@
 package cgeo.geocaching.connector.gc;
 
 import cgeo.geocaching.Geocache;
+import cgeo.geocaching.LogCacheActivity;
 import cgeo.geocaching.R;
 import cgeo.geocaching.Settings;
 import cgeo.geocaching.TrackableLog;
-import cgeo.geocaching.LogCacheActivity;
 import cgeo.geocaching.activity.ActivityMixin;
 import cgeo.geocaching.connector.ILoggingManager;
 import cgeo.geocaching.connector.ImageResult;
@@ -80,7 +80,7 @@ public class GCLoggingManager implements ILoggingManager, LoaderManager.LoaderCa
     }
 
     @Override
-    public LogResult postLog(Geocache cache, LogType logType, Calendar date, String log, List<TrackableLog> trackableLogs) {
+    public LogResult postLog(Geocache cache, LogType logType, Calendar date, String log, String logPassword, List<TrackableLog> trackableLogs) {
 
         try {
             final ImmutablePair<StatusCode, String> postResult = GCParser.postLog(cache.getGeocode(), cache.getCacheId(), viewstates, logType,

commit 3dfb1b3c5aea16e7a5d730f2ec7327e381f0236d

Fixing serious bug in cache-table creation script.
This bug was introduced by me in dc53fa7be33f3052e39952112679c7b0e0b3b324

main/src/cgeo/geocaching/cgData.java
@@ -182,7 +182,7 @@ public class cgData {
             + "inventoryunknown integer default 0, "
             + "onWatchlist integer default 0, "
             + "coordsChanged integer default 0, "
-            + "finalDefined integer default 0"
+            + "finalDefined integer default 0, "
             + "logPasswordRequired integer default 0"
             + "); ";
     private static final String dbCreateLists = ""