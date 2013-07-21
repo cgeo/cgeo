6e4a8d4ba8a705be3d1e3b2714fe3590c49092e0
Bananeweizen <bananeweizen@gmx.de>
2013-07-21 23:18:20 +0200

    refactoring: simplify distance filter

--- main/src/cgeo/geocaching/filter/DistanceFilter.java
+++ main/src/cgeo/geocaching/filter/DistanceFilter.java
@@ -2,44 +2,52 @@ package cgeo.geocaching.filter;
 
 import cgeo.geocaching.Geocache;
 import cgeo.geocaching.IGeoData;
+import cgeo.geocaching.R;
 import cgeo.geocaching.cgeoapplication;
-import cgeo.geocaching.enumerations.CacheDistance;
 import cgeo.geocaching.geopoint.Geopoint;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 class DistanceFilter extends AbstractFilter {
-	private final CacheDistance cacheDistance;
     private final IGeoData geo;
+    private final int minDistance;
+    private final int maxDistance;
 
-    public DistanceFilter(final CacheDistance cacheDistance) {
-        super(cacheDistance.id);
-        this.cacheDistance = cacheDistance;
+    public DistanceFilter(String name, final int minDistance, final int maxDistance) {
+        super(name);
+        this.minDistance = minDistance;
+        this.maxDistance = maxDistance;
         geo = cgeoapplication.getInstance().currentGeo();
     }
 
     @Override
-    public boolean accepts(Geocache cache) {
-        Geopoint temp = new Geopoint(geo.getLocation());
-        double distance = temp.distanceTo(cache.getCoords());
+    public boolean accepts(final Geocache cache) {
+        final Geopoint currentPos = new Geopoint(geo.getLocation());
+        final float distance = currentPos.distanceTo(cache.getCoords());
 
-        return (distance > cacheDistance.minDistance) && (distance <= cacheDistance.maxDistance);
-    }
-
-    @Override
-    public String getName() {
-        return cacheDistance.getL10n();
+        return (distance >= minDistance) && (distance <= maxDistance);
     }
 
     public static class Factory implements IFilterFactory {
 
+        private static final int[] KILOMETERS = { 0, 2, 5, 10, 20, 50 };
+
         @Override
         public List<IFilter> getFilters() {
-            final CacheDistance[] cacheDistances = CacheDistance.values();
-            final List<IFilter> filters = new LinkedList<IFilter>();
-            for (CacheDistance cacheDistance : cacheDistances) {
-                filters.add(new DistanceFilter(cacheDistance));
+            final List<IFilter> filters = new ArrayList<IFilter>(KILOMETERS.length);
+            for (int i = 0; i < KILOMETERS.length; i++) {
+                final int minRange = KILOMETERS[i];
+                final int maxRange;
+                if (i < KILOMETERS.length - 1) {
+                    maxRange = KILOMETERS[i + 1];
+                }
+                else {
+                    maxRange = Integer.MAX_VALUE;
+                }
+                final String range = maxRange == Integer.MAX_VALUE ? "> " + String.valueOf(minRange) : String.valueOf(minRange) + " - " + String.valueOf(maxRange);
+                final String name = cgeoapplication.getInstance().getResources().getQuantityString(R.plurals.tts_kilometers, maxRange, range);
+                filters.add(new DistanceFilter(name, minRange, maxRange));
             }
             return filters;
         }
