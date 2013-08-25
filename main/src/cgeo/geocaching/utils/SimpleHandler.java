4e0b5281f318ba99b93b150c594cf994169958a2
Samuel Tardieu <sam@rfc1149.net>
2013-08-25 16:20:31 +0200

    refactoring: make SimpleHandler even simpler
    Remove everything not used by instances or subclasses.

--- main/src/cgeo/geocaching/utils/SimpleHandler.java
+++ main/src/cgeo/geocaching/utils/SimpleHandler.java
@@ -3,7 +3,6 @@ package cgeo.geocaching.utils;
 import cgeo.geocaching.activity.AbstractActivity;
 import cgeo.geocaching.activity.Progress;
 
-import android.content.res.Resources;
 import android.os.Handler;
 import android.os.Message;
 
@@ -15,47 +14,20 @@ public abstract class SimpleHandler extends Handler {
     protected final WeakReference<Progress> progressDialogRef;
 
     public SimpleHandler(final AbstractActivity activity, final Progress progress) {
-        this.activityRef = new WeakReference<AbstractActivity>(activity);
-        this.progressDialogRef = new WeakReference<Progress>(progress);
+        activityRef = new WeakReference<AbstractActivity>(activity);
+        progressDialogRef = new WeakReference<Progress>(progress);
     }
 
     @Override
     public void handleMessage(final Message msg) {
-        AbstractActivity activity = activityRef.get();
+        final AbstractActivity activity = activityRef.get();
         if (activity != null && msg.getData() != null && msg.getData().getString(MESSAGE_TEXT) != null) {
             activity.showToast(msg.getData().getString(MESSAGE_TEXT));
         }
-        dismissProgress();
-    }
-
-    protected final void showToast(final int resId) {
-        AbstractActivity activity = activityRef.get();
-        if (activity != null) {
-            Resources res = activity.getResources();
-            activity.showToast(res.getText(resId).toString());
-        }
-    }
-
-    protected final void dismissProgress() {
-        Progress progressDialog = progressDialogRef.get();
+        final Progress progressDialog = progressDialogRef.get();
         if (progressDialog != null) {
             progressDialog.dismiss();
         }
-    }
-
-    protected final void setProgressMessage(final String txt) {
-        Progress progressDialog = progressDialogRef.get();
-        if (progressDialog != null) {
-            progressDialog.setMessage(txt);
-        }
-    }
-
-    protected final void finishActivity() {
-        AbstractActivity activity = activityRef.get();
-        if (activity != null) {
-            activity.finish();
-        }
-
     }
 
 }
