3fe97b8a53c8aac0e3a65226010326847c8a08f3
Bananeweizen <bananeweizen@gmx.de>
2013-10-20 15:31:07 +0200

    fix compiler warning

--- main/src/cgeo/geocaching/WaypointViewHolder.java
+++ main/src/cgeo/geocaching/WaypointViewHolder.java
@@ -5,6 +5,7 @@ import butterknife.InjectView;
 import cgeo.geocaching.ui.AbstractViewHolder;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class WaypointViewHolder extends AbstractViewHolder {
@@ -13,7 +14,7 @@ public class WaypointViewHolder extends AbstractViewHolder {
     @InjectView(R.id.info) protected TextView infoView;
     @InjectView(R.id.name) protected TextView nameView;
     @InjectView(R.id.note) protected TextView noteView;
-    @InjectView(R.id.wpDefaultNavigation) protected View wpNavView;
+    @InjectView(R.id.wpDefaultNavigation) protected ImageView wpNavView;
 
     public WaypointViewHolder(View rowView) {
         super(rowView);
