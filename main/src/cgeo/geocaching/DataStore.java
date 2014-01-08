20232a47f33653f0950663c4e96467f4de60e303
Samuel Tardieu <sam@rfc1149.net>
2014-01-08 13:26:24 +0100

    fix #3527: bad query string
    Let's hope that the bad formatting occurring for, at least, very small
    negative values in `String.format()` with a `null` Locale will not
    happen with `StringBuilder.append(double)`.

--- main/src/cgeo/geocaching/DataStore.java
+++ main/src/cgeo/geocaching/DataStore.java
@@ -1675,7 +1675,7 @@ public class DataStore {
      * @return
      */
 
-    private static String buildCoordinateWhere(final String dbTable, final Viewport viewport) {
+    private static StringBuilder buildCoordinateWhere(final String dbTable, final Viewport viewport) {
         return viewport.resize(1.5).sqlWhere(dbTable);
     }
 
@@ -2265,7 +2265,7 @@ public class DataStore {
         }
 
         // viewport limitation
-        final StringBuilder selection = new StringBuilder(buildCoordinateWhere(dbTableCaches, viewport));
+        final StringBuilder selection = buildCoordinateWhere(dbTableCaches, viewport);
 
         // cacheType limitation
         String[] selectionArgs = null;
@@ -2870,7 +2870,7 @@ public class DataStore {
      */
 
     public static Set<Waypoint> loadWaypoints(final Viewport viewport, boolean excludeMine, boolean excludeDisabled, CacheType type) {
-        final StringBuilder where = new StringBuilder(buildCoordinateWhere(dbTableWaypoints, viewport));
+        final StringBuilder where = buildCoordinateWhere(dbTableWaypoints, viewport);
         if (excludeMine) {
             where.append(" and ").append(dbTableCaches).append(".found == 0");
         }
