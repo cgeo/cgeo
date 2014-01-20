d2be8fed38edf156700d6f9029de6fa4bd49afde
Bananeweizen <bananeweizen@gmx.de>
2014-01-20 19:05:51 +0100

    fix #3555: No marker on external map

--- main/src/cgeo/geocaching/apps/cache/navi/GoogleMapsApp.java
+++ main/src/cgeo/geocaching/apps/cache/navi/GoogleMapsApp.java
@@ -1,6 +1,8 @@
 package cgeo.geocaching.apps.cache.navi;
 
+import cgeo.geocaching.Geocache;
 import cgeo.geocaching.R;
+import cgeo.geocaching.Waypoint;
 import cgeo.geocaching.activity.ActivityMixin;
 import cgeo.geocaching.geopoint.Geopoint;
 import cgeo.geocaching.utils.Log;
@@ -22,10 +24,15 @@ class GoogleMapsApp extends AbstractPointNavigationApp {
 
     @Override
     public void navigate(Activity activity, Geopoint point) {
-        // INFO: q parameter works with Google Maps, but breaks cooperation with all other apps
+        navigate(activity, point, activity.getString(R.string.waypoint));
+    }
+
+    private static void navigate(Activity activity, Geopoint point, String label) {
         try {
-            activity.startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("geo:" + point.getLatitude() + "," + point.getLongitude())));
+            final String geoLocation = "geo:" + point.getLatitude() + "," + point.getLongitude();
+            final String query = point.getLatitude() + "," + point.getLongitude() + "(" + label + ")";
+            final String uriString = geoLocation + "?q=" + Uri.encode(query);
+            activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(uriString)));
             return;
         } catch (RuntimeException e) {
             // nothing
@@ -35,4 +42,13 @@ class GoogleMapsApp extends AbstractPointNavigationApp {
         ActivityMixin.showToast(activity, getString(R.string.err_application_no));
     }
 
+    @Override
+    public void navigate(Activity activity, Geocache cache) {
+        navigate(activity, cache.getCoords(), cache.getName());
+    }
+
+    @Override
+    public void navigate(Activity activity, Waypoint waypoint) {
+        navigate(activity, waypoint.getCoords(), waypoint.getName());
+    }
 }
