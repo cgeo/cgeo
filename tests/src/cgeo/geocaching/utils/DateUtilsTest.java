2c386f2b3eaf8a250d84361af67ce4f4f8e7a27b
Michael Keppler <michael.keppler@gmx.de>
2014-04-22 10:33:54 +0200

    new test for DateUtils.isPastEvent

--- tests/src/cgeo/geocaching/utils/DateUtilsTest.java
+++ tests/src/cgeo/geocaching/utils/DateUtilsTest.java
@@ -1,5 +1,8 @@
 package cgeo.geocaching.utils;
 
+import cgeo.geocaching.Geocache;
+import cgeo.geocaching.enumerations.CacheType;
+
 import java.util.Calendar;
 
 import junit.framework.TestCase;
@@ -12,6 +15,27 @@ public class DateUtilsTest extends TestCase {
             start.set(Calendar.HOUR_OF_DAY, hour);
             assertEquals(0, DateUtils.daysSince(start.getTimeInMillis()));
         }
+    }
+
+    public static void testIsPastEvent() {
+        final Calendar start = Calendar.getInstance();
+        start.set(Calendar.HOUR_OF_DAY, 0);
+        start.set(Calendar.MINUTE, 10);
+        assertPastEvent(start, false);
+
+        start.set(Calendar.HOUR_OF_DAY, 23);
+        assertPastEvent(start, false);
+
+        start.add(Calendar.DAY_OF_MONTH, -1);
+        assertPastEvent(start, true);
+    }
+
+    private static void assertPastEvent(final Calendar start, boolean expectedPast) {
+        final Geocache cache = new Geocache();
+        cache.setType(CacheType.EVENT);
+
+        cache.setHidden(start.getTime());
+        assertEquals(expectedPast, DateUtils.isPastEvent(cache));
     }
 
 }
