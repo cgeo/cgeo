06d6621f6667b8547530995ed85d827f79ba8fc2
Bananeweizen <bananeweizen@gmx.de>
2014-09-07 12:19:19 +0200

    verify waypoint name change on type change

--- tests/src/cgeo/geocaching/waypointactivity/AddWaypointActivityTest.java
+++ tests/src/cgeo/geocaching/waypointactivity/AddWaypointActivityTest.java
@@ -1,15 +1,27 @@
 package cgeo.geocaching.waypointactivity;
 
+import cgeo.geocaching.R;
+import cgeo.geocaching.enumerations.WaypointType;
+
+import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isClickable;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isNotChecked;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withChild;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 
-import cgeo.geocaching.R;
-import cgeo.geocaching.enumerations.WaypointType;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
 
 public class AddWaypointActivityTest extends AbstractAddWaypointActivityTest {
 
@@ -30,5 +42,21 @@ public class AddWaypointActivityTest extends AbstractAddWaypointActivityTest {
 
     public static void testNewWaypointNotVisited() {
         onView(withId(R.id.wpt_visited_checkbox)).check(matches(isNotChecked()));
+    }
+
+    public static void testSwitchingWaypointTypeChangesWaypointName() {
+        WaypointType waypointType = WaypointType.FINAL;
+
+        // verify we don't have a final type yet
+        onView(withId(R.id.name)).check(matches(not(withText(waypointType.getL10n()))));
+
+        // open type selector
+        onView(withId(R.id.type)).perform(ViewActions.click());
+
+        // select final type
+        onData(hasToString(startsWith(waypointType.getL10n()))).inAdapterView(isClickable()).perform(click());
+
+        // verify changed name
+        onView(withId(R.id.name)).check(matches(withText(waypointType.getL10n())));
     }
 }
