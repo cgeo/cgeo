25d1d3a911d76da4bfbc44c3b985caae661943f0
Mathieu Alorent <github@kumy.net>
2014-09-20 00:57:08 +0200

    Create a TimeDialog Fragment

---
+++ main/src/cgeo/geocaching/ui/dialog/TimeDialog.java
@@ -0,0 +1,62 @@
+package cgeo.geocaching.ui.dialog;
+
+import butterknife.ButterKnife;
+
+import cgeo.geocaching.R;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class TimeDialog extends DialogFragment {
+
+    public interface TimeDialogParent {
+        abstract public void setTime(final Calendar date);
+    }
+
+    private Calendar date;
+
+    public static TimeDialog getInstance(final Calendar date) {
+        final TimeDialog timeDialog = new TimeDialog();
+        final Bundle args = new Bundle();
+        args.putSerializable("date", date);
+        timeDialog.setArguments(args);
+        return timeDialog;
+    }
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+        final Bundle args = getArguments();
+        date = (Calendar) args.getSerializable("date");
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.time, container, false);
+
+        final TimePicker picker = ButterKnife.findById(view, R.id.picker);
+        picker.setCurrentHour(date.get(Calendar.HOUR_OF_DAY));
+        picker.setCurrentMinute(date.get(Calendar.MINUTE));
+        picker.setOnTimeChangedListener(new TimePickerListener());
+        return view;
+    }
+
+    private class TimePickerListener implements TimePicker.OnTimeChangedListener {
+
+        @Override
+        public void onTimeChanged(final TimePicker picker, final int hour, final int minutes) {
+            date.set(Calendar.HOUR_OF_DAY, hour);
+            date.set(Calendar.MINUTE, minutes);
+
+            ((TimeDialogParent) getActivity()).setTime(date);
+
+        }
+    }
+}
