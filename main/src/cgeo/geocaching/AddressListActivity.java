ac99b7505db1fa9e7c64ed886225502a6c37c4f9
Bananeweizen <bananeweizen@gmx.de>
2014-11-02 15:36:40 +0100

    refactoring: move Geocoder functionality into a separate class

--- main/src/cgeo/geocaching/AddressListActivity.java
+++ main/src/cgeo/geocaching/AddressListActivity.java
@@ -1,25 +1,22 @@
 package cgeo.geocaching;
 
 import cgeo.geocaching.activity.AbstractListActivity;
+import cgeo.geocaching.location.Geocoder;
 import cgeo.geocaching.ui.AddressListAdapter;
-import cgeo.geocaching.utils.Log;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import android.app.ProgressDialog;
 import android.location.Address;
-import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import java.util.List;
-import java.util.Locale;
 
 public class AddressListActivity extends AbstractListActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState, R.layout.addresslist_activity);
 
         // get parameters
@@ -38,23 +35,16 @@ public class AddressListActivity extends AbstractListActivity {
                 ProgressDialog.show(this, res.getString(R.string.search_address_started), keyword, true);
         waitDialog.setCancelable(true);
 
+        lookupAddressInBackground(keyword, adapter, waitDialog);
+    }
+
+    private void lookupAddressInBackground(final String keyword, final AddressListAdapter adapter, final ProgressDialog waitDialog) {
         new AsyncTask<Void, Void, List<Address>>() {
 
             @Override
-            protected List<Address> doInBackground(Void... params) {
-                final Geocoder geocoder = new Geocoder(AddressListActivity.this, Locale.getDefault());
-                try {
-                    return geocoder.getFromLocationName(keyword, 20);
-                } catch (Exception e) {
-                    // non Google devices come without the geocoder
-                    if (StringUtils.containsIgnoreCase(e.getMessage(), "Service not Available")) {
-                        Log.i("No geocoder available");
-                    }
-                    else {
-                        Log.e("AddressListActivity.doInBackground", e);
-                    }
-                    return null;
-                }
+            protected List<Address> doInBackground(final Void... params) {
+                final Geocoder geocoder = new Geocoder(AddressListActivity.this);
+                return geocoder.getFromLocationName(keyword);
             }
 
             @Override
