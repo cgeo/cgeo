c3a3fe7b9b6a65b7a91684ae325355537865c4cf
rsudev <rasch@munin-soft.de>
2014-12-23 23:46:21 +0100

    Fixes #4559, opencaching: use language instead of country to retrieve descriptions

--- main/src/cgeo/geocaching/connector/oc/OkapiClient.java
+++ main/src/cgeo/geocaching/connector/oc/OkapiClient.java
@@ -739,7 +739,7 @@ final class OkapiClient {
     }
 
     private static String getPreferredLanguage() {
-        final String code = Locale.getDefault().getCountry();
+        final String code = Locale.getDefault().getLanguage();
         if (StringUtils.isNotBlank(code)) {
             return StringUtils.lowerCase(code) + "|en";
         }
