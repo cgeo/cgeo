109102414e61d9f5c6773e979a05784c9cc0d9be
rsudev <rasch@munin-soft.de>
2015-02-09 23:36:17 +0100

    Make direction width depend on pixel density

--- main/src/cgeo/geocaching/maps/DirectionDrawer.java
+++ main/src/cgeo/geocaching/maps/DirectionDrawer.java
@@ -1,25 +1,37 @@
 package cgeo.geocaching.maps;
 
+import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.location.Geopoint;
 import cgeo.geocaching.maps.interfaces.MapItemFactory;
 import cgeo.geocaching.maps.interfaces.MapProjectionImpl;
 import cgeo.geocaching.settings.Settings;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.location.Location;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 public class DirectionDrawer {
     private Geopoint currentCoords;
     private final Geopoint destinationCoords;
     private final MapItemFactory mapItemFactory;
+    private final float width;
 
     private Paint line = null;
 
     public DirectionDrawer(final Geopoint coords) {
         this.destinationCoords = coords;
         this.mapItemFactory = Settings.getMapProvider().getMapItemFactory();
+
+        final DisplayMetrics metrics = new DisplayMetrics();
+        final WindowManager windowManager = (WindowManager) CgeoApplication.getInstance().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(metrics);
+
+        width = 4f * metrics.density;
+
     }
 
     public void setCoordinates(final Location coordinatesIn) {
@@ -34,7 +46,7 @@ public class DirectionDrawer {
         if (line == null) {
             line = new Paint();
             line.setAntiAlias(true);
-            line.setStrokeWidth(4f);
+            line.setStrokeWidth(width);
             line.setColor(0x80EB391E);
         }
 
