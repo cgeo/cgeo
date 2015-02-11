380b16655bc09c2ccc3d269b4d4470e41ca7c3fe
Samuel Tardieu <sam@rfc1149.net>
2015-02-11 20:26:44 +0100

    Remove unused helper method in tests

--- tests/src/cgeo/geocaching/utils/FileUtilsTest.java
+++ tests/src/cgeo/geocaching/utils/FileUtilsTest.java
@@ -6,9 +6,6 @@ import cgeo.geocaching.files.LocalStorage;
 
 import junit.framework.TestCase;
 
-import android.content.Context;
-import android.test.ServiceTestCase;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -18,15 +15,6 @@ public class FileUtilsTest extends TestCase {
     final File baseFile = new File(testDir, "prefix.ext");
     final File alternative1 = new File(testDir, "prefix_1.ext");
     final File alternative2 = new File(testDir, "prefix_2.ext");
-
-    private Context getTestContext() {
-        try {
-            return (Context) ServiceTestCase.class.getMethod("getTestContext").invoke(this);
-        } catch (final Exception e) {
-            Log.e("Unable to get test context", e);
-            return null;
-        }
-    }
 
     public void testGetUniqueNamedFile() throws IOException {
         FileUtils.deleteDirectory(testDir);
