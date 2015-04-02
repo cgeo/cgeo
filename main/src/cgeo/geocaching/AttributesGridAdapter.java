43bc29539a451782b84548429c943883867734ad
Samuel Tardieu <sam@rfc1149.net>
2015-04-02 08:45:25 +0200

    Remove unused import

--- main/src/cgeo/geocaching/AttributesGridAdapter.java
+++ main/src/cgeo/geocaching/AttributesGridAdapter.java
@@ -4,7 +4,6 @@ import cgeo.geocaching.enumerations.CacheAttribute;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
