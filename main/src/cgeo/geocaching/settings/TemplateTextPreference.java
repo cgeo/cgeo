90098c21ae6aa8d80d10076e8ce7c38733032d9e
Bananeweizen <bananeweizen@gmx.de>
2015-04-08 19:36:50 +0200

    remove unused import and make it an error in Eclipse

--- main/src/cgeo/geocaching/settings/TemplateTextPreference.java
+++ main/src/cgeo/geocaching/settings/TemplateTextPreference.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class TemplateTextPreference extends DialogPreference {
