30a00a8ce89af2113c93c8c9e1fde675c29ae108
Mathieu Alorent <github@kumy.net>
2015-04-25 20:19:35 +0200

    Add some Java doc

--- main/src/cgeo/geocaching/connector/trackable/TrackableConnector.java
+++ main/src/cgeo/geocaching/connector/trackable/TrackableConnector.java
@@ -57,33 +57,97 @@ public interface TrackableConnector {
     @NonNull
     public String getUrl(@NonNull final Trackable trackable);
 
+    /**
+     * Tell if the trackable has logging capabilities.
+     *
+     * @return True if trackable is loggable.
+     */
     public boolean isLoggable();
 
+    /**
+     * Return a Trackable corresponding to the Tracable Geocode (Tracking Code) or Guid.
+     * Note: Only GC conenctor support guid.
+     *
+     * @param geocode the trackable Tracking Code
+     * @param guid the trackable guid
+     * @param id the trackable id
+     * @return the Trackable object.
+     */
     @Nullable
     public Trackable searchTrackable(final String geocode, final String guid, final String id);
 
+    /**
+     * Return a Trackable corresponding to the Tracable Geocode.
+     *
+     * @param geocode the trackable
+     * @return the Trackable object.
+     */
     @NonNull
     public List<Trackable> searchTrackables(final String geocode);
 
+    /**
+     * Return a Trackable id from an url.
+     *
+     * @param url for one trackable
+     * @return the Trackable Geocode.
+     */
     @Nullable
     public String getTrackableCodeFromUrl(final @NonNull String url);
 
+    /**
+     * Return available user actions for the trackable.
+     *
+     * @return the List of available user action.
+     */
     @NonNull
     public List<UserAction> getUserActions();
 
+    /**
+     * Return the Brand object for the Trackable.
+     * If Brand could not be defined, return UNKNOWN_BRAND
+     *
+     * @return the Trackable Brand object.
+     */
     @NonNull
     public TrackableBrand getBrand();
 
+    /**
+     * Return a list of Trackable in user's inventory.
+     * In most case, user must be connected to the service.
+     *
+     * @return the Trackable list.
+     */
     @NonNull
     public List<Trackable> loadInventory();
 
+    /**
+     * Return the Trackable Logging Manager for the Trackable.
+     *
+     * @param activity currently running
+     * @return the Trackable logging manager.
+     */
     @Nullable
     public AbstractTrackableLoggingManager getTrackableLoggingManager(final AbstractLoggingActivity activity);
 
+    /**
+     * Tell if the trackable is loggable via a generic Trackable Connector.
+     *
+     * @return True if Trackable is loggable via a generic Trackable Connector.
+     */
     boolean isGenericLoggable();
 
+    /**
+     * Tell if the connector for this trackable is active.
+     *
+     * @return True if connector is active.
+     */
     boolean isActive();
 
+    /**
+     * Tell if user is registered to the connector for this trackable.
+     *
+     * @return True if user is connected to service.
+     */
     public boolean isRegistered();
 
     public int getInventoryLoaderId();
