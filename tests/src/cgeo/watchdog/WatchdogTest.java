3a74db56616c76beae15dab3df88f9f94dba005b
Bananeweizen <bananeweizen@gmx.de>
2015-05-25 18:08:34 +0200

    have new test for online website checks

--- tests/src/cgeo/watchdog/WatchdogTest.java
+++ tests/src/cgeo/watchdog/WatchdogTest.java
@@ -3,14 +3,40 @@ package cgeo.watchdog;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import cgeo.CGeoTestCase;
+import cgeo.geocaching.Geocache;
 import cgeo.geocaching.SearchResult;
 import cgeo.geocaching.connector.ConnectorFactory;
 import cgeo.geocaching.connector.oc.OCApiConnector;
+import cgeo.geocaching.enumerations.LoadFlags;
 
+/**
+ * This test is intended to run regularly on our CI server, to verify the availability of several geocaching websites
+ * and our ability to parse a cache from it.
+ * <p>
+ * You need all the opencaching API keys for this test to run.
+ * </p>
+ *
+ */
 public class WatchdogTest extends CGeoTestCase {
 
     public static void testOpenCachingDE() {
         downloadOpenCaching("OC1234");
+    }
+
+    public static void testOpenCachingNL() {
+        downloadOpenCaching("OB1AF6");
+    }
+
+    public static void testOpenCachingPL() {
+        downloadOpenCaching("OP89HC");
+    }
+
+    public static void testOpenCachingRO() {
+        downloadOpenCaching("OR011D");
+    }
+
+    public static void testOpenCachingUK() {
+        downloadOpenCaching("OK0345");
     }
 
     public static void testOpenCachingUS() {
@@ -21,6 +47,11 @@ public class WatchdogTest extends CGeoTestCase {
         final OCApiConnector connector = (OCApiConnector) ConnectorFactory.getConnector(geocode);
         final SearchResult searchResult = connector.searchByGeocode(geocode, null, null);
         assertThat(searchResult.getCount()).overridingErrorMessage("Failed to download from " + connector.getName()).isNotNull();
+
+        final Geocache geocache = searchResult.getFirstCacheFromResult(LoadFlags.LOAD_CACHE_OR_DB);
+        assertThat(geocache).isNotNull();
+        assert geocache != null; // Eclipse null analysis weakness
+        assertThat(geocache.getGeocode()).isEqualTo(geocode);
     }
 
 }
