705acc608958069fed4a80d6306d44795cbc4626
Bananeweizen <bananeweizen@gmx.de>
2015-05-31 08:37:15 +0200

    inline display of service capabilities

--- main/src/cgeo/geocaching/settings/CapabilitiesPreference.java
+++ main/src/cgeo/geocaching/settings/CapabilitiesPreference.java
@@ -1,20 +1,14 @@
 package cgeo.geocaching.settings;
 
-import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.R;
 import cgeo.geocaching.connector.ConnectorFactory;
 import cgeo.geocaching.connector.IConnector;
 
-import android.app.AlertDialog;
+import org.apache.commons.lang3.StringUtils;
+
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.res.TypedArray;
-import android.preference.Preference;
-import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
 
 /**
  * Preference for displaying the supported capabilities of an {@link IConnector} implementation.
@@ -35,53 +29,26 @@ public class CapabilitiesPreference extends AbstractAttributeBasedPreference {
         super(context, attrs, defStyle);
     }
 
-    @Override
-    public View getView(final View convertView, final ViewGroup parent) {
-        setOnPreferenceClickListener(new ClickListener());
-        return super.getView(convertView, parent);
-    }
-
-    private final class ClickListener implements OnPreferenceClickListener {
-        @Override
-        public boolean onPreferenceClick(final Preference preference) {
-            final WebView htmlView = new WebView(preference.getContext());
-            htmlView.loadDataWithBaseURL(null, createCapabilitiesMessage(), "text/html", "utf-8", null);
-            final AlertDialog.Builder builder = new AlertDialog.Builder(preference.getContext());
-            builder.setView(htmlView)
-                    .setIcon(android.R.drawable.ic_dialog_info)
-                    .setTitle(R.string.settings_features)
-                    .setPositiveButton(R.string.err_none, new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(final DialogInterface dialog, final int id) {
-                            dialog.cancel();
-                        }
-                    });
-            builder.create().show();
-            return false;
-        }
-    }
-
-    public String createCapabilitiesMessage() {
+    private String createCapabilitiesMessage() {
         // TODO: this needs a better key for the connectors
         final IConnector connector = ConnectorFactory.getConnector(connectorCode + "1234");
-        final StringBuilder builder = new StringBuilder("<p>"
-                + TextUtils.htmlEncode(CgeoApplication.getInstance().getString(R.string.feature_description)) + "</p><ul>");
 
-        for (final String capability : connector.getCapabilities()) {
-            builder.append("<li>").append(TextUtils.htmlEncode(capability)).append("</li>");
-        }
-
-        builder.append("</ul>");
-        return builder.toString();
+        return StringUtils.join(connector.getCapabilities(), "\n");
     }
 
     @Override
     protected void processAttributeValues(final TypedArray values) {
         connectorCode = values.getString(0);
+        setSummary(StringUtils.EMPTY);
     }
 
     @Override
     protected int[] getAttributeNames() {
         return new int[] { R.attr.connector };
+    }
+
+    @Override
+    public void setSummary(final CharSequence summary) {
+        super.setSummary(createCapabilitiesMessage());
     }
 }
