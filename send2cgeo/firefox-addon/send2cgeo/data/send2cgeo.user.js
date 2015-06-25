f68cbccf478fd424b2acfe4dba62f6b24a328bc8
SammysHP <sven@sammyshp.de>
2015-06-25 20:36:21 +0200

    Fix #5057: Split send2cgeo into a separate repository
    Now available at https://github.com/cgeo/send2cgeo

--- send2cgeo/firefox-addon/send2cgeo/data/send2cgeo.user.js
+++
@@ -1,1 +0,0 @@
-../../../send2cgeo.user.js
--- send2cgeo/firefox-addon/send2cgeo/lib/main.js
+++
@@ -1,8 +0,0 @@
-var pageMod = require("sdk/page-mod");
-var data = require("sdk/self").data;
- 
-pageMod.PageMod({
-  include: ["http://www.geocaching.com/seek/cache_details*", "http://www.geocaching.com/geocache/*", "http://www.geocaching.com/map/*"],
-  contentScriptWhen: "end",
-  contentScriptFile: data.url("send2cgeo.user.js")
-});
