18a6f6b98caee7d4757c8f344c6189062a5ca100
Samuel Tardieu <sam@rfc1149.net>
2016-02-23 16:07:01 +0100

    Do not repeat code

--- main/src/cgeo/geocaching/utils/EnvironmentUtils.java
+++ main/src/cgeo/geocaching/utils/EnvironmentUtils.java
@@ -1,8 +1,5 @@
 package cgeo.geocaching.utils;
 
-import org.apache.commons.lang3.StringUtils;
-import org.eclipse.jdt.annotation.NonNull;
-
 import android.os.Environment;
 
 public class EnvironmentUtils {
@@ -10,21 +7,7 @@ public class EnvironmentUtils {
         // utility class
     }
 
-    /**
-     * Same as {@link Environment#getExternalStorageState()} but more stable. We have seen null pointers here, probably
-     * when there are issues in the underlying mount.
-     */
-    @NonNull
-    public static String getExternalStorageState() {
-        try {
-            return Environment.getExternalStorageState();
-        } catch (final NullPointerException e) {
-            Log.w("Could not get external storage state", e);
-        }
-        return StringUtils.EMPTY;
-    }
-
     public static boolean isExternalStorageAvailable() {
-        return EnvironmentUtils.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
     }
 }
--- tests/src-android/cgeo/geocaching/test/EmulatorStateTest.java
+++ tests/src-android/cgeo/geocaching/test/EmulatorStateTest.java
@@ -4,14 +4,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import cgeo.geocaching.utils.EnvironmentUtils;
 
-import android.os.Environment;
-
 import junit.framework.TestCase;
 
 public class EmulatorStateTest extends TestCase {
 
     public static void testWritableMedia() {
         // check the emulator running our tests
-        assertThat(EnvironmentUtils.getExternalStorageState()).isEqualTo(Environment.MEDIA_MOUNTED);
+        assertThat(EnvironmentUtils.isExternalStorageAvailable()).isTrue();
     }
 }
