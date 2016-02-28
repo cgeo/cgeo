eb4e2f1394f498bea2e51318f989385e6440cf3e
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-02-28 11:17:24 +0100

    Remove redundant modifiers of interface fields.
    Every field declaration in the body of an interface is implicitly
    public, static, and final.

--- main/src/cgeo/geocaching/enumerations/LoadFlags.java
+++ main/src/cgeo/geocaching/enumerations/LoadFlags.java
@@ -22,13 +22,13 @@ public interface LoadFlags {
     }
 
     /** Retrieve cache from CacheCache only. Do not load from DB */
-    public final static EnumSet<LoadFlag> LOAD_CACHE_ONLY = EnumSet.of(LoadFlag.CACHE_BEFORE);
+    EnumSet<LoadFlag> LOAD_CACHE_ONLY = EnumSet.of(LoadFlag.CACHE_BEFORE);
     /** Retrieve cache from CacheCache first. If not found load from DB */
-    public final static EnumSet<LoadFlag> LOAD_CACHE_OR_DB = EnumSet.of(LoadFlag.CACHE_BEFORE, LoadFlag.DB_MINIMAL, LoadFlag.OFFLINE_LOG, LoadFlag.SPOILERS);
+    EnumSet<LoadFlag> LOAD_CACHE_OR_DB = EnumSet.of(LoadFlag.CACHE_BEFORE, LoadFlag.DB_MINIMAL, LoadFlag.OFFLINE_LOG, LoadFlag.SPOILERS);
     /** Retrieve cache (minimalistic information including waypoints) from DB first. If not found load from CacheCache */
-    public final static EnumSet<LoadFlag> LOAD_WAYPOINTS = EnumSet.of(LoadFlag.CACHE_AFTER, LoadFlag.DB_MINIMAL, LoadFlag.WAYPOINTS, LoadFlag.OFFLINE_LOG, LoadFlag.SPOILERS);
+    EnumSet<LoadFlag> LOAD_WAYPOINTS = EnumSet.of(LoadFlag.CACHE_AFTER, LoadFlag.DB_MINIMAL, LoadFlag.WAYPOINTS, LoadFlag.OFFLINE_LOG, LoadFlag.SPOILERS);
     /** Retrieve cache (all stored informations) from DB only. Do not load from CacheCache */
-    public final static EnumSet<LoadFlag> LOAD_ALL_DB_ONLY = EnumSet.range(LoadFlag.DB_MINIMAL, LoadFlag.OFFLINE_LOG);
+    EnumSet<LoadFlag> LOAD_ALL_DB_ONLY = EnumSet.range(LoadFlag.DB_MINIMAL, LoadFlag.OFFLINE_LOG);
 
     public enum SaveFlag {
         CACHE, // save only to CacheCache
@@ -36,7 +36,7 @@ public interface LoadFlags {
     }
 
     @NonNull
-    public final static EnumSet<SaveFlag> SAVE_ALL = EnumSet.allOf(SaveFlag.class);
+    EnumSet<SaveFlag> SAVE_ALL = EnumSet.allOf(SaveFlag.class);
 
     public enum RemoveFlag {
         CACHE, // save only to CacheCache
@@ -45,6 +45,6 @@ public interface LoadFlags {
     }
 
     @NonNull
-    public final static EnumSet<RemoveFlag> REMOVE_ALL = EnumSet.of(RemoveFlag.CACHE, RemoveFlag.DB);
+    EnumSet<RemoveFlag> REMOVE_ALL = EnumSet.of(RemoveFlag.CACHE, RemoveFlag.DB);
 
 }
--- main/src/cgeo/geocaching/location/IConversion.java
+++ main/src/cgeo/geocaching/location/IConversion.java
@@ -1,13 +1,13 @@
 package cgeo.geocaching.location;
 
 public interface IConversion {
-    public static final float MILES_TO_KILOMETER = 1.609344f;
-    public static final float FEET_TO_KILOMETER = 0.0003048f;
-    public static final float YARDS_TO_KILOMETER = 0.0009144f;
+    float MILES_TO_KILOMETER = 1.609344f;
+    float FEET_TO_KILOMETER = 0.0003048f;
+    float YARDS_TO_KILOMETER = 0.0009144f;
     /**
      * Factor used to calculate distance from meters to foot;
      * <p>
      * ft = m * METERS_TO_FEET;
      */
-    public static final double METERS_TO_FEET = 3.2808399d;
+    double METERS_TO_FEET = 3.2808399d;
 }
