dc1c24f6c510ceb16fad4c33526ddb9cd7f2ff8e
rsudev <rasch@munin-soft.de>
2016-04-06 10:41:33 +0200

    Code cleanup (duplications, codacy remarks)

--- main/src/cgeo/geocaching/maps/mapsforge/v6/caches/StoredCachesOverlay.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v6/caches/StoredCachesOverlay.java
@@ -6,7 +6,6 @@ import cgeo.geocaching.location.Viewport;
 import cgeo.geocaching.maps.mapsforge.v6.MapHandlers;
 import cgeo.geocaching.maps.mapsforge.v6.MfMapView;
 import cgeo.geocaching.models.Geocache;
-import cgeo.geocaching.models.Waypoint;
 import cgeo.geocaching.settings.Settings;
 import cgeo.geocaching.storage.DataStore;
 import cgeo.geocaching.utils.Log;
@@ -15,9 +14,6 @@ import org.eclipse.jdt.annotation.NonNull;
 import org.mapsforge.map.layer.Layer;
 
 import java.lang.ref.WeakReference;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -64,12 +60,8 @@ public class StoredCachesOverlay extends AbstractCachesOverlay {
                 // check if map moved or zoomed
                 //TODO Portree Use Rectangle inside with bigger search window. That will stop reloading on every move
                 final boolean moved = overlay.isInvalidated() || (previousViewport == null) || zoomNow != previousZoom ||
-                        (mapMoved(previousViewport, viewportNow) || !previousViewport.includes(viewportNow));
+                        (AbstractCachesOverlay.mapMoved(previousViewport, viewportNow) || !previousViewport.includes(viewportNow));
 
-                // update title on any change
-                if (moved || !viewportNow.equals(previousViewport)) {
-                    //                    map.displayHandler.sendEmptyMessage(UPDATE_TITLE);
-                }
                 previousZoom = zoomNow;
 
                 // save new values
@@ -81,8 +73,6 @@ public class StoredCachesOverlay extends AbstractCachesOverlay {
                 }
             } catch (final Exception e) {
                 Log.w("StoredCachesOverlay.startLoadtimer.start", e);
-            } finally {
-                //
             }
         }
     }
@@ -105,75 +95,10 @@ public class StoredCachesOverlay extends AbstractCachesOverlay {
         }
     }
 
-    private void fill(final Set<Geocache> caches) {
-        try {
-            //            showProgressHandler.sendEmptyMessage(SHOW_PROGRESS);
-            final Collection<String> removeCodes = getGeocodes();
-            final Collection<String> newCodes = new HashSet<>();
-
-            // display caches
-            final Set<Geocache> cachesToDisplay = caches;
-
-            if (!cachesToDisplay.isEmpty()) {
-                // Only show waypoints when less than showWaypointsthreshold Caches shown
-                final boolean showWaypoints = cachesToDisplay.size() < Settings.getWayPointsThreshold();
-
-                Log.d(String.format("CachsToDisplay: %d, showWaypoints: %b", cachesToDisplay.size(), showWaypoints));
-
-                for (final Geocache cache : cachesToDisplay) {
-
-                    if (cache == null) {
-                        continue;
-                    }
-                    if (showWaypoints) {
-                        final List<Waypoint> waypoints = cache.getWaypoints();
-                        for (final Waypoint waypoint : waypoints) {
-                            if (waypoint == null || waypoint.getCoords() == null) {
-                                continue;
-                            }
-                            if (removeCodes.contains(waypoint.getGpxId())) {
-                                removeCodes.remove(waypoint.getGpxId());
-                            } else {
-                                if (addItem(waypoint)) {
-                                    newCodes.add(waypoint.getGpxId());
-                                }
-                            }
-                        }
-                    }
-
-                    if (cache.getCoords() == null) {
-                        continue;
-                    }
-                    if (removeCodes.contains(cache.getGeocode())) {
-                        removeCodes.remove(cache.getGeocode());
-                    } else {
-                        if (addItem(cache)) {
-                            newCodes.add(cache.getGeocode());
-                        }
-                    }
-                }
-            }
-
-            syncLayers(removeCodes, newCodes);
-
-            repaint();
-        } finally {
-            //            showProgressHandler.sendEmptyMessage(HIDE_PROGRESS);
-        }
-    }
-
     @Override
     public void onDestroy() {
         timer.unsubscribe();
 
         super.onDestroy();
     }
-
-    private static boolean mapMoved(final Viewport referenceViewport, final Viewport newViewport) {
-        return Math.abs(newViewport.getLatitudeSpan() - referenceViewport.getLatitudeSpan()) > 50e-6 ||
-                Math.abs(newViewport.getLongitudeSpan() - referenceViewport.getLongitudeSpan()) > 50e-6 ||
-                Math.abs(newViewport.center.getLatitude() - referenceViewport.center.getLatitude()) > referenceViewport.getLatitudeSpan() / 4 ||
-                Math.abs(newViewport.center.getLongitude() - referenceViewport.center.getLongitude()) > referenceViewport.getLongitudeSpan() / 4;
-    }
-
 }
