7856798c019aa253a56ab97110ce4336c92570fe
Bananeweizen <bananeweizen@gmx.de>
2016-05-08 21:14:55 +0200

    add test for trimSpanned

--- tests/src/cgeo/geocaching/utils/TextUtilsTest.java
+++ tests/src/cgeo/geocaching/utils/TextUtilsTest.java
@@ -1,13 +1,15 @@
 package cgeo.geocaching.utils;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import android.text.SpannableString;
+
+import junit.framework.TestCase;
+
+import java.util.regex.Pattern;
 
 import cgeo.geocaching.connector.gc.GCConstants;
 import cgeo.geocaching.test.mock.MockedCache;
 
-import java.util.regex.Pattern;
-
-import junit.framework.TestCase;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TextUtilsTest extends TestCase {
     public static void testRegEx() {
@@ -30,5 +32,16 @@ public class TextUtilsTest extends TestCase {
         final String text = "abc-foobar-def-fooxyz-ghi-foobaz-jkl";
         assertThat(TextUtils.getMatch(text, patternAll, false, 1, null, false)).isEqualTo("bar");
         assertThat(TextUtils.getMatch(text, patternAll, false, 1, null, true)).isEqualTo("baz");
+    }
+
+    public static void testTrimSpanned() {
+        assertTrimSpanned(" ","");
+        assertTrimSpanned("\n", "");
+        assertTrimSpanned("a ", "a");
+        assertTrimSpanned("a\n", "a");
+    }
+
+    private static void assertTrimSpanned(String input, String expected) {
+        assertThat(TextUtils.trimSpanned(new SpannableString(input)).toString()).isEqualTo(new SpannableString(expected).toString());
     }
 }
