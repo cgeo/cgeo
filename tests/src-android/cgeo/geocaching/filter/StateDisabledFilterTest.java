ba12e2483140f68015e0e45190012f69a0923738
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2016-05-11 19:59:20 +0200

    Fix #5734: Do not show archived caches when filtering for disabled ones

--- tests/src-android/cgeo/geocaching/filter/StateDisabledFilterTest.java
+++ tests/src-android/cgeo/geocaching/filter/StateDisabledFilterTest.java
@@ -10,6 +10,7 @@ public class StateDisabledFilterTest extends TestCase {
 
     private StateFilterFactory.StateDisabledFilter disabledFilter;
     private Geocache disabledCache;
+    private Geocache archivedCache;
 
     @Override
     protected void setUp() throws Exception {
@@ -18,10 +19,15 @@ public class StateDisabledFilterTest extends TestCase {
         disabledFilter = new StateFilterFactory.StateDisabledFilter();
         disabledCache = new Geocache();
         disabledCache.setDisabled(true);
+
+        archivedCache = new Geocache();
+        archivedCache.setArchived(true);
+        archivedCache.setDisabled(true);
     }
 
     public void testAccepts() {
         assertThat(disabledFilter.accepts(disabledCache)).isTrue();
+        assertThat(disabledFilter.accepts(archivedCache)).isFalse();
         assertThat(disabledFilter.accepts(new Geocache())).isFalse();
     }
 
