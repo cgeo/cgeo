c7c751b4b8d51cae4db6056c1125cdd45ca3bb41
Samuel Tardieu <sam@rfc1149.net>
2016-07-13 23:22:08 +0200

    Remove useless TargetApi annotation
    Since minSdkVersion is 9, a TargetApi annotation with SDK 8 is no longer
    useful.

--- main/src/cgeo/geocaching/backup/CentralBackupAgent.java
+++ main/src/cgeo/geocaching/backup/CentralBackupAgent.java
@@ -2,11 +2,9 @@ package cgeo.geocaching.backup;
 
 import cgeo.geocaching.utils.ApplicationSettings;
 
-import android.annotation.TargetApi;
 import android.app.backup.BackupAgentHelper;
 import android.app.backup.SharedPreferencesBackupHelper;
 
-@TargetApi(8)
 public class CentralBackupAgent extends BackupAgentHelper {
 
     private static final String PREFS_BACKUP_KEY = "prefs";
