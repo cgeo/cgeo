0ec0be1efb514703ce66d96a48be3e7ee741a5f5
Sebastian Buchwald <UniQP@users.noreply.github.com>
2016-07-20 22:32:58 +0200

    Simplify angle normalization (#5846)

--- tests/src/cgeo/geocaching/utils/AngleUtilsTest.java
+++ tests/src/cgeo/geocaching/utils/AngleUtilsTest.java
@@ -7,26 +7,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AngleUtilsTest extends TestCase {
 
     public static void testNormalize() {
-        assertThat(AngleUtils.normalize(0)).isEqualTo(0.0f);
-        assertThat(AngleUtils.normalize(360)).isEqualTo(0.0f);
-        assertThat(AngleUtils.normalize(720)).isEqualTo(0.0f);
-        assertThat(AngleUtils.normalize(-360)).isEqualTo(0.0f);
-        assertThat(AngleUtils.normalize(-720)).isEqualTo(0.0f);
-        assertThat(AngleUtils.normalize(721)).isEqualTo(1.0f);
-        assertThat(AngleUtils.normalize(-721)).isEqualTo(359.0f);
+        assertEquals(AngleUtils.normalize(0), 0.0f, 0);
+        assertEquals(AngleUtils.normalize(-0.0f), 0.0f, 0);
+        assertEquals(AngleUtils.normalize(360), 0.0f, 0);
+        assertEquals(AngleUtils.normalize(720), 0.0f, 0);
+        assertEquals(AngleUtils.normalize(-360), 0.0f, 0);
+        assertEquals(AngleUtils.normalize(-720), 0.0f, 0);
+        assertEquals(AngleUtils.normalize(721), 1.0f, 0);
+        assertEquals(AngleUtils.normalize(-721), 359.0f, 0);
     }
 
     public static void testDifference() {
-        assertThat(AngleUtils.difference(12, 12)).isEqualTo(0.0f);
-        assertThat(AngleUtils.difference(372, 12)).isEqualTo(0.0f);
-        assertThat(AngleUtils.difference(12, 372)).isEqualTo(0.0f);
-        assertThat(AngleUtils.difference(10, 20)).isEqualTo(10.0f);
-        assertThat(AngleUtils.difference(355, 5)).isEqualTo(10.0f);
-        assertThat(AngleUtils.difference(715, -715)).isEqualTo(10.0f);
-        assertThat(AngleUtils.difference(20, 10)).isEqualTo(-10.0f);
-        assertThat(AngleUtils.difference(5, 355)).isEqualTo(-10.0f);
-        assertThat(AngleUtils.difference(-715, 715)).isEqualTo(-10.0f);
-        assertThat(AngleUtils.difference(-90, 90)).isEqualTo(-180.0f);
-        assertThat(AngleUtils.difference(90, -90)).isEqualTo(-180.0f);
+        assertEquals(AngleUtils.difference(12, 12), 0.0f, 0);
+        assertEquals(AngleUtils.difference(372, 12), 0.0f, 0);
+        assertEquals(AngleUtils.difference(12, 372), 0.0f, 0);
+        assertEquals(AngleUtils.difference(10, 20), 10.0f, 0);
+        assertEquals(AngleUtils.difference(355, 5), 10.0f, 0);
+        assertEquals(AngleUtils.difference(715, -715), 10.0f, 0);
+        assertEquals(AngleUtils.difference(20, 10), -10.0f, 0);
+        assertEquals(AngleUtils.difference(5, 355), -10.0f, 0);
+        assertEquals(AngleUtils.difference(-715, 715), -10.0f, 0);
+        assertEquals(AngleUtils.difference(-90, 90), -180.0f, 0);
+        assertEquals(AngleUtils.difference(90, -90), -180.0f, 0);
     }
 }
