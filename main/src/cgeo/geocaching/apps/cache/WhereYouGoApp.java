ca3a767dda230ee5b809d1d4b033760038726e6f
Bananeweizen <bananeweizen@gmx.de>
2016-07-29 17:05:39 +0200

    fix #5857: only allow wheryougo invocation for unique cartridge

--- main/src/cgeo/geocaching/apps/cache/WhereYouGoApp.java
+++ main/src/cgeo/geocaching/apps/cache/WhereYouGoApp.java
@@ -1,18 +1,21 @@
 package cgeo.geocaching.apps.cache;
 
-import cgeo.geocaching.models.Geocache;
 import cgeo.geocaching.R;
 import cgeo.geocaching.enumerations.CacheType;
-import cgeo.geocaching.utils.TextUtils;
-
-import org.apache.commons.lang3.StringUtils;
-import android.support.annotation.NonNull;
+import cgeo.geocaching.models.Geocache;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class WhereYouGoApp extends AbstractGeneralApp {
     private static final Pattern PATTERN_CARTRIDGE = Pattern.compile("(" + Pattern.quote("http://www.wherigo.com/cartridge/details.aspx?") + ".*?)" + Pattern.quote("\""));
@@ -31,7 +34,21 @@ public class WhereYouGoApp extends AbstractGeneralApp {
         activity.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getWhereIGoUrl(cache))));
     }
 
+    /**
+     * get the URL of the cartridge.
+     *
+     * @return {@code null} if there is no link to a cartridge, or if there are multiple different links
+     */
+    @Nullable
     protected static String getWhereIGoUrl(final Geocache cache) {
-        return TextUtils.getMatch(cache.getDescription(), PATTERN_CARTRIDGE, null);
+        final Matcher matcher = PATTERN_CARTRIDGE.matcher(cache.getDescription());
+        final Set<String> urls = new HashSet<>();
+        while (matcher.find()) {
+            urls.add(matcher.group(1));
+        }
+        if (urls.size() == 1) {
+            return urls.iterator().next();
+        }
+        return null;
     }
 }
--- tests/src-android/cgeo/geocaching/apps/cache/WhereYouGoAppTest.java
+++ tests/src-android/cgeo/geocaching/apps/cache/WhereYouGoAppTest.java
@@ -12,4 +12,10 @@ public class WhereYouGoAppTest extends TestCase {
         cache.setDescription("<p style=\"max-width:670px;\"><a href=\"http://www.wherigo.com/cartridge/details.aspx?CGUID=c4577c31-09e9-44f0-ae48-83737e57adbd\"><img class=\"InsideTable\"");
         assertThat(WhereYouGoApp.getWhereIGoUrl(cache)).isEqualTo("http://www.wherigo.com/cartridge/details.aspx?CGUID=c4577c31-09e9-44f0-ae48-83737e57adbd");
     }
+
+    public static void testGetWhereIGoUrlMultipleURLs() throws Exception {
+        final Geocache cache = new Geocache();
+        cache.setDescription("Lade dir den <font color=\"#FF0000\"><strong><a href=\"http://www.wherigo.com/cartridge/details.aspx?CGUID=22e7ef35-d8b6-4d4b-b506-8d8b520316b3\" target=\"_blank\"><img src=\"http://surfstoff.de/wherigo/gifs/wherigo_logo_klein.gif\" width=\"14\" height=\"14\" border=\"0\" /></a> <a href=\"http://www.wherigo.com/cartridge/details.aspx?CGUID=8fc0fb5e-7310-4685-ad06-143edf873ab0\" target=\"_blank\">Wherigo</a>");
+        assertThat(WhereYouGoApp.getWhereIGoUrl(cache)).isNull();
+    }
 }
