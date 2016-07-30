72ce3294d08b704f4dd4d0c8b5daada43cbbda15
Mathieu Alorent <github@kumy.net>
2016-07-30 13:57:13 +0200

    Fix codacy: Fields should be declared at the top of the class

--- main/src/cgeo/geocaching/models/TrackableLog.java
+++ main/src/cgeo/geocaching/models/TrackableLog.java
@@ -6,6 +6,14 @@ import cgeo.geocaching.enumerations.LogTypeTrackable;
 import org.apache.commons.lang3.StringUtils;
 
 public final class TrackableLog {
+    public final int ctl;
+    public final int id;
+    public final String geocode;
+    public final String trackCode;
+    public final String name;
+    public final TrackableBrand brand;
+    public LogTypeTrackable action = LogTypeTrackable.DO_NOTHING; // base.logTrackablesAction - no action
+
     public TrackableLog(final String geocode, final String trackCode, final String name, final int id, final int ctl, final TrackableBrand brand) {
         this.geocode = geocode;
         this.trackCode = trackCode;
@@ -14,14 +22,6 @@ public final class TrackableLog {
         this.ctl = ctl;
         this.brand = brand;
     }
-
-    public final int ctl;
-    public final int id;
-    public final String geocode;
-    public final String trackCode;
-    public final String name;
-    public final TrackableBrand brand;
-    public LogTypeTrackable action = LogTypeTrackable.DO_NOTHING; // base.logTrackablesAction - no action
 
     public void setAction(final LogTypeTrackable logTypeTrackable) {
         action = logTypeTrackable;
