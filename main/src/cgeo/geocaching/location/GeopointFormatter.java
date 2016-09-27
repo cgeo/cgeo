18bf26549f376d59e1f509086b36d7baacf2c07a
Bananeweizen <bananeweizen@gmx.de>
2016-09-27 07:03:50 +0200

    refactor: extract constant in geopoint formatter

--- main/src/cgeo/geocaching/location/GeopointFormatter.java
+++ main/src/cgeo/geocaching/location/GeopointFormatter.java
@@ -6,6 +6,8 @@ import java.util.Locale;
  * Formatting of Geopoint.
  */
 public class GeopointFormatter {
+    private static final String SEPARATOR = " · ";
+
     private GeopointFormatter() {
         // utility class
     }
@@ -73,7 +75,7 @@ public class GeopointFormatter {
 
             case LAT_LON_DECMINUTE: {
                 final Geopoint rgp = gp.roundedAt(60 * 1000);
-                return String.format(Locale.getDefault(), "%c %02d° %06.3f · %c %03d° %06.3f",
+                return String.format(Locale.getDefault(), "%c %02d° %06.3f" + SEPARATOR + "%c %03d° %06.3f",
                         rgp.getLatDir(), rgp.getLatDeg(), rgp.getLatMinRaw(), rgp.getLonDir(), rgp.getLonDeg(), rgp.getLonMinRaw());
             }
 
@@ -85,7 +87,7 @@ public class GeopointFormatter {
 
             case LAT_LON_DECSECOND: {
                 final Geopoint rgp = gp.roundedAt(3600 * 1000);
-                return String.format(Locale.getDefault(), "%c %02d° %02d' %06.3f\" · %c %03d° %02d' %06.3f\"",
+                return String.format(Locale.getDefault(), "%c %02d° %02d' %06.3f\"" + SEPARATOR + "%c %03d° %02d' %06.3f\"",
                         rgp.getLatDir(), rgp.getLatDeg(), rgp.getLatMin(), rgp.getLatSecRaw(),
                         rgp.getLonDir(), rgp.getLonDeg(), rgp.getLonMin(), rgp.getLonSecRaw());
             }
@@ -128,8 +130,8 @@ public class GeopointFormatter {
      * Reformats coordinates for Clipboard.
      * It removes the middle dot if present.
      */
-    public static CharSequence reformatForClipboard(CharSequence coordinatesToCopy) {
-        return coordinatesToCopy.toString().replace("· ", "");
+    public static CharSequence reformatForClipboard(final CharSequence coordinatesToCopy) {
+        return coordinatesToCopy.toString().replace(SEPARATOR, " ");
     }
 
 }
