985e3a5f677e1b34d0c73166c71bdfa42d0d8136
Bananeweizen <bananeweizen@gmx.de>
2016-10-04 17:11:00 +0200

    fix android lint

--- main/src/cgeo/geocaching/ui/recyclerview/RecyclerViewProvider.java
+++ main/src/cgeo/geocaching/ui/recyclerview/RecyclerViewProvider.java
@@ -1,14 +1,15 @@
 package cgeo.geocaching.ui.recyclerview;
 
 import android.app.Activity;
-import android.support.annotation.IntegerRes;
+import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import butterknife.ButterKnife;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+
+import butterknife.ButterKnife;
 
 public final class RecyclerViewProvider {
 
@@ -16,7 +17,7 @@ public final class RecyclerViewProvider {
         // utility class
     }
 
-    public static RecyclerView provideRecyclerView(@NonNull final Activity context, @IntegerRes final int viewId, final boolean fixedSize) {
+    public static RecyclerView provideRecyclerView(@NonNull final Activity context, @IdRes final int viewId, final boolean fixedSize) {
         final RecyclerView view = ButterKnife.findById(context, viewId);
         view.setHasFixedSize(fixedSize);
         view.addItemDecoration(new HorizontalDividerItemDecoration.Builder(context).build());
