c0fe708c3c60e4c1753738e76079637a56d8dd54
Samuel Tardieu <sam@rfc1149.net>
2016-11-16 23:52:19 +0100

    More complete tests for Network.rfc3986URLEncode

--- tests/src/cgeo/geocaching/network/NetworkTest.java
+++ tests/src/cgeo/geocaching/network/NetworkTest.java
@@ -1,8 +1,8 @@
 package cgeo.geocaching.network;
 
-import junit.framework.TestCase;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import junit.framework.TestCase;
 
 public class NetworkTest extends TestCase {
 
@@ -10,6 +10,8 @@ public class NetworkTest extends TestCase {
         assertThat(Network.rfc3986URLEncode("*")).isEqualTo("*");
         assertThat(Network.rfc3986URLEncode("~")).isEqualTo("~");
         assertThat(Network.rfc3986URLEncode(" ")).isEqualTo("%20");
+        assertThat(Network.rfc3986URLEncode("foo @+%/")).isEqualTo("foo%20%40%2B%25%2F");
+        assertThat(Network.rfc3986URLEncode("sales and marketing/Miami")).isEqualTo("sales%20and%20marketing%2FMiami");
     }
 
 }
