b3a65c81c598f3324ccdd56afd0f051f33b6b0ea
Sebastian Buchwald <Sebastian.Buchwald@kit.edu>
2017-06-10 10:02:41 +0200

    Sort by number of votes in case of equal GCVote rating

--- main/src/cgeo/geocaching/sorting/RatingComparator.java
+++ main/src/cgeo/geocaching/sorting/RatingComparator.java
@@ -12,7 +12,21 @@ class RatingComparator extends AbstractCacheComparator {
     protected int compareCaches(final Geocache cache1, final Geocache cache2) {
         final float rating1 = cache1.getRating();
         final float rating2 = cache2.getRating();
+
         // Voting can be disabled for caches, then assume an average rating instead
-        return Float.compare(rating2 != 0.0 ? rating2 : 2.5f, rating1 != 0.0 ? rating1 : 2.5f);
+        final int ratingComparison = Float.compare(rating2 != 0.0 ? rating2 : 2.5f, rating1 != 0.0 ? rating1 : 2.5f);
+        if (ratingComparison != 0) {
+            return ratingComparison;
+        }
+
+        return compare(cache2.getVotes(), cache1.getVotes());
+    }
+
+    /**
+     * copy of {@link Integer#compare(int, int)}, as that is not available on lower API levels
+     *
+     */
+    private static int compare(final int left, final int right) {
+        return left < right ? -1 : (left == right ? 0 : 1);
     }
 }
