35d9b143e071381505ce95c2e2f828e9ed6e80e3
Bananeweizen <bananeweizen@gmx.de>
2017-06-30 17:43:29 +0200

    code clean up

--- main/src/cgeo/geocaching/sorting/RatingComparator.java
+++ main/src/cgeo/geocaching/sorting/RatingComparator.java
@@ -8,28 +8,26 @@ import cgeo.geocaching.models.Geocache;
  */
 class RatingComparator extends AbstractCacheComparator {
 
+    /**
+     * Average rating of GC50*** determined on June 26th, 2017
+     */
+    private static final float AVERAGE_RATING = 3.4f;
+
+    private static final int AVERAGE_VOTES = 5;
+
     @Override
     protected int compareCaches(final Geocache cache1, final Geocache cache2) {
         return Float.compare(getWeightedArithmeticMean(cache2), getWeightedArithmeticMean(cache1));
     }
 
+    /**
+     * Add some artificial average ratings to weight caches with few ratings towards the average rating.
+     */
     private static float getWeightedArithmeticMean(final Geocache cache) {
-        // Add some artificial average ratings to weight caches with few ratings towards the average rating.
-        final int averageVotes = 5;
-
-        // Average rating of GC50*** determined on June 26th, 2017
-        final float averageRating = 3.4f;
         final float rating = cache.getRating();
         final int votes = cache.getVotes();
 
-        return (votes * rating + averageVotes * averageRating) / (votes + averageVotes);
+        return (votes * rating + AVERAGE_VOTES * AVERAGE_RATING) / (votes + AVERAGE_VOTES);
     }
 
-    /**
-     * copy of {@link Integer#compare(int, int)}, as that is not available on lower API levels
-     *
-     */
-    private static int compare(final int left, final int right) {
-        return left < right ? -1 : (left == right ? 0 : 1);
-    }
 }
