2d61c542d0d3646873163dca40961d60baf83326
Peter Storch <peter.storch@gmail.com>
2017-10-02 23:53:57 +0200

    fix #6744 attach oc log image

--- main/src/cgeo/geocaching/connector/oc/OkapiService.java
+++ main/src/cgeo/geocaching/connector/oc/OkapiService.java
@@ -13,7 +13,10 @@ enum OkapiService {
     SERVICE_ADD_LOG_IMAGE("/okapi/services/logs/images/add", OAuthLevel.Level3),
     SERVICE_USER("/okapi/services/users/user", OAuthLevel.Level1),
     SERVICE_USER_BY_USERNAME("/okapi/services/users/by_username", OAuthLevel.Level1),
-    SERVICE_UPLOAD_PERSONAL_NOTE("/okapi/services/caches/save_personal_notes", OAuthLevel.Level3), SERVICE_RESOLVE_URL("/okapi/services/caches/search/by_urls", OAuthLevel.Level1), SERVICE_API_INSTALLATION("/okapi/services/apisrv/installation", OAuthLevel.Level0);
+    SERVICE_UPLOAD_PERSONAL_NOTE("/okapi/services/caches/save_personal_notes", OAuthLevel.Level3),
+    SERVICE_RESOLVE_URL("/okapi/services/caches/search/by_urls", OAuthLevel.Level1),
+    SERVICE_API_INSTALLATION("/okapi/services/apisrv/installation", OAuthLevel.Level0),
+    SERVICE_LOG_ENTRY("/okapi/services/logs/entry", OAuthLevel.Level1);
 
     @NonNull
     final String methodName;
