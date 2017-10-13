c476b8c75464934b1e798ebb9369d5db130daf9b
Grobbo <claes.erik.sundstrom@gmail.com>
2017-10-13 08:08:39 +0200

    Fixed menu for user saved menu

--- main/src/cgeo/geocaching/CacheDetailActivity.java
+++ main/src/cgeo/geocaching/CacheDetailActivity.java
@@ -1826,10 +1826,14 @@ public class CacheDetailActivity extends AbstractViewPagerActivity<CacheDetailAc
             if (coordinates != null) {
                 coordinatesView.setOnClickListener(new CoordinatesFormatSwitcher(coordinates));
                 coordinatesView.setText(coordinates.toString());
+                addContextMenu(coordinatesView);
                 coordinatesView.setVisibility(View.VISIBLE);
             } else {
                 coordinatesView.setVisibility(View.GONE);
             }
+
+
+
 
             // info
             final String waypointInfo = Formatter.formatWaypointInfo(wpt);
@@ -2099,6 +2103,11 @@ public class CacheDetailActivity extends AbstractViewPagerActivity<CacheDetailAc
                                 buildDetailsContextMenu(actionMode, menu, res.getString(R.string.cache_event), true);
                                 menu.findItem(R.id.menu_calendar).setVisible(cache.canBeAddedToCalendar());
                                 return true;
+                            case R.id.coordinates:
+                                clickedItemText = ((TextView)view).getText();
+                                clickedItemText = GeopointFormatter.reformatForClipboard(clickedItemText);
+                                buildDetailsContextMenu(actionMode,menu,res.getText(R.string.cache_coordinates),true);
+                                return true;
                         }
                         return false;
                     }
@@ -2111,7 +2120,7 @@ public class CacheDetailActivity extends AbstractViewPagerActivity<CacheDetailAc
                     @Override
                     public boolean onCreateActionMode(final ActionMode actionMode, final Menu menu) {
                         actionMode.getMenuInflater().inflate(R.menu.details_context, menu);
-                        prepareClipboardActionMode(view, actionMode, menu);
+                        // prepareClipboardActionMode(view, actionMode, menu); // Calling method in lifecycle..
                         // Return true so that the action mode is shown
                         return true;
                     }
@@ -2130,6 +2139,7 @@ public class CacheDetailActivity extends AbstractViewPagerActivity<CacheDetailAc
                         }
                     }
                 });
+
                 return true;
             }
         });
