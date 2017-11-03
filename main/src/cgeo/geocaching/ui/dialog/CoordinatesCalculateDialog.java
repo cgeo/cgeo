c587c5c8311061e594015ff74d0983b20aa67d26
Slartibartfast <His.Royal.Slartness@gmail.com>
2017-11-03 14:37:29 +1030

    Fixed  #6823: Calculator Notes Cleanup

--- main/src/cgeo/geocaching/ui/dialog/CoordinatesCalculateDialog.java
+++ main/src/cgeo/geocaching/ui/dialog/CoordinatesCalculateDialog.java
@@ -5,6 +5,7 @@ import static cgeo.geocaching.R.id.coordTable;
 import static cgeo.geocaching.models.CalcState.ERROR_CHAR;
 import static cgeo.geocaching.ui.dialog.CoordinatesInputDialog.GEOPOINT_ARG;
 
+import cgeo.geocaching.EditWaypointActivity;
 import cgeo.geocaching.R;
 import cgeo.geocaching.activity.AbstractActivity;
 import cgeo.geocaching.location.Geopoint;
@@ -70,14 +71,12 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
     private static final String SYMBOL_SEC = "\"";
     private static final String SYMBOL_POINT = ".";
     public static final String CALC_STATE = "calc_state";
-    public static final String NOTES = "notes";
 
     private ImageButton doneButton;
     private boolean stateSaved = false;
 
     private Geopoint gp;
     private CalcState savedState;
-    private String savedNotes;
 
     /** List of equations to be displayed in the calculator */
     private List<CalculatorVariable> equations;
@@ -166,9 +165,9 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
         public void onClick(final View v) {
             // Save calculator state regardless of weather the coordinates are valid or not.
             final CalcState currentState = getCurrentState();
-            final String currentNotes = notes.getText().toString();
-            setSavedState(currentState, currentNotes);
-            ((CoordinatesInputDialog.CalculateState) getActivity()).saveCalculatorState(currentState, currentNotes);
+            setSavedState(currentState);
+            ((CoordinatesInputDialog.CalculateState) getActivity()).saveCalculatorState(currentState);
+            ((EditWaypointActivity) getActivity()).getUserNotes().setText(notes.getText());
 
             if (!areCurrentCoordinatesValid()) {
                 return;
@@ -311,9 +310,8 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
     /**
      * @param gp               Geopoint representing the coordinates from the CoordinateInputDialog
      * @param calculationState State to set the calculator to when created
-     * @param notes            user notes
      */
-    public static CoordinatesCalculateDialog getInstance(final Geopoint gp, final CalcState calculationState, final String notes) {
+    public static CoordinatesCalculateDialog getInstance(final Geopoint gp, final CalcState calculationState) {
         final Bundle args = new Bundle();
 
         if (gp != null) {
@@ -322,7 +320,7 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
 
         final CoordinatesCalculateDialog ccd = new CoordinatesCalculateDialog();
         ccd.setArguments(args);
-        ccd.setSavedState(calculationState, notes);
+        ccd.setSavedState(calculationState);
         return ccd;
     }
 
@@ -342,9 +340,8 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
                 gp = savedInstanceState.getParcelable(GEOPOINT_ARG);
             }
 
-            final String savedNotes = savedInstanceState.getString(NOTES);
             final byte[] bytes = savedInstanceState.getByteArray(CALC_STATE);
-            setSavedState(bytes != null ? (CalcState) SerializationUtils.deserialize(bytes) : null, savedNotes);
+            setSavedState(bytes != null ? (CalcState) SerializationUtils.deserialize(bytes) : null);
         }
     }
 
@@ -431,6 +428,7 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
         tLonResult = ButterKnife.findById(v, R.id.lonRes);
 
         notes = ButterKnife.findById(v, R.id.notes_text);
+        notes.setText(((EditWaypointActivity) getActivity()).getUserNotes().getText());
 
         latButtons = Arrays.asList(            bLatDeg[1], bLatDeg[0],
                                                bLatMin[1], bLatMin[0],
@@ -523,9 +521,6 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
             resetCalculator();
         }
 
-        if (savedNotes != null) {
-            notes.setText(savedNotes);
-        }
 
         resortEquations();
         updateResult();
@@ -538,7 +533,6 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
     public void onSaveInstanceState(final Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putByteArray(CALC_STATE, SerializationUtils.serialize(getCurrentState()));
-        outState.putString(NOTES, notes.getText().toString());
     }
 
     private void displayToast(final int message) {
@@ -651,9 +645,8 @@ public class CoordinatesCalculateDialog extends DialogFragment implements ClickC
         return false;
     }
 
-    private void setSavedState(final CalcState savedState, final String savedNotes) {
+    private void setSavedState(final CalcState savedState) {
         this.savedState = savedState;
-        this.savedNotes = savedNotes;
         stateSaved = true;
 
         if (doneButton != null) {
