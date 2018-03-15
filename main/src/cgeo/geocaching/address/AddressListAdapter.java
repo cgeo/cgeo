cgeo/cgeo

commit f56ac4d0be43d7ca61c707e57facad494176f0ff

refactor: reduce allocations in view holders
Avoid creating new view listeners in view holders, since memory
allocation may lead to garbage collection, and therefore notable delays
in list views.

Now only as many listeners as recycled views are created and they are
bound lately to the item underneath via the view tag.

main/src/cgeo/geocaching/address/AddressListAdapter.java
@@ -4,11 +4,11 @@ import cgeo.geocaching.R;
 import cgeo.geocaching.location.Geopoint;
 import cgeo.geocaching.location.Units;
 import cgeo.geocaching.sensors.Sensors;
+import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 
 import android.location.Address;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,7 +21,7 @@ import java.util.List;
 import butterknife.BindView;
 import org.apache.commons.lang3.StringUtils;
 
-class AddressListAdapter extends RecyclerView.Adapter<AddressListAdapter.AddressListHolder> {
+class AddressListAdapter extends AbstractRecyclerViewAdapter<AddressListAdapter.AddressListHolder> {
 
     @NonNull private final Geopoint location;
     @NonNull private final List<Address> addresses;
@@ -52,21 +52,23 @@ class AddressListAdapter extends RecyclerView.Adapter<AddressListAdapter.Address
     @Override
     public AddressListHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.addresslist_item, parent, false);
-        return new AddressListHolder(view);
+        final AddressListHolder viewHolder = new AddressListHolder(view);
+        viewHolder.itemView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(final View view) {
+                clickListener.onClickAddress(addresses.get(viewHolder.getItemPosition()));
+            }
+        });
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(final AddressListHolder holder, final int position) {
+        super.onBindViewHolder(holder, position);
         final Address address = addresses.get(position);
         holder.label.setText(getAddressText(address));
         holder.distance.setText(getDistanceText(address));
-        holder.itemView.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(final View v) {
-                clickListener.onClickAddress(address);
-            }
-        });
     }
 
     private CharSequence getDistanceText(final Address address) {

main/src/cgeo/geocaching/connector/gc/PocketQueryListAdapter.java
@@ -3,11 +3,11 @@ package cgeo.geocaching.connector.gc;
 import cgeo.geocaching.CacheListActivity;
 import cgeo.geocaching.R;
 import cgeo.geocaching.models.PocketQuery;
+import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 import cgeo.geocaching.utils.Formatter;
 
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +18,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-class PocketQueryListAdapter extends RecyclerView.Adapter<PocketQueryListAdapter.ViewHolder> {
+class PocketQueryListAdapter extends AbstractRecyclerViewAdapter<PocketQueryListAdapter.ViewHolder> {
 
     @NonNull private final List<PocketQuery> queries;
 
@@ -45,26 +45,28 @@ class PocketQueryListAdapter extends RecyclerView.Adapter<PocketQueryListAdapter
     @Override
     public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pocketquery_item, parent, false);
-        return new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.cachelist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                CacheListActivity.startActivityPocket(view.getContext(), queries.get(viewHolder.getItemPosition()));
+            }
+        });
+
+        viewHolder.download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                CacheListActivity.startActivityPocketDownload(view.getContext(), queries.get(viewHolder.getItemPosition()));
+            }
+        });
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
+        super.onBindViewHolder(holder, position);
         final PocketQuery pocketQuery = queries.get(position);
-        holder.cachelist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                CacheListActivity.startActivityPocket(v.getContext(), pocketQuery);
-            }
-        });
-
-        holder.download.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                CacheListActivity.startActivityPocketDownload(v.getContext(), pocketQuery);
-            }
-        });
-
         holder.download.setVisibility(pocketQuery.isDownloadable() ? View.VISIBLE : View.GONE);
         holder.label.setText(pocketQuery.getName());
         holder.info.setText(Formatter.formatPocketQueryInfo(pocketQuery));

main/src/cgeo/geocaching/files/FileSelectionListAdapter.java
@@ -1,13 +1,14 @@
 package cgeo.geocaching.files;
 
 import cgeo.geocaching.R;
+import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -16,7 +17,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-public class FileSelectionListAdapter extends RecyclerView.Adapter<FileSelectionListAdapter.ViewHolder> {
+public class FileSelectionListAdapter extends AbstractRecyclerViewAdapter<FileSelectionListAdapter.ViewHolder> {
 
     private final IFileSelectionView parentView;
     @NonNull private final List<File> files;
@@ -34,11 +35,23 @@ public class FileSelectionListAdapter extends RecyclerView.Adapter<FileSelection
     @Override
     public ViewHolder onCreateViewHolder(final ViewGroup parent, final int position) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mapfile_item, parent, false);
-        return new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.itemView.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(final View view) {
+                final File file = files.get(viewHolder.getItemPosition());
+                parentView.setCurrentFile(file.toString());
+                parentView.close();
+            }
+        });
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
+        super.onBindViewHolder(holder, position);
         final File file = files.get(position);
 
         final String currentFile = parentView.getCurrentFile();
@@ -48,26 +61,8 @@ public class FileSelectionListAdapter extends RecyclerView.Adapter<FileSelection
             holder.filename.setTypeface(holder.filename.getTypeface(), Typeface.NORMAL);
         }
 
-        final TouchListener touchLst = new TouchListener(file);
-        holder.itemView.setOnClickListener(touchLst);
-
         holder.filepath.setText(file.getParent());
         holder.filename.setText(file.getName());
-    }
-
-    private class TouchListener implements View.OnClickListener {
-        private File file = null;
-
-        TouchListener(final File fileIn) {
-            file = fileIn;
-        }
-
-        // tap on item
-        @Override
-        public void onClick(final View view) {
-            parentView.setCurrentFile(file.toString());
-            parentView.close();
-        }
     }
 
     protected static final class ViewHolder extends AbstractRecyclerViewHolder {

main/src/cgeo/geocaching/files/GPXListAdapter.java
@@ -2,6 +2,7 @@ package cgeo.geocaching.files;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.ui.dialog.Dialogs;
+import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 import cgeo.geocaching.utils.FileUtils;
 
@@ -18,7 +19,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-class GPXListAdapter extends RecyclerView.Adapter<GPXListAdapter.ViewHolder> {
+class GPXListAdapter extends AbstractRecyclerViewAdapter<GPXListAdapter.ViewHolder> {
     private final GpxFileListActivity activity;
     @NonNull private final List<File> files;
 
@@ -44,32 +45,24 @@ class GPXListAdapter extends RecyclerView.Adapter<GPXListAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(final ViewGroup parent, final int position) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.gpx_item, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
-        final File file = files.get(position);
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(final View v) {
+            public void onClick(final View view) {
+                final File file = files.get(viewHolder.getItemPosition());
                 (new GPXImporter(activity, activity.getListId(), null)).importGPX(file);
             }
         });
-
-        holder.filepath.setText(file.getParent());
-        holder.filename.setText(file.getName());
-
-        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+        viewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
 
             @Override
-            public boolean onLongClick(final View v) {
+            public boolean onLongClick(final View view) {
+                final File file = files.get(viewHolder.getItemPosition());
                 Dialogs.confirmYesNo(activity, R.string.gpx_import_delete_title, activity.getString(R.string.gpx_import_delete_message, file.getName()), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(final DialogInterface dialog, final int id) {
-                        final int currentPosition = holder.getAdapterPosition();
+                        final int currentPosition = viewHolder.getAdapterPosition();
                         if (currentPosition != RecyclerView.NO_POSITION) {
                             FileUtils.deleteIgnoringFailure(file);
                             files.remove(currentPosition);
@@ -80,6 +73,17 @@ class GPXListAdapter extends RecyclerView.Adapter<GPXListAdapter.ViewHolder> {
                 return true;
             }
         });
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        super.onBindViewHolder(holder, position);
+        final File file = files.get(position);
+
+        holder.filepath.setText(file.getParent());
+        holder.filename.setText(file.getName());
     }
 
 }

main/src/cgeo/geocaching/helper/HelperAppAdapter.java
@@ -2,12 +2,12 @@ package cgeo.geocaching.helper;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.compatibility.Compatibility;
+import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +20,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-final class HelperAppAdapter extends RecyclerView.Adapter<HelperAppAdapter.ViewHolder> {
+final class HelperAppAdapter extends AbstractRecyclerViewAdapter<HelperAppAdapter.ViewHolder> {
 
     @NonNull private final List<HelperApp> helperApps;
     @NonNull private final HelperAppClickListener clickListener;
@@ -50,21 +50,24 @@ final class HelperAppAdapter extends RecyclerView.Adapter<HelperAppAdapter.ViewH
     @Override
     public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
         final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.usefulapps_item, parent, false);
-        return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
-        final HelperApp app = helperApps.get(position);
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        final ViewHolder viewHolder = new ViewHolder(view);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(final View v) {
+            public void onClick(final View view) {
+                final HelperApp app = helperApps.get(viewHolder.getItemPosition());
                 clickListener.onClickHelperApp(app);
             }
         });
 
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        super.onBindViewHolder(holder, position);
+
+        final HelperApp app = helperApps.get(position);
         final Resources resources = context.getResources();
         holder.title.setText(resources.getString(app.titleId));
         holder.image.setImageDrawable(Compatibility.getDrawable(resources, app.iconId));

commit 49b096bbccd65be436be42966dd96fd984d6ec3b

Fix #6912: Do not cache item position
The regression was introduced in f56ac4d0be43d7ca61c707e57facad494176f0ff.

main/src/cgeo/geocaching/address/AddressListAdapter.java
@@ -4,11 +4,11 @@ import cgeo.geocaching.R;
 import cgeo.geocaching.location.Geopoint;
 import cgeo.geocaching.location.Units;
 import cgeo.geocaching.sensors.Sensors;
-import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 
 import android.location.Address;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,7 +21,7 @@ import java.util.List;
 import butterknife.BindView;
 import org.apache.commons.lang3.StringUtils;
 
-class AddressListAdapter extends AbstractRecyclerViewAdapter<AddressListAdapter.AddressListHolder> {
+class AddressListAdapter extends RecyclerView.Adapter<AddressListAdapter.AddressListHolder> {
 
     @NonNull private final Geopoint location;
     @NonNull private final List<Address> addresses;
@@ -57,7 +57,7 @@ class AddressListAdapter extends AbstractRecyclerViewAdapter<AddressListAdapter.
 
             @Override
             public void onClick(final View view) {
-                clickListener.onClickAddress(addresses.get(viewHolder.getItemPosition()));
+                clickListener.onClickAddress(addresses.get(viewHolder.getAdapterPosition()));
             }
         });
         return viewHolder;
@@ -65,7 +65,6 @@ class AddressListAdapter extends AbstractRecyclerViewAdapter<AddressListAdapter.
 
     @Override
     public void onBindViewHolder(final AddressListHolder holder, final int position) {
-        super.onBindViewHolder(holder, position);
         final Address address = addresses.get(position);
         holder.label.setText(getAddressText(address));
         holder.distance.setText(getDistanceText(address));

main/src/cgeo/geocaching/connector/gc/PocketQueryListAdapter.java
@@ -3,11 +3,11 @@ package cgeo.geocaching.connector.gc;
 import cgeo.geocaching.CacheListActivity;
 import cgeo.geocaching.R;
 import cgeo.geocaching.models.PocketQuery;
-import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 import cgeo.geocaching.utils.Formatter;
 
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +16,7 @@ import android.widget.TextView;
 
 import butterknife.BindView;
 
-class PocketQueryListAdapter extends AbstractRecyclerViewAdapter<PocketQueryListAdapter.ViewHolder> {
+class PocketQueryListAdapter extends RecyclerView.Adapter<PocketQueryListAdapter.ViewHolder> {
 
     @NonNull private final PocketQueryListActivity activity;
 
@@ -47,7 +47,7 @@ class PocketQueryListAdapter extends AbstractRecyclerViewAdapter<PocketQueryList
         viewHolder.cachelist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
-                CacheListActivity.startActivityPocket(view.getContext(), activity.getQueries().get(viewHolder.getItemPosition()));
+                CacheListActivity.startActivityPocket(view.getContext(), activity.getQueries().get(viewHolder.getAdapterPosition()));
             }
         });
         viewHolder.cachelist.setVisibility(activity.onlyDownloadable() ? View.GONE : View.VISIBLE);
@@ -55,7 +55,7 @@ class PocketQueryListAdapter extends AbstractRecyclerViewAdapter<PocketQueryList
         viewHolder.download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                final PocketQuery pocketQuery = activity.getQueries().get(viewHolder.getItemPosition());
+                final PocketQuery pocketQuery = activity.getQueries().get(viewHolder.getAdapterPosition());
                 if (activity.onlyDownloadable()) {
                     activity.returnResult(pocketQuery);
                 } else {
@@ -69,7 +69,6 @@ class PocketQueryListAdapter extends AbstractRecyclerViewAdapter<PocketQueryList
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        super.onBindViewHolder(holder, position);
         final PocketQuery pocketQuery = activity.getQueries().get(position);
         holder.download.setVisibility(pocketQuery.isDownloadable() ? View.VISIBLE : View.GONE);
         holder.label.setText(pocketQuery.getName());

main/src/cgeo/geocaching/files/FileSelectionListAdapter.java
@@ -1,11 +1,11 @@
 package cgeo.geocaching.files;
 
 import cgeo.geocaching.R;
-import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,7 +17,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-public class FileSelectionListAdapter extends AbstractRecyclerViewAdapter<FileSelectionListAdapter.ViewHolder> {
+public class FileSelectionListAdapter extends RecyclerView.Adapter<FileSelectionListAdapter.ViewHolder> {
 
     private final IFileSelectionView parentView;
     @NonNull private final List<File> files;
@@ -40,7 +40,7 @@ public class FileSelectionListAdapter extends AbstractRecyclerViewAdapter<FileSe
 
             @Override
             public void onClick(final View view) {
-                final File file = files.get(viewHolder.getItemPosition());
+                final File file = files.get(viewHolder.getAdapterPosition());
                 parentView.setCurrentFile(file.toString());
                 parentView.close();
             }
@@ -51,7 +51,6 @@ public class FileSelectionListAdapter extends AbstractRecyclerViewAdapter<FileSe
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        super.onBindViewHolder(holder, position);
         final File file = files.get(position);
 
         final String currentFile = parentView.getCurrentFile();

main/src/cgeo/geocaching/files/GPXListAdapter.java
@@ -2,7 +2,6 @@ package cgeo.geocaching.files;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.ui.dialog.Dialogs;
-import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 import cgeo.geocaching.utils.FileUtils;
 
@@ -19,7 +18,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-class GPXListAdapter extends AbstractRecyclerViewAdapter<GPXListAdapter.ViewHolder> {
+class GPXListAdapter extends RecyclerView.Adapter<GPXListAdapter.ViewHolder> {
     private final GpxFileListActivity activity;
     @NonNull private final List<File> files;
 
@@ -50,7 +49,7 @@ class GPXListAdapter extends AbstractRecyclerViewAdapter<GPXListAdapter.ViewHold
 
             @Override
             public void onClick(final View view) {
-                final File file = files.get(viewHolder.getItemPosition());
+                final File file = files.get(viewHolder.getAdapterPosition());
                 (new GPXImporter(activity, activity.getListId(), null)).importGPX(file);
             }
         });
@@ -58,7 +57,7 @@ class GPXListAdapter extends AbstractRecyclerViewAdapter<GPXListAdapter.ViewHold
 
             @Override
             public boolean onLongClick(final View view) {
-                final File file = files.get(viewHolder.getItemPosition());
+                final File file = files.get(viewHolder.getAdapterPosition());
                 Dialogs.confirmYesNo(activity, R.string.gpx_import_delete_title, activity.getString(R.string.gpx_import_delete_message, file.getName()), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(final DialogInterface dialog, final int id) {
@@ -79,7 +78,6 @@ class GPXListAdapter extends AbstractRecyclerViewAdapter<GPXListAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        super.onBindViewHolder(holder, position);
         final File file = files.get(position);
 
         holder.filepath.setText(file.getParent());

main/src/cgeo/geocaching/helper/HelperAppAdapter.java
@@ -2,12 +2,12 @@ package cgeo.geocaching.helper;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.compatibility.Compatibility;
-import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,7 +20,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-final class HelperAppAdapter extends AbstractRecyclerViewAdapter<HelperAppAdapter.ViewHolder> {
+final class HelperAppAdapter extends RecyclerView.Adapter<HelperAppAdapter.ViewHolder> {
 
     @NonNull private final List<HelperApp> helperApps;
     @NonNull private final HelperAppClickListener clickListener;
@@ -55,7 +55,7 @@ final class HelperAppAdapter extends AbstractRecyclerViewAdapter<HelperAppAdapte
 
             @Override
             public void onClick(final View view) {
-                final HelperApp app = helperApps.get(viewHolder.getItemPosition());
+                final HelperApp app = helperApps.get(viewHolder.getAdapterPosition());
                 clickListener.onClickHelperApp(app);
             }
         });
@@ -65,8 +65,6 @@ final class HelperAppAdapter extends AbstractRecyclerViewAdapter<HelperAppAdapte
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        super.onBindViewHolder(holder, position);
-
         final HelperApp app = helperApps.get(position);
         final Resources resources = context.getResources();
         holder.title.setText(resources.getString(app.titleId));

main/src/cgeo/geocaching/ui/TrackableListAdapter.java
@@ -2,12 +2,12 @@ package cgeo.geocaching.ui;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.models.Trackable;
-import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewAdapter;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 import cgeo.geocaching.utils.TextUtils;
 
 import android.graphics.Paint;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +18,7 @@ import java.util.List;
 
 import butterknife.BindView;
 
-public class TrackableListAdapter extends AbstractRecyclerViewAdapter<TrackableListAdapter.ViewHolder> {
+public class TrackableListAdapter extends RecyclerView.Adapter<TrackableListAdapter.ViewHolder> {
 
     public interface TrackableClickListener {
         void onTrackableClicked(final Trackable trackable);
@@ -54,7 +54,7 @@ public class TrackableListAdapter extends AbstractRecyclerViewAdapter<TrackableL
 
             @Override
             public void onClick(final View view) {
-                trackableClickListener.onTrackableClicked(trackables.get(viewHolder.getItemPosition()));
+                trackableClickListener.onTrackableClicked(trackables.get(viewHolder.getAdapterPosition()));
             }
         });
         return viewHolder;
@@ -62,7 +62,6 @@ public class TrackableListAdapter extends AbstractRecyclerViewAdapter<TrackableL
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        super.onBindViewHolder(holder, position);
         final Trackable trackable = trackables.get(position);
 
         holder.imageBrand.setImageResource(trackable.getIconBrand());

main/src/cgeo/geocaching/ui/recyclerview/AbstractRecyclerViewAdapter.java
@@ -1,19 +1,0 @@
-package cgeo.geocaching.ui.recyclerview;
-
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.Adapter;
-
-/**
- * {@link Adapter} specialization to automatically store the current adapter position as view tag.
- *
- * @param <VH>
- *            View Holder
- */
-public abstract class AbstractRecyclerViewAdapter<VH extends AbstractRecyclerViewHolder> extends RecyclerView.Adapter<VH> {
-
-    @Override
-    public void onBindViewHolder(final VH viewHolder, final int position) {
-        viewHolder.setItemPosition(position);
-    }
-
-}

main/src/cgeo/geocaching/ui/recyclerview/AbstractRecyclerViewHolder.java
@@ -16,15 +16,4 @@ public abstract class AbstractRecyclerViewHolder extends RecyclerView.ViewHolder
         ButterKnife.bind(this, view);
     }
 
-    /**
-     * remember item position at view for retrieval in listeners
-     */
-    protected void setItemPosition(final int position) {
-        itemView.setTag(position);
-    }
-
-    public int getItemPosition() {
-        return (int) itemView.getTag();
-    }
-
 }