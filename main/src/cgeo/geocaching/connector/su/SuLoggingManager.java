7b7477e83219c122cc50348bfaa4b645d8b55312
Oleg Kainov <kainov.oleg@gmail.com>
2018-11-11 22:49:16 +0100

    geocaching.su: use Loader for logging
    Current behavior does not allow to have simple LoggingManager
    without implementing LoaderManager.
    
    See #7188

--- main/src/cgeo/geocaching/connector/su/SuLoggingManager.java
+++ main/src/cgeo/geocaching/connector/su/SuLoggingManager.java
@@ -3,6 +3,7 @@ package cgeo.geocaching.connector.su;
 import cgeo.geocaching.connector.AbstractLoggingManager;
 import cgeo.geocaching.connector.ImageResult;
 import cgeo.geocaching.connector.LogResult;
+import cgeo.geocaching.enumerations.Loaders;
 import cgeo.geocaching.enumerations.StatusCode;
 import cgeo.geocaching.log.LogCacheActivity;
 import cgeo.geocaching.log.LogType;
@@ -12,14 +13,19 @@ import cgeo.geocaching.models.Geocache;
 import cgeo.geocaching.models.Image;
 import cgeo.geocaching.utils.Log;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
-public class SuLoggingManager extends AbstractLoggingManager {
+public class SuLoggingManager extends AbstractLoggingManager implements LoaderManager.LoaderCallbacks<String> {
 
     @NonNull
     private final SuConnector connector;
@@ -36,9 +42,8 @@ public class SuLoggingManager extends AbstractLoggingManager {
 
     @Override
     public void init() {
-        activity.onLoadFinished();
+        activity.getSupportLoaderManager().initLoader(Loaders.LOGGING_GEOCHACHING.getLoaderId(), null, this);
     }
-
 
     @Override
     @NonNull
@@ -63,11 +68,42 @@ public class SuLoggingManager extends AbstractLoggingManager {
         return connector.getPossibleLogTypes(cache);
     }
 
-
     @NonNull
     @Override
     public List<ReportProblemType> getReportProblemTypes(@NonNull final Geocache geocache) {
         return Collections.emptyList();
     }
 
+    @Override
+    public Loader<String> onCreateLoader(final int id, final Bundle args) {
+        activity.onLoadStarted();
+        return new SuLoggingLoader(activity.getBaseContext());
+    }
+
+    @Override
+    public void onLoadFinished(final Loader<String> loader, final String data) {
+        activity.onLoadFinished();
+    }
+
+    @Override
+    public void onLoaderReset(final Loader<String> loader) {
+        // nothing to do
+    }
+
+    static class SuLoggingLoader extends AsyncTaskLoader<String> {
+        SuLoggingLoader(final Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onStartLoading() {
+            forceLoad();
+        }
+
+        public String loadInBackground() {
+            // XXX: We need this dummy Loader because in other case button "Send log" will complain
+            // about "Downloading data in progress"
+            return "nothing to load here";
+        }
+    }
 }
