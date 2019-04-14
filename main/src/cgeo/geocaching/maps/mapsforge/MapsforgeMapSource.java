3f2d2e58aacb7d34adc20a53b68a86eb039170bf
rsudev <rasch@munin-soft.de>
2019-04-14 14:54:32 +0200

    Fix  #7494, Upgrade to mapsforge 0.11.0
    Reference the new version
    Adjust to some small API changes

--- main/src/cgeo/geocaching/maps/mapsforge/MapsforgeMapSource.java
+++ main/src/cgeo/geocaching/maps/mapsforge/MapsforgeMapSource.java
@@ -5,11 +5,10 @@ import cgeo.geocaching.maps.interfaces.MapProvider;
 import cgeo.geocaching.maps.mapsforge.v6.layers.DownloadLayer;
 import cgeo.geocaching.maps.mapsforge.v6.layers.ITileLayer;
 
-
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
 import org.mapsforge.map.layer.cache.TileCache;
 import org.mapsforge.map.layer.download.tilesource.OpenStreetMapMapnik;
-import org.mapsforge.map.model.MapViewPosition;
+import org.mapsforge.map.model.IMapViewPosition;
 import org.mapsforge.v3.android.maps.mapgenerator.MapGeneratorInternal;
 
 public class MapsforgeMapSource extends AbstractMapSource {
@@ -25,7 +24,7 @@ public class MapsforgeMapSource extends AbstractMapSource {
         return generator;
     }
 
-    public ITileLayer createTileLayer(final TileCache tileCache, final MapViewPosition mapViewPosition) {
+    public ITileLayer createTileLayer(final TileCache tileCache, final IMapViewPosition mapViewPosition) {
         return new DownloadLayer(tileCache, mapViewPosition, OpenStreetMapMapnik.INSTANCE, AndroidGraphicFactory.INSTANCE);
     }
 
