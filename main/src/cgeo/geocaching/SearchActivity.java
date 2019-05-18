294748e93a84254b8cd60e724d5b9f256e4967b6
moving-bits <github@moving-bits.net>
2019-05-18 17:52:17 +0200

    temporaribly disable check for active trackable connectors due to #7617 (#7624)
    temporaribly disable check for active trackable connectors due to #7617

--- main/src/cgeo/geocaching/SearchActivity.java
+++ main/src/cgeo/geocaching/SearchActivity.java
@@ -412,13 +412,16 @@ public class SearchActivity extends AbstractActionBarActivity implements Coordin
             return;
         }
 
-        if (ConnectorFactory.anyTrackableConnectorActive()) {
+        // check temporaribly disabled due to #7617
+        // if (ConnectorFactory.anyTrackableConnectorActive()) {
             final Intent trackablesIntent = new Intent(this, TrackableActivity.class);
             trackablesIntent.putExtra(Intents.EXTRA_GEOCODE, trackableText.toUpperCase(Locale.US));
             startActivity(trackablesIntent);
+        /*
         } else {
             showToast(getString(R.string.warn_no_connector));
         }
+        */
     }
 
     @Override
