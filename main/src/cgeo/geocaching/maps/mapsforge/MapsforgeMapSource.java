edbbf95de6ece9b142e28e59085319d8f3a9bdc7
Lars <gc@droescher.eu>
2019-05-29 11:47:37 +0200

    Use mapsforge 0.10.0

--- main/src/cgeo/geocaching/maps/mapsforge/MapsforgeMapSource.java
+++ main/src/cgeo/geocaching/maps/mapsforge/MapsforgeMapSource.java
@@ -9,7 +9,7 @@ import cgeo.geocaching.maps.mapsforge.v6.layers.ITileLayer;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
 import org.mapsforge.map.layer.cache.TileCache;
 import org.mapsforge.map.layer.download.tilesource.OpenStreetMapMapnik;
-import org.mapsforge.map.model.MapViewPosition;
+import org.mapsforge.map.model.IMapViewPosition;
 import org.mapsforge.v3.android.maps.mapgenerator.MapGeneratorInternal;
 
 public class MapsforgeMapSource extends AbstractMapSource {
@@ -25,7 +25,7 @@ public class MapsforgeMapSource extends AbstractMapSource {
         return generator;
     }
 
-    public ITileLayer createTileLayer(final TileCache tileCache, final MapViewPosition mapViewPosition) {
+    public ITileLayer createTileLayer(final TileCache tileCache, final IMapViewPosition mapViewPosition) {
         return new DownloadLayer(tileCache, mapViewPosition, OpenStreetMapMapnik.INSTANCE, AndroidGraphicFactory.INSTANCE);
     }
 
