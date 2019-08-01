99f1fd3797dbcf890b136a3d6e3c4f63b9a2eac2
moving-bits <github@moving-bits.net>
2019-08-01 21:13:19 +0200

    Make seekbar value editable (opens input dialog on touch) (fix #7726) (#7747)
    * Make seekbar value editable (opens input dialog on touch)
    Add @Override where applicable
    
    * higher resolution for AbstractProximityPreference
    
    * avoid creating intermediate object

--- main/src/cgeo/geocaching/settings/AbstractProximityPreference.java
+++ main/src/cgeo/geocaching/settings/AbstractProximityPreference.java
@@ -8,8 +8,6 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.Locale;
-
 public abstract class AbstractProximityPreference extends AbstractSeekbarPreference {
     private int maxSeekbarLength = 0;   // initialized in onCreateView
     private boolean farDistance = true;
@@ -19,28 +17,39 @@ public abstract class AbstractProximityPreference extends AbstractSeekbarPrefere
         this.farDistance = farDistance;
     }
 
-    private int valueToProgressHelper(final int value) {
-        return (int) Math.round(25.0 * Math.log10(value + 1));
+    @Override
+    protected int valueToProgressHelper(final int value) {
+        return (int) Math.round(250.0 * Math.log10(value + 1));
     }
 
-    private int valueToProgress(final int value) {
+    @Override
+    protected int valueToProgress(final int value) {
         final int progress = valueToProgressHelper(value);
         return progress < 0 ? 0 : progress > maxSeekbarLength ? maxSeekbarLength : progress;
     }
 
-    private int progressToValue(final int progress) {
-        final int value = (int) Math.pow(10, Double.valueOf(progress) / 25.0) - 1;
+    @Override
+    protected int progressToValue(final int progress) {
+        final int value = (int) Math.pow(10, Double.valueOf(progress) / 250.0) - 1;
         return value < 0 ? 0 : value > Settings.PROXIMITY_NOTIFICATION_MAX_DISTANCE ? Settings.PROXIMITY_NOTIFICATION_MAX_DISTANCE : value;
     }
 
-    protected String getValueString(final int progress) {
-        final int value = progressToValue(progress);
-        return Settings.useImperialUnits()
-            ? String.format(Locale.US, "%.2f mi", value / (1000 * IConversion.MILES_TO_KILOMETER))
-            : value + " m"
-        ;
+    @Override
+    protected String valueToShownValue(final int value) {
+        return Settings.useImperialUnits() ? String.format("%.2f", value / (1000 * IConversion.MILES_TO_KILOMETER)) : String.valueOf(value);
     }
 
+    @Override
+    protected int shownValueToValue(final float shownValue) {
+        return Math.round(Settings.useImperialUnits() ? shownValue * 1000 * IConversion.MILES_TO_KILOMETER : shownValue);
+    }
+
+    @Override
+    protected String getValueString(final int progress) {
+        return valueToShownValue(progressToValue(progress)) + (Settings.useImperialUnits() ? " mi" : " m");
+    }
+
+    @Override
     protected void saveSetting(final int progress) {
         Settings.setProximityNotificationThreshold(farDistance, progressToValue(progress));
     }
@@ -48,7 +57,7 @@ public abstract class AbstractProximityPreference extends AbstractSeekbarPrefere
     @Override
     protected View onCreateView(final ViewGroup parent) {
         maxSeekbarLength = valueToProgressHelper(Settings.PROXIMITY_NOTIFICATION_MAX_DISTANCE);
-        configure(1, maxSeekbarLength, valueToProgress(Settings.getProximityNotificationThreshold(farDistance)), farDistance ? getContext().getString(R.string.far_distance) : getContext().getString(R.string.near_distance));
+        configure(1, maxSeekbarLength, valueToProgress(Settings.getProximityNotificationThreshold(farDistance)), farDistance ? getContext().getString(R.string.far_distance) : getContext().getString(R.string.near_distance), Settings.useImperialUnits());
         return super.onCreateView(parent);
     }
 }
