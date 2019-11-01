9be6e58fcca8baf99fcc7362b2c019dda354d2ea
moving-bits <github@moving-bits.net>
2019-11-01 09:34:30 +0100

    Revert "respect isMapDirection() setting for GMv2 (fix #7908)" (#7925)
    This reverts commit 7fb166f502b576275bd3646e43daeaac5ae290e0.

--- main/src/cgeo/geocaching/maps/google/v2/GooglePositionAndHistory.java
+++ main/src/cgeo/geocaching/maps/google/v2/GooglePositionAndHistory.java
@@ -168,7 +168,7 @@ public class GooglePositionAndHistory implements PositionAndHistory {
         );
 
         final Geopoint destCoords = mapView.getDestinationCoords();
-        if (destCoords != null && Settings.isMapDirection()) {
+        if (destCoords != null) {
             // draw direction line
             positionObjs.addPolyline(getDirectionPolyline(new Geopoint(coordinates), destCoords));
         }
