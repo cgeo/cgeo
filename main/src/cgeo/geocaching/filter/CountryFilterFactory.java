a78097ebb7da660963aad7fabc66482ea1bb498b
JakeDot <dev@jakedot.net>
2020-05-02 23:38:57 +0200

    Get sorted countries from DataStore, clean up CountryFilter

--- main/src/cgeo/geocaching/filter/CountryFilterFactory.java
+++ main/src/cgeo/geocaching/filter/CountryFilterFactory.java
@@ -8,20 +8,28 @@ import cgeo.geocaching.storage.DataStore;
 import android.os.Parcel;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class CountryFilterFactory implements IFilterFactory {
 
+    private static String getDisplayName(final @NonNull String country) {
+        if (country.isEmpty()) {
+            return CgeoApplication.getInstance().getString(R.string.caches_filter_empty);
+        } else {
+            return country;
+        }
+    }
 
     static class CountryFilter extends AbstractFilter {
-        private String location = null;
+        private @NonNull String country;
 
-        public static final Creator<CountryFilter> CREATOR
-                = new Creator<CountryFilter>() {
+        public static final Creator<CountryFilter> CREATOR = new Creator<CountryFilter>() {
 
             @Override
             public CountryFilter createFromParcel(final Parcel in) {
@@ -34,56 +42,64 @@ public class CountryFilterFactory implements IFilterFactory {
             }
         };
 
-        CountryFilter(final String location, final String title) {
-            super(title);
-            this.location = location;
+        CountryFilter(final String country) {
+            super(getDisplayName(country));
+            this.country = Objects.requireNonNull(country);
         }
 
-        protected CountryFilter(final Parcel in) {
+        CountryFilter(final Parcel in) {
             super(in);
-            location = in.readString();
+            country = Objects.requireNonNull(in.readString());
         }
 
         @Override
         public void writeToParcel(final Parcel dest, final int flags) {
             super.writeToParcel(dest, flags);
-            dest.writeString(location);
+            dest.writeString(country);
         }
 
         @Override
-        public boolean accepts(@NonNull final Geocache cache) {
+        public boolean accepts(final @NonNull Geocache cache) {
             final @NonNull String location = cache.getLocation();
 
-            if (this.location.isEmpty()) {
-                return null == location || location.isEmpty();
+            if (this.country.isEmpty()) {
+                return location.isEmpty();
             } else {
-                return null != location && location.endsWith(this.location);
+                return location.endsWith(this.country);
             }
         }
 
+        @Override
+        public int hashCode() {
+            return country.hashCode();
+        }
+
+        @Override
+        public boolean equals(final @Nullable Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (obj == this) {
+                return true;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            return country.equals(((CountryFilter) obj).country);
+        }
     }
 
     @Override
     @NonNull
     public List<IFilter> getFilters() {
 
-        final Map<String, IFilter> filters = new HashMap<>();
+        final Set<CountryFilter> filters = new LinkedHashSet<>();
 
-        final String separator = ", ";
-
-        // TODO: use DataStore.getStoredLocationsForSet(Set<String>)
-        for (final String location : DataStore.getAllStoredLocations()) {
-
-            final int indexOfSeparator = location.lastIndexOf(separator);
-            final int subStringIndex = indexOfSeparator == -1 ? 0 : indexOfSeparator + separator.length();
-
-            final String countryKey = location.substring(subStringIndex);
-            if (!filters.containsKey(countryKey)) {
-                filters.put(countryKey, new CountryFilter(countryKey, countryKey.isEmpty() ? CgeoApplication.getInstance().getString(R.string.caches_filter_empty) : countryKey));
-            }
+        for (final String country : DataStore.getAllStoredCountries()) {
+            filters.add(new CountryFilter(country));
         }
 
-        return new ArrayList<>(filters.values());
+        return new ArrayList<>(filters);
     }
 
 }
