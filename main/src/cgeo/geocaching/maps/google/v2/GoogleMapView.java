6a06effc40664ca53af6b2422f83203d8efcd43f
Sebastian Buchwald <UniQP@web.de>
2020-05-04 19:17:43 +0200

    Remove superfluous semicolons

--- main/src/cgeo/geocaching/maps/google/v2/GoogleMapView.java
+++ main/src/cgeo/geocaching/maps/google/v2/GoogleMapView.java
@@ -282,7 +282,7 @@ public class GoogleMapView extends MapView implements MapViewImpl<GoogleCacheOve
     public void zoomToBounds(final Viewport bounds, final GeoPointImpl center) {
         mapController.zoomToSpan(bounds.topRight.getLatitudeE6() - bounds.bottomLeft.getLatitudeE6(), bounds.topRight.getLongitudeE6() - bounds.bottomLeft.getLongitudeE6());
         mapController.animateTo(center);
-    };
+    }
 
     @Override
     public void setMapSource() {
