1aac9ea9367eebad54cb72fda6e993fe40ee96fd
moving-bits <github@moving-bits.net>
2020-08-02 12:08:28 +0200

    remove duplicated calls to clearIndividualRoute

--- main/src/cgeo/geocaching/maps/mapsforge/v6/NewMap.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v6/NewMap.java
@@ -480,9 +480,6 @@ public class NewMap extends AbstractActionBarActivity implements XmlRenderThemeM
                 new TrailHistoryExport(this, this::clearTrailHistory);
                 return true;
             }
-            case R.id.menu_clear_individual_route:
-                clearIndividualRoute();
-                return true;
             case R.id.menu_hint:
                 menuShowHint();
                 return true;
