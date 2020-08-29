f68426582ad41ebfaa6271ddf2611268d4a36713
moving-bits <github@moving-bits.net>
2020-08-29 09:20:13 +0200

    add Chirp Wolf to useful apps list (fix #8883)

--- main/src/cgeo/geocaching/helper/UsefulAppsActivity.java
+++ main/src/cgeo/geocaching/helper/UsefulAppsActivity.java
@@ -26,6 +26,7 @@ public final class UsefulAppsActivity extends AbstractActionBarActivity {
             new HelperApp(R.string.helper_bluetoothgps_title, R.string.helper_bluetoothgps_description, R.drawable.helper_bluetoothgps, "googoo.android.btgps"),
             new HelperApp(R.string.helper_gps_locker_title, R.string.helper_gps_locker_description, R.drawable.helper_gps_locker, "com.silentlexx.gpslock"),
             new HelperApp(R.string.helper_barcode_title, R.string.helper_barcode_description, R.drawable.helper_barcode, "com.google.zxing.client.android"),
+            new HelperApp(R.string.helper_chirpwolf, R.string.helper_chirpwolf_description, R.drawable.helper_chirpwolf, "de.wolffire.chirpwolf"),
             new HelperApp(R.string.helper_locus_title, R.string.helper_locus_description, R.drawable.helper_locus, "menion.android.locus"),
     };
 
