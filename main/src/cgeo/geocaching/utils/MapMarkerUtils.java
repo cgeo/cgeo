3f8717262495de09fdf145565731adb666b92d4a
moving-bits <github@moving-bits.net>
2020-09-21 22:11:52 +0200

    show floppy symbol instead of text offline in cache lists

--- main/src/cgeo/geocaching/utils/MapMarkerUtils.java
+++ main/src/cgeo/geocaching/utils/MapMarkerUtils.java
@@ -413,7 +413,7 @@ public final class MapMarkerUtils {
      *         True if the floppy overlay should be displayed
      */
     private static boolean showFloppyOverlay(@Nullable final CacheListType cacheListType) {
-        return cacheListType != CacheListType.OFFLINE; // also covers null check
+        return cacheListType != null;
     }
 
     private static void readLists() {
