36724bdfdbb3a9d2d50a40582e728d6ec84f3e82
rsudev <rasch@munin-soft.de>
2020-09-24 13:20:12 +0200

    Test: Remove zoom-level adaption mf<->google

--- main/src/cgeo/geocaching/maps/mapsforge/v6/MfMapView.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v6/MfMapView.java
@@ -82,11 +82,11 @@ public class MfMapView extends MapView {
     }
 
     public int getMapZoomLevel() {
-        return getModel().mapViewPosition.getZoomLevel() + 3;
+        return getModel().mapViewPosition.getZoomLevel() /* + 3 */;
     }
 
     public void setMapZoomLevel(final int zoomLevel) {
-        getModel().mapViewPosition.setZoomLevel((byte) (zoomLevel - 3));
+        getModel().mapViewPosition.setZoomLevel((byte) (zoomLevel /* - 3 */));
     }
 
     public void zoomToViewport(final Viewport viewport) {
