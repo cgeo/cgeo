387a99a0e88303e581e7080a0f30fdf027af8e5e
eddiemuc <andreas-kopp@gmx.net>
2020-09-28 20:27:33 +0200

    related to #8978: fixing non-GUI-parts (img delete, remove exif, move img fct to helper)

--- main/src/cgeo/geocaching/ImageSelectActivity.java
+++ main/src/cgeo/geocaching/ImageSelectActivity.java
@@ -3,38 +3,27 @@ package cgeo.geocaching;
 import cgeo.geocaching.activity.AbstractActionBarActivity;
 import cgeo.geocaching.models.Image;
 import cgeo.geocaching.settings.Settings;
+import cgeo.geocaching.ui.ImageActivityHelper;
 import cgeo.geocaching.ui.TextSpinner;
 import cgeo.geocaching.ui.dialog.Dialogs;
 import cgeo.geocaching.utils.ImageUtils;
-import cgeo.geocaching.utils.Log;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Arrays;
 
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
@@ -52,15 +41,15 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
     private final TextSpinner<Integer> imageScale = new TextSpinner<>();
 
     private static final String SAVED_STATE_IMAGE = "cgeo.geocaching.saved_state_image";
+    private static final String SAVED_STATE_ORIGINAL_IMAGE = "cgeo.geocaching.saved_state_original_image";
     private static final String SAVED_STATE_IMAGE_INDEX = "cgeo.geocaching.saved_state_image_index";
     private static final String SAVED_STATE_IMAGE_SCALE = "cgeo.geocaching.saved_state_image_scale";
     private static final String SAVED_STATE_MAX_IMAGE_UPLOAD_SIZE = "cgeo.geocaching.saved_state_max_image_upload_size";
     private static final String SAVED_STATE_IMAGE_CAPTION_MANDATORY = "cgeo.geocaching.saved_state_image_caption_mandatory";
 
-    private static final int SELECT_NEW_IMAGE = 1;
-    private static final int SELECT_STORED_IMAGE = 2;
+    private final ImageActivityHelper imageActivityHelper = new ImageActivityHelper(this, 1);
 
-    // Data to be saved while reconfiguring
+    private Image originalImage;
     private Image image;
     private int imageIndex = -1;
     private long maxImageUploadSize;
@@ -75,12 +64,13 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
                 .setDisplayMapper(scaleSize -> scaleSize < 0 ? getResources().getString(R.string.log_image_scale_option_noscaling) : getResources().getString(R.string.log_image_scale_option_entry, scaleSize))
                 .setValues(Arrays.asList(ArrayUtils.toObject(getResources().getIntArray(R.array.log_image_scale_values))))
                 .set(Settings.getLogImageScale())
-                .setChangeListener(scaleSize -> Settings.setLogImageScale(scaleSize));
+                .setChangeListener(Settings::setLogImageScale);
 
         // Get parameters from intent and basic cache information from database
         final Bundle extras = getIntent().getExtras();
         if (extras != null) {
             image = extras.getParcelable(Intents.EXTRA_IMAGE);
+            originalImage = image;
             imageIndex = extras.getInt(Intents.EXTRA_INDEX, -1);
             //imageScale.set(extras.getInt(Intents.EXTRA_SCALE, imageScale.get()));
             maxImageUploadSize = extras.getLong(Intents.EXTRA_MAX_IMAGE_UPLOAD_SIZE);
@@ -92,6 +82,7 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
         // Restore previous state
         if (savedInstanceState != null) {
             image = savedInstanceState.getParcelable(SAVED_STATE_IMAGE);
+            originalImage = savedInstanceState.getParcelable(SAVED_STATE_ORIGINAL_IMAGE);
             imageIndex = savedInstanceState.getInt(SAVED_STATE_IMAGE_INDEX, -1);
             imageScale.set(savedInstanceState.getInt(SAVED_STATE_IMAGE_SCALE));
             maxImageUploadSize = savedInstanceState.getLong(SAVED_STATE_MAX_IMAGE_UPLOAD_SIZE);
@@ -128,6 +119,7 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
         super.onSaveInstanceState(outState);
         syncEditTexts();
         outState.putParcelable(SAVED_STATE_IMAGE, image);
+        outState.putParcelable(SAVED_STATE_ORIGINAL_IMAGE, originalImage);
         outState.putInt(SAVED_STATE_IMAGE_INDEX, imageIndex);
         outState.putInt(SAVED_STATE_IMAGE_SCALE, imageScale.get());
         outState.putLong(SAVED_STATE_MAX_IMAGE_UPLOAD_SIZE, maxImageUploadSize);
@@ -139,7 +131,11 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
             new AsyncTask<Void, Void, ImageUtils.ScaleImageResult>() {
                 @Override
                 protected ImageUtils.ScaleImageResult doInBackground(final Void... params) {
-                    return writeScaledImage(image.getPath());
+                    final int maxXY = imageScale.get();
+                    if (image.getPath() == null) {
+                        return null;
+                    }
+                    return ImageUtils.readScaleAndWriteImage(image.getPath(), maxXY, true, true);
                 }
 
                 @Override
@@ -167,6 +163,7 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
                         intent.putExtra(Intents.EXTRA_IMAGE, image);
                         intent.putExtra(Intents.EXTRA_INDEX, imageIndex);
                         intent.putExtra(Intents.EXTRA_SCALE, imageScale.get());
+                        //"originalImage" is now obsolete. But we never delete originalImage (in case log gets not stored)
                         setResult(RESULT_OK, intent);
                     } else {
                         showToast(res.getString(R.string.err_select_logimage_failed));
@@ -180,8 +177,11 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
             intent.putExtra(Intents.EXTRA_DELETE_FLAG, true);
             intent.putExtra(Intents.EXTRA_INDEX, imageIndex);
             setResult(RESULT_OK, intent);
+            deleteImageFromDeviceIfNotOriginal(image);
+            //original image is now obsolete. BUt we never delete original Image (in case log gets not stored)
             finish();
         } else {
+            deleteImageFromDeviceIfNotOriginal(image);
             setResult(RESULT_CANCELED);
             finish();
         }
@@ -196,129 +196,36 @@ public class ImageSelectActivity extends AbstractActionBarActivity {
     }
 
     private void selectImageFromCamera() {
-        final File file = ImageUtils.getOutputImageFile();
-        if (file == null) {
-            showFailure();
-            return;
-        }
 
-        image = new Image.Builder().setUrl(Uri.fromFile(file)).build();
-        if (image.isEmpty()) {
-            showFailure();
-            return;
-        }
-
-        // create Intent to take a picture and return control to the calling application
-        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        final Context context = getApplicationContext();
-        final Uri uri = FileProvider.getUriForFile(
-                context,
-                context.getString(R.string.file_provider_authority),
-                file);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri); // set the image file name
-
-        // start the image capture Intent
-        startActivityForResult(intent, SELECT_NEW_IMAGE);
+        imageActivityHelper.getImageFromCamera(-1, false, false, img -> {
+            deleteImageFromDeviceIfNotOriginal(image);
+            image = img;
+            loadImagePreview();
+        });
     }
 
     private void selectImageFromStorage() {
-        final Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
+        imageActivityHelper.getImageFromStorage(-1, false, false,  img -> {
+            deleteImageFromDeviceIfNotOriginal(image);
+            image = img;
+            loadImagePreview();
+        });
+    }
 
-        startActivityForResult(Intent.createChooser(intent, "Select Image"), SELECT_STORED_IMAGE);
+    private boolean deleteImageFromDeviceIfNotOriginal(final Image img) {
+        if (img != null &&  !img.getPath().isEmpty() && (originalImage == null || !img.getPath().equals(originalImage.getPath()))) {
+            return new File(img.getPath()).delete();
+        }
+        return false;
     }
 
     @Override
     protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
         super.onActivityResult(requestCode, resultCode, data);  // call super to make lint happy
-        if (resultCode == RESULT_CANCELED) {
-            // User cancelled the image capture
-            showToast(getString(R.string.info_select_logimage_cancelled));
-            return;
-        }
-
-        if (resultCode != RESULT_OK) {
-            // Image capture failed, advise user
-            showFailure();
-            return;
-        }
-
-        // null is an acceptable result if the image has been placed in the imageUri file by the
-        // camera application.
-        if (data != null && data.getData() != null) {
-            final Uri selectedImage = data.getData();
-            final String mimeType = getContentResolver().getType(selectedImage);
-            if (!("image/jpeg".equals(mimeType) || "image/png".equals(mimeType) || "image/gif".equals(mimeType))) {
-                showToast(getString(R.string.err_unsupported_image_format));
-                return;
-            }
-            InputStream input = null;
-            OutputStream output = null;
-            try {
-                input = getContentResolver().openInputStream(selectedImage);
-                final File outputFile = ImageUtils.getOutputImageFile();
-                if (outputFile != null) {
-                    output = new FileOutputStream(outputFile);
-                    IOUtils.copy(input, output);
-                    image = new Image.Builder().setUrl(outputFile).build();
-                }
-            } catch (final IOException e) {
-                Log.e("ImageSelectActivity.onActivityResult", e);
-            } finally {
-                IOUtils.closeQuietly(input);
-                IOUtils.closeQuietly(output);
-            }
-        } else {
-            // Image capture failed, advise user
-            showFailure();
-            return;
-        }
-
-        if (requestCode == SELECT_NEW_IMAGE) {
-            showToast(getString(R.string.info_stored_image) + '\n' + image.getUrl());
-        }
-
-        loadImagePreview();
-    }
-
-    /**
-     * Scales and writes the scaled image.
-     *
-     * @return the scaled image result, or <tt>null</tt> if the image cannot be decoded
-     */
-    @Nullable
-    private ImageUtils.ScaleImageResult writeScaledImage(@Nullable final String filePath) {
-        if (filePath == null) {
-            return null;
-        }
-        final int maxXY = imageScale.get(); //getResources().getIntArray(R.array.log_image_scale_values)[scaleChoiceIndex];
-        return ImageUtils.readScaleAndWriteImage(filePath, maxXY);
-    }
-
-    private void showFailure() {
-        showToast(getString(R.string.err_acquire_image_failed));
-    }
+        imageActivityHelper.onActivityResult(requestCode, resultCode, data);
+   }
 
     private void loadImagePreview() {
-        if (image.isEmpty()) {
-            return;
-        }
-        if (!image.existsLocal()) {
-            Log.i("Image does not exist");
-            return;
-        }
-
-        new AsyncTask<Void, Void, Bitmap>() {
-            @Override
-            protected Bitmap doInBackground(final Void... params) {
-                return ImageUtils.readAndScaleImageToFitDisplay(image.getPath());
-            }
-
-            @Override
-            protected void onPostExecute(final Bitmap bitmap) {
-                imagePreview.setImageBitmap(bitmap);
-                imagePreview.setVisibility(View.VISIBLE);
-            }
-        }.execute();
+        ImageActivityHelper.displayImageAsync(image, imagePreview);
     }
 }
