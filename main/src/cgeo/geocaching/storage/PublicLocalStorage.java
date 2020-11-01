95a808b5f7d78b730170f983844623c00917f798
Eddie <andreas-kopp@gmx.net>
2020-11-01 18:01:47 +0100

    write draft for publiclocalstorage stuff (#9331)

--- main/src/cgeo/geocaching/storage/PublicLocalStorage.java
+++ main/src/cgeo/geocaching/storage/PublicLocalStorage.java
@@ -1,9 +1,9 @@
 package cgeo.geocaching.storage;
 
-import cgeo.geocaching.settings.Settings;
 import cgeo.geocaching.utils.Log;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
@@ -11,6 +11,7 @@ import android.content.UriPermission;
 import android.net.Uri;
 import android.provider.DocumentsContract;
 
+import androidx.core.util.Consumer;
 import androidx.documentfile.provider.DocumentFile;
 
 import java.io.IOException;
@@ -22,26 +23,83 @@ import org.apache.commons.io.IOUtils;
 //Hackish code, trying out a few things
 public class PublicLocalStorage {
 
-    public static final int REQUEST_CODE = -582;
+    public static final int REQUEST_CODE_GRANT_URI_ACCESS = -582;
 
     private final Context context;
     private final ContentResolver contentResolver;
 
-    private Uri baseUri;
+    private IntentData runningIntentData;
+
+    private static class IntentData {
+        public final PublicLocalFolder folder;
+        public final Consumer<PublicLocalFolder> callback;
+
+        IntentData(final PublicLocalFolder folder, final Consumer<PublicLocalFolder> callback) {
+            this.folder = folder;
+            this.callback = callback;
+        }
+    }
 
     public PublicLocalStorage(final Context context) {
         this.context = context;
         this.contentResolver = context.getContentResolver();
     }
 
+    /**
+     * Checks and (if necessary) asks for grant of a read/write permission for a folder. Note that asking for grants is only possible
+     * when the context associated with this instance is an Activity AND in this Activities {@link Activity#onActivityResult(int, int, Intent)} method
+     * does call {@link #onActivityResult(int, int, Intent)}
+     * @param folder folder to ask/grant access for
+     * @param requestGrantIfNecessary if true and folder does NOT have necessary grants, then these grants are requested
+     * @param callback if grants are requested, then this callback is called when request is granted
+     * @return true if grant is already available, false otherwise
+     */
+
+    public boolean checkAndGrantFolderAccess(final PublicLocalFolder folder, final boolean requestGrantIfNecessary, final Consumer<PublicLocalFolder> callback) {
+
+        if (checkUriPermissions(folder.getBaseUri(), folder.needsWrite())) {
+            return true;
+        }
+        if (requestGrantIfNecessary) {
+            if (!(this.context instanceof Activity)) {
+                //TODO: do something better
+                throw new IllegalArgumentException("Programming error: context MUST be activity to start intent!");
+            }
+            //TODO: internationalize, cleanup, ...
+            new AlertDialog.Builder(this.context)
+                .setTitle("Select and grant access to folder")
+                .setMessage("Please select and grant read/write access to parent folder for subfolder '" + folder.getFolderName() + "'")
+                .setPositiveButton("OK", (d, p) -> {
+                    // call for document tree dialog
+                    final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                    intent.addFlags(
+                        (folder.needsWrite() ? Intent.FLAG_GRANT_WRITE_URI_PERMISSION : Intent.FLAG_GRANT_READ_URI_PERMISSION) |
+                            Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
+                    Log.e("Start uri dir: " + folder.getBaseUri());
+                    intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, folder.getBaseUri());
+
+                    runningIntentData = new IntentData(folder, callback);
+                    ((Activity) this.context).startActivityForResult(intent, REQUEST_CODE_GRANT_URI_ACCESS);
+                }).create().show();
+        }
+        return false;
+    }
+
+    /** include in {@link android.app.Activity#onActivityResult(int, int, android.content.Intent)} of using Activity */
     public boolean onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
-        if (requestCode == REQUEST_CODE) {
+        if (requestCode == REQUEST_CODE_GRANT_URI_ACCESS) {
             if (resultCode == Activity.RESULT_OK && intent != null) {
                 final Uri uri = intent.getData();
                 if (uri != null) {
                     contentResolver.takePersistableUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                     Log.e("permissions: " + uri.getPath());
-                    Settings.setBaseDir(uri);
+                    if (runningIntentData != null) {
+                        runningIntentData.folder.setBaseUri(uri);
+                        if (runningIntentData.callback != null) {
+                            runningIntentData.callback.accept(runningIntentData.folder);
+                        }
+                    }
+                    runningIntentData = null;
                 }
             }
             return true;
@@ -49,17 +107,18 @@ public class PublicLocalStorage {
         return false;
     }
 
-    private Uri getFolderUri(final LocalFolder folder) {
-        if (baseUri == null) {
-            baseUri = Settings.getBaseDir();
-        }
-        if (baseUri == null || !takeUriPermissions(baseUri, true, true)) {
+    private Uri getFolderUri(final PublicLocalFolder folder) {
+        final Uri baseUri = folder.getBaseUri();
+        if (baseUri == null || !checkUriPermissions(baseUri, folder.needsWrite())) {
             return null;
         }
 
         final DocumentFile cgeoDir = DocumentFile.fromTreeUri(context, baseUri);
         if (!cgeoDir.isDirectory()) {
             return null;
+        }
+        if (folder.getFolderName() == null) {
+            return cgeoDir.getUri();
         }
         for (DocumentFile child : cgeoDir.listFiles()) {
             if (child.getName().equals(folder.getFolderName())) {
@@ -72,27 +131,23 @@ public class PublicLocalStorage {
         return cgeoDir.createDirectory(folder.getFolderName()).getUri();
     }
 
-    private boolean takeUriPermissions(final Uri uri, final boolean checkRead, final boolean checkWrite) {
+    private boolean checkUriPermissions(final Uri uri, final boolean checkWrite) {
         for (UriPermission up : contentResolver.getPersistedUriPermissions()) {
             if (up.getUri().equals(uri)) {
-                final boolean hasAdequateRights = (!checkRead || up.isReadPermission()) && (!checkWrite || up.isWritePermission());
+                final boolean hasAdequateRights = (up.isReadPermission()) && (!checkWrite || up.isWritePermission());
                 if (!hasAdequateRights) {
                     return false;
                 }
-                final int flags = (checkRead ? Intent.FLAG_GRANT_READ_URI_PERMISSION : 0) | (checkWrite ? Intent.FLAG_GRANT_WRITE_URI_PERMISSION : 0);
+                final int flags = Intent.FLAG_GRANT_READ_URI_PERMISSION | (checkWrite ? Intent.FLAG_GRANT_WRITE_URI_PERMISSION : 0);
                 contentResolver.takePersistableUriPermission(uri, flags);
                 return true;
             }
         }
-        //MediaStore.Images.Media.INTERNAL_CONTENT_URI
         return false;
     }
 
-    public boolean hasUriPermission(final Uri uri, final boolean checkRead, final boolean checkWrite) {
-        return false;
-    }
-
-    public boolean writeToStorage(final LocalFolder folder, final String name, final InputStream in) {
+    /** Write something to external storage */
+    public boolean writeToStorage(final PublicLocalFolder folder, final String name, final InputStream in) {
         OutputStream out = null;
         try {
             final Uri folderUri = getFolderUri(folder);
@@ -108,5 +163,6 @@ public class PublicLocalStorage {
         return true;
     }
 
+    //TODO: methods for reading file lists and single files
 
 }
