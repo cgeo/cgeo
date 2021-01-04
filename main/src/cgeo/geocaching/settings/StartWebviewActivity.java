afb495ed1681b60ba44e7cef1a668de104f0e426
Florian <64581222+fm-sys@users.noreply.github.com>
2021-01-04 14:16:44 +0100

    [open in browser] avoid loop if c:geo is set as default application

---
+++ main/src/cgeo/geocaching/settings/StartWebviewActivity.java
@@ -0,0 +1,48 @@
+package cgeo.geocaching.settings;
+
+import cgeo.geocaching.utils.ShareUtils;
+import static cgeo.geocaching.utils.ProcessUtils.isChromeLaunchable;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class StartWebviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final String url = getIntent().getDataString();
+        if (url != null) {
+            if (isChromeLaunchable()) {
+                ShareUtils.openCustomTab(this, url);
+            } else {
+
+                final Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse("https://example.com"));
+
+                final ResolveInfo resolveInfo = getPackageManager().queryIntentActivities(browserIntent, PackageManager.MATCH_DEFAULT_ONLY).get(0);
+
+                final Intent launchIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                launchIntent.setPackage(resolveInfo.activityInfo.packageName);
+
+                startActivity(launchIntent);
+            }
+        }
+
+        finish();
+
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        finish();
+    }
+}
