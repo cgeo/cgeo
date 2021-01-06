693d404afb528d33e038a16f218a9cb5dba8b415
moving-bits <github@moving-bits.net>
2021-01-06 12:49:17 +0100

    added undo function for set cache icon for list

--- main/src/cgeo/geocaching/command/SetCacheIconCommand.java
+++ main/src/cgeo/geocaching/command/SetCacheIconCommand.java
@@ -10,14 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 public abstract class SetCacheIconCommand extends AbstractCachesCommand {
 
     private final int newCacheIcon;
+    private final HashMap<String, Integer> undo;
 
     protected SetCacheIconCommand(@NonNull final Activity context, @NonNull final Collection<Geocache> caches, final int newCacheIcon) {
         super(context, caches, R.string.command_set_cache_icons_progress);
         this.newCacheIcon = newCacheIcon;
+        final int size = caches.size();
+        this.undo = new HashMap<>(size);
+        for (final Geocache cache : caches) {
+            this.undo.put(cache.getGeocode(), cache.getAssignedEmoji());
+        }
     }
 
     @Override
@@ -33,12 +40,13 @@ public abstract class SetCacheIconCommand extends AbstractCachesCommand {
 
     @Override
     protected void undoCommand() {
-        // no undo available
-    }
+        DataStore.setCacheIcons(getCaches(), this.undo);
+     }
 
     @Override
     @Nullable
     protected String getResultMessage() {
-        return null;
+        final int size = getCaches().size();
+        return getContext().getResources().getQuantityString(R.plurals.command_set_cache_icons_result, size, size);
     }
 }
