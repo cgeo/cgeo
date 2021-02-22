4e848b6657bfc0b502ff7707dd1f1a8b6f00b598
moving-bits <github@moving-bits.net>
2021-02-22 21:44:42 +0100

    offer to download missing FZK theme after successful FZK map download

--- main/src/cgeo/geocaching/downloader/MapDownloaderOpenAndroMaps.java
+++ main/src/cgeo/geocaching/downloader/MapDownloaderOpenAndroMaps.java
@@ -2,9 +2,6 @@ package cgeo.geocaching.downloader;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.models.OfflineMap;
-import cgeo.geocaching.storage.ContentStorage;
-import cgeo.geocaching.storage.PersistableFolder;
-import cgeo.geocaching.ui.dialog.Dialogs;
 import cgeo.geocaching.utils.CalendarUtils;
 import cgeo.geocaching.utils.Formatter;
 import cgeo.geocaching.utils.MatcherWrapper;
@@ -21,7 +18,7 @@ public class MapDownloaderOpenAndroMaps extends AbstractMapDownloader {
     private static final Pattern PATTERN_MAP = Pattern.compile("<a href=\"([A-Za-z0-9_-]+\\.zip)\">([A-Za-z0-9_-]+)\\.zip<\\/a>[ ]*([0-9]{2}-[A-Za-z]{3}-[0-9]{4}) [0-9]{2}:[0-9]{2}[ ]*([0-9]+)"); // 1:file name, 2:display name, 3:date DD-MMM-YYYY, 4:size (bytes)
     private static final Pattern PATTERN_DIR = Pattern.compile("<a href=\"([A-Z-a-z0-9]+\\/)\">([A-Za-z0-9]+)\\/<\\/a>");  // 1:file name, 2:display name
     private static final Pattern PATTERN_UP = Pattern.compile("<a href=\"(\\.\\.\\/)\">(\\.\\.)\\/<\\/a>"); // 1:relative dir, 2:..
-    private static final String ELEVATE_THEME = "Elevate.zip";
+    private static final String[] THEME_FILES = {"Elevate.zip"};
     private static final MapDownloaderOpenAndroMaps INSTANCE = new MapDownloaderOpenAndroMaps();
 
     private MapDownloaderOpenAndroMaps() {
@@ -66,23 +63,7 @@ public class MapDownloaderOpenAndroMaps extends AbstractMapDownloader {
     @Override
     protected void onFollowup(final Activity activity, final Runnable callback) {
         // check whether Elevate.zip exists in theme folder and ask whether user wants to download it as well, if it does not exist yet
-        boolean themeFound = false;
-        final List<ContentStorage.FileInformation> mapDirContent = ContentStorage.get().list(PersistableFolder.OFFLINE_MAP_THEMES);
-        for (ContentStorage.FileInformation fi : mapDirContent) {
-            if (fi.name.equals(ELEVATE_THEME)) {
-                themeFound = true;
-                break;
-            }
-        }
-
-        if (!themeFound) {
-            Dialogs.confirm(activity, activity.getString(R.string.downloadmap_install_theme_title), activity.getString(R.string.downloadmap_install_theme_info), activity.getString(android.R.string.ok), (d, w) -> {
-                final Uri newUri = Uri.parse(activity.getString(R.string.mapserver_elevate_downloadurl) + ELEVATE_THEME);
-                MapDownloaderUtils.triggerDownload(activity, OfflineMap.OfflineMapType.MAP_DOWNLOAD_TYPE_ELEVATE.id, newUri, "", System.currentTimeMillis(), callback);
-            }, dialog -> callback.run());
-        } else {
-            callback.run();
-        }
+        findOrDownload(activity, THEME_FILES, activity.getString(R.string.mapserver_elevate_downloadurl), OfflineMap.OfflineMapType.MAP_DOWNLOAD_TYPE_ELEVATE, callback);
     }
 
     @NonNull
