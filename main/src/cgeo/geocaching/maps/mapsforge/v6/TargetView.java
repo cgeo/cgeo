ae9a2a93117af86bd8a7e9778d9b28a69977c9af
bekuno <>
2021-02-24 23:31:43 +0100

    check & add NonNull/Nullable annotation
    - add some NonNull/Nullable annotation
    - fix a nullability problem

--- main/src/cgeo/geocaching/maps/mapsforge/v6/TargetView.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v6/TargetView.java
@@ -3,6 +3,9 @@ package cgeo.geocaching.maps.mapsforge.v6;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class TargetView {
@@ -10,16 +13,16 @@ public class TargetView {
     private final TextView targetView;
     private final TextView targetViewSupersize;
 
-    public TargetView(final TextView targetView, final TextView targetViewSupersize, final String geocode, final String name) {
+    public TargetView(@NonNull final TextView targetView, @NonNull final TextView targetViewSupersize, @Nullable final String geocode, @Nullable final String name) {
         this.targetView = targetView;
         this.targetViewSupersize = targetViewSupersize;
 
         setTarget(geocode, name);
     }
 
-    public void setTarget(final String geocode, final String name) {
+    public void setTarget(@Nullable final String geocode, @Nullable final String name) {
         if (StringUtils.isNotEmpty(geocode)) {
-            targetView.setText(String.format("%s: %s", geocode, name));
+            targetView.setText(String.format("%s: %s", geocode, StringUtils.isNotEmpty(name) ? name : StringUtils.EMPTY));
             targetView.setVisibility(View.VISIBLE);
         } else {
             targetView.setText("");
