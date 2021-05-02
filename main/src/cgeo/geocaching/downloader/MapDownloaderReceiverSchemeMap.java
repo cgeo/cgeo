a7aa2ee2d8e191a814b0eabd74abe5fefb324ab2
moving-bits <github@moving-bits.net>
2021-05-02 21:40:17 +0200

    add support for mf-v4-map and mf-theme schemes for Hylly map/theme

--- main/src/cgeo/geocaching/downloader/MapDownloaderReceiverSchemeMap.java
+++ main/src/cgeo/geocaching/downloader/MapDownloaderReceiverSchemeMap.java
@@ -20,11 +20,15 @@ class MapDownloaderReceiverSchemeMap extends AbstractActivity {
         final String path = uri.getPath();
         Log.i("MapDownloaderReceiverSchemeMap: host=" + host + ", path=" + path);
 
-        // check for OpenAndroMaps
         if (host.equals("download.openandromaps.org") && path.startsWith("/mapsV4/") && path.endsWith(".zip")) {
+            // check for OpenAndroMaps - mf-v4-map://download.openandromaps.org/mapsV4/Germany/bayern.zip
             // remap Uri to their ftp server
             final Uri newUri = Uri.parse(getString(R.string.mapserver_openandromaps_downloadurl) + path.substring(8));
             DownloaderUtils.triggerDownload(this, R.string.downloadmap_title, Download.DownloadType.DOWNLOADTYPE_MAP_OPENANDROMAPS.id, newUri, "", "", System.currentTimeMillis(), this::callback);
+        } else if (host.equals("kartat-dl.hylly.org") && path.endsWith("/mtk_suomi.map")) {
+            // check for Hylly maps - mf-v4-map://kartat-dl.hylly.org/2021-04-25/mtk_suomi.map
+            final Uri newUri = Uri.parse("https://" + host + path);
+            DownloaderUtils.triggerDownload(this, R.string.downloadmap_title, Download.DownloadType.DOWNLOADTYPE_MAP_HYLLY.id, newUri, "", "", System.currentTimeMillis(), this::callback);
         } else {
             // generic map download
             Log.w("MapDownloaderReceiverSchemeMap: Received map download intent from unknown source: " + uri.toString());
