33e6e77eb7a7b6d1c886c91be878676606fe66db
moving-bits <github@moving-bits.net>
2021-07-04 10:07:22 +0200

    partly revert logtemplate migration to TextInputLayout (fix #11024)

--- main/src/cgeo/geocaching/settings/TemplateTextPreference.java
+++ main/src/cgeo/geocaching/settings/TemplateTextPreference.java
@@ -1,28 +1,21 @@
 package cgeo.geocaching.settings;
 
-import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.R;
 import cgeo.geocaching.activity.ActivityMixin;
 import cgeo.geocaching.log.LogTemplateProvider;
 import cgeo.geocaching.log.LogTemplateProvider.LogTemplate;
 import cgeo.geocaching.ui.dialog.Dialogs;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -57,8 +50,6 @@ public class TemplateTextPreference extends DialogPreference {
 
         editText = view.findViewById(R.id.signature_dialog_text);
         editText.setText(getPersistedString(initialValue != null ? initialValue : StringUtils.EMPTY));
-        // @todo yet another workaround to be dismissed after migration of settings to PreferenceFragment
-        setCursorDrawableColor(editText, Settings.isLightSkin(getContext()) ? 0xff000000 : 0xffffffff);
         Dialogs.moveCursorToEnd(editText);
 
         final Button button = view.findViewById(R.id.signature_templates);
@@ -75,42 +66,10 @@ public class TemplateTextPreference extends DialogPreference {
                 final LogTemplate template = templates.get(position);
                 insertSignatureTemplate(template);
             });
-            final AlertDialog dialog = alert.create();
-            // @todo yet another workaround to be dismissed after migration of settings to PreferenceFragment
-            final int c = Build.VERSION.SDK_INT > 22 ? getContext().getColor(Settings.isLightSkin(getContext()) ? R.color.settings_colorDialogBackgroundLight : R.color.settings_colorDialogBackgroundDark) : CgeoApplication.getInstance().getResources().getColor(R.color.colorBackgroundSelected);
-            dialog.getListView().setBackgroundColor(c);
-            dialog.show();
+            alert.create().show();
         });
 
         super.onBindDialogView(view);
-    }
-
-    // @todo the whole method is yet another workaround to be dismissed after migration of settings to PreferenceFragment
-    @SuppressLint("UseCompatLoadingForDrawables")
-    public static void setCursorDrawableColor(final EditText editText, final int color) {
-        // adapted from https://stackoverflow.com/questions/11554078/set-textcursordrawable-programmatically
-        if (Build.VERSION.SDK_INT >= 29) {
-            editText.setTextCursorDrawable(R.drawable.cursor);
-        } else {
-            try {
-                final Field fCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
-                fCursorDrawableRes.setAccessible(true);
-                final int mCursorDrawableRes = fCursorDrawableRes.getInt(editText);
-                final Field fEditor = TextView.class.getDeclaredField("mEditor");
-                fEditor.setAccessible(true);
-                final Object editor = fEditor.get(editText);
-                final Field fCursorDrawable = editor.getClass().getDeclaredField("mCursorDrawable");
-                fCursorDrawable.setAccessible(true);
-                final Drawable[] drawables = new Drawable[2];
-                drawables[0] = editText.getContext().getResources().getDrawable(mCursorDrawableRes);
-                drawables[1] = editText.getContext().getResources().getDrawable(mCursorDrawableRes);
-                drawables[0].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-                drawables[1].setColorFilter(color, PorterDuff.Mode.SRC_IN);
-                fCursorDrawable.set(editor, drawables);
-            } catch (Throwable ignored) {
-            }
-        }
-        editText.setTextColor(color);
     }
 
     private void insertSignatureTemplate(final LogTemplate template) {
