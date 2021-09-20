ecba87292e86a69b40628f54069463e63df05dd6
fm-sys <64581222+fm-sys@users.noreply.github.com>
2021-09-20 22:03:42 +0200

    show navigation chooser as dialog instead of submenu inside map popup
    fix #11466
    fix #11635

--- main/src/cgeo/geocaching/CacheDetailActivity.java
+++ main/src/cgeo/geocaching/CacheDetailActivity.java
@@ -699,7 +699,7 @@ public class CacheDetailActivity extends TabbedViewPagerActivity
 
     @Override
     public boolean onOptionsItemSelected(final MenuItem item) {
-        if (CacheMenuHandler.onMenuItemSelected(item, this, cache, this::notifyDataSetChanged)) {
+        if (CacheMenuHandler.onMenuItemSelected(item, this, cache, this::notifyDataSetChanged, false)) {
             return true;
         }
 
