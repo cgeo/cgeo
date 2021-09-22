fb5f83c6a2fddfbdaa0c2063f8d353fa11e20d48
Bernd <bekuno@sags-per-mail.de>
2021-09-22 23:01:55 +0200

    remove outdated OC sites (#11714)
    remove outdated OC sites
    
    remove the references to the not more used OC sites
     - opencachingspain.es
     - opencaching.it
     - opencaching.fr
     - opencaching.se
     - opencaching.no
    
    And do not more use
     - opencaching.es
    as it is only a forward to opencaching.de.
    
    Fixes #11710

--- main/src/cgeo/geocaching/connector/oc/OCDEConnector.java
+++ main/src/cgeo/geocaching/connector/oc/OCDEConnector.java
@@ -11,8 +11,9 @@ public final class OCDEConnector extends OCApiLiveConnector {
 
     /**
      * Alternate Opencaching domains which are managed by Opencaching.DE.
+     * current: no mapped domain; opencaching.es is a direct forward to opencaching.de/?locale=ES
      */
-    private static final String[] MAPPED_DOMAINS = new String[] { "opencachingspain.es", "opencaching.it", "opencaching.fr" };
+    private static final String[] MAPPED_DOMAINS = new String[] { null };
 
     public OCDEConnector() {
         super("opencaching.de", "www.opencaching.de", true, "OC", "CC BY-NC-ND, alle Logeinträge © jeweiliger Autor",
@@ -24,6 +25,7 @@ public final class OCDEConnector extends OCApiLiveConnector {
     @Nullable
     public String getGeocodeFromUrl(@NonNull final String url) {
         for (final String mappedDomain : MAPPED_DOMAINS) {
+            //noinspection ConstantConditions
             if (StringUtils.containsIgnoreCase(url, mappedDomain)) {
                 // replace the country specific URL to not confuse the OKAPI interface
                 final String deUrl = StringUtils.replaceIgnoreCase(url, mappedDomain, "opencaching.de");
