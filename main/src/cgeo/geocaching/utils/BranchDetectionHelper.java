7fe84ff676be1e618d4af67cff39ad1aecdb5eaa
moving-bits <github@moving-bits.net>
2021-10-11 07:58:41 +0200

    prepare changelog and version file for upcoming beta

--- main/src/cgeo/geocaching/utils/BranchDetectionHelper.java
+++ main/src/cgeo/geocaching/utils/BranchDetectionHelper.java
@@ -2,10 +2,10 @@ package cgeo.geocaching.utils;
 
 public class BranchDetectionHelper {
     // should be "1" if on branch "release", and "0" else
-    public static final int FROM_BRANCH_RELEASE = 0;
+    public static final int FROM_BRANCH_RELEASE = 1;
 
     // should contain the version name of the last feature release
-    public static final String FEATURE_VERSION_NAME = "2021.09.27";
+    public static final String FEATURE_VERSION_NAME = "2021.10-11-RC";
 
     private BranchDetectionHelper() {
         // utility class
