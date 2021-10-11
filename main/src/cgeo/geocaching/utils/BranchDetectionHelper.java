03b5eca70254c56cbb0d3dc2d7b9161c2fd1c84b
fm-sys <64581222+fm-sys@users.noreply.github.com>
2021-10-11 13:29:11 +0200

    smart-detect build type in BranchDetectionHelper

--- main/src/cgeo/geocaching/utils/BranchDetectionHelper.java
+++ main/src/cgeo/geocaching/utils/BranchDetectionHelper.java
@@ -1,13 +1,21 @@
 package cgeo.geocaching.utils;
 
+import cgeo.geocaching.BuildConfig;
+
 public class BranchDetectionHelper {
-    // should be "1" if on branch "release", and "0" else
-    public static final int FROM_BRANCH_RELEASE = 1;
 
     // should contain the version name of the last feature release
     public static final String FEATURE_VERSION_NAME = "2021.10-11-RC";
 
     private BranchDetectionHelper() {
         // utility class
+    }
+
+    /**
+     * @return true, if BUILD_TYPE is not a nightly or debug build
+     */
+    @SuppressWarnings("ConstantConditions") // BUILD_TYPE is detected as constant but can change depending on the build configuration
+    public static boolean isProductionBuild() {
+        return !(BuildConfig.BUILD_TYPE.equals("debug") || BuildConfig.BUILD_TYPE.equals("nightly"));
     }
 }
