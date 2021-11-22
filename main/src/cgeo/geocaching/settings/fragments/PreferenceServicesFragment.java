1fc8e8ef3cc1e71e7fbcba70e806b733a7a99235
moving-bits <github@moving-bits.net>
2021-11-22 21:50:02 +0100

    show connectors' activation status in settings => services

--- main/src/cgeo/geocaching/settings/fragments/PreferenceServicesFragment.java
+++ main/src/cgeo/geocaching/settings/fragments/PreferenceServicesFragment.java
@@ -1,6 +1,8 @@
 package cgeo.geocaching.settings.fragments;
 
 import cgeo.geocaching.R;
+import cgeo.geocaching.settings.Settings;
+import static cgeo.geocaching.utils.SettingsUtils.setPrefSummaryActiveStatus;
 
 import android.os.Bundle;
 
@@ -16,5 +18,21 @@ public class PreferenceServicesFragment extends PreferenceFragmentCompat {
     public void onResume() {
         super.onResume();
         getActivity().setTitle(R.string.settings_title_services);
+
+        // display connectors' activation status
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_gc_authorization, Settings.isGCConnectorActive());
+        setPrefSummaryActiveStatus(this, R.string.pref_connectorALActive, Settings.isGCConnectorActive() && Settings.isALConnectorActive());
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ocde_authorization, Settings.isOCConnectorActive(R.string.pref_connectorOCActive));
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ocpl_authorization, Settings.isOCConnectorActive(R.string.pref_connectorOCPLActive));
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ocnl_authorization, Settings.isOCConnectorActive(R.string.pref_connectorOCNLActive));
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ocus_authorization, Settings.isOCConnectorActive(R.string.pref_connectorOCUSActive));
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ocro_authorization, Settings.isOCConnectorActive(R.string.pref_connectorOCROActive));
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ocuk_authorization, Settings.isOCConnectorActive(R.string.pref_connectorOCUKActive));
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_ec_authorization, Settings.isECConnectorActive());
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_su_authorization, Settings.isSUConnectorActive());
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_gcvote_authorization, Settings.isRatingWanted());
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_geokrety_authorization, Settings.isGeokretyConnectorActive());
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_sendtocgeo_info, Settings.isRegisteredForSend2cgeo());
+        setPrefSummaryActiveStatus(this, R.string.pref_fakekey_twitter_authorization, Settings.isUseTwitter());
     }
 }
