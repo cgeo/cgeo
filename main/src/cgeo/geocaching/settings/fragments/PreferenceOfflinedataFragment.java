bfd8ae11a6b94160092d5572b8a40282d42f9de2
moving-bits <github@moving-bits.net>
2021-12-08 22:47:49 +0100

    restore maintenance method (fix #12267)

--- main/src/cgeo/geocaching/settings/fragments/PreferenceOfflinedataFragment.java
+++ main/src/cgeo/geocaching/settings/fragments/PreferenceOfflinedataFragment.java
@@ -1,18 +1,31 @@
 package cgeo.geocaching.settings.fragments;
 
 import cgeo.geocaching.R;
+import cgeo.geocaching.activity.ActivityMixin;
 import cgeo.geocaching.settings.Settings;
 import cgeo.geocaching.settings.SettingsActivity;
+import cgeo.geocaching.storage.DataStore;
+import cgeo.geocaching.utils.AndroidRxUtils;
 import static cgeo.geocaching.utils.SettingsUtils.initPublicFolders;
 
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class PreferenceOfflinedataFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
         setPreferencesFromResource(R.xml.preferences_offlinedata, rootKey);
+
+        findPreference(getString(R.string.pref_fakekey_preference_maintenance_directories)).setOnPreferenceClickListener(preference -> {
+            // disable the button, as the cleanup runs in background and should not be invoked a second time
+            preference.setEnabled(false);
+            ActivityMixin.showShortToast(getActivity(), R.string.init_maintenance_start);
+            AndroidRxUtils.andThenOnUi(Schedulers.io(), DataStore::removeObsoleteGeocacheDataDirectories, () -> ActivityMixin.showShortToast(getActivity(), R.string.init_maintenance_finished));
+            return true;
+        });
 
         // TODO
         findPreference(getString(R.string.pref_fakekey_dataDir)).setSummary(Settings.getExternalPrivateCgeoDirectory());
