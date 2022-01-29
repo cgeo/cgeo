cgeo/cgeo

commit 509578dd6ff4cb6b4423ee7a2c4076a5ffbefed9

update avatar when loading server params (fix #12620)

main/src/cgeo/geocaching/connector/gc/GCLogin.java
@@ -4,16 +4,14 @@ import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.R;
 import cgeo.geocaching.connector.AbstractLogin;
 import cgeo.geocaching.enumerations.StatusCode;
-import cgeo.geocaching.network.HtmlImage;
 import cgeo.geocaching.network.Network;
 import cgeo.geocaching.network.Parameters;
 import cgeo.geocaching.settings.Credentials;
 import cgeo.geocaching.settings.Settings;
+import cgeo.geocaching.ui.AvatarUtils;
 import cgeo.geocaching.utils.Log;
 import cgeo.geocaching.utils.MatcherWrapper;
 import cgeo.geocaching.utils.TextUtils;
-
-import android.graphics.drawable.Drawable;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,7 +26,6 @@ import java.util.Locale;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 import okhttp3.Response;
 import org.apache.commons.lang3.ArrayUtils;
@@ -67,7 +64,9 @@ public class GCLogin extends AbstractLogin {
      *      "roles": [
      *         "Public",
      *         "Premium"
-     *         ]
+     *         ],
+     *     "publicGuid": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+     *     "avatarUrl": "https://img.geocaching.com/avatar/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx.png"
      *      },
      *  "app:options": {
      *       "localRegion": "en-US",
@@ -101,6 +100,10 @@ public class GCLogin extends AbstractLogin {
             String unitSetName;
             @JsonProperty("roles")
             String[] roles;
+            @JsonProperty("publicGuid")
+            String publicGuid;
+            @JsonProperty("avatarUrl")
+            String avatarUrl;
         }
 
         @JsonIgnoreProperties(ignoreUnknown = true)
@@ -230,7 +233,7 @@ public class GCLogin extends AbstractLogin {
     public StatusCode logout() {
         try {
             getResponseBodyOrStatus(Network.postRequest("https://www.geocaching.com/account/logout", null).blockingGet());
-            serverParameters = null;
+            resetServerParameters();
         } catch (final StatusException status) {
             return status.statusCode;
         } catch (final Exception ignored) {
@@ -334,44 +337,6 @@ public class GCLogin extends AbstractLogin {
     }
 
     /**
-     * Retrieve avatar url from GC
-     *
-     * @return the avatar url
-     */
-    public String getAvatarUrl() {
-        try {
-            final String responseData = StringUtils.defaultString(Network.getResponseData(Network.getRequest("https://www.geocaching.com/play/serverparameters/params")));
-            final String avatarURL = TextUtils.getMatch(responseData, GCConstants.PATTERN_AVATAR_IMAGE_SERVERPARAMETERS, false, null);
-            if (avatarURL != null) {
-                return avatarURL.replace("avatar", "user/large");
-            }
-            // No match? There may be no avatar set by user.
-            Log.d("No avatar set for user");
-        } catch (final Exception e) {
-            Log.w("Error when retrieving user avatar url", e);
-        }
-        return StringUtils.EMPTY;
-    }
-
-    /**
-     * Download the avatar
-     *
-     * @return the avatar drawable
-     */
-    public Observable<Drawable> downloadAvatar() {
-        try {
-            final String avatarURL = getAvatarUrl();
-            if (!avatarURL.isEmpty()) {
-                final HtmlImage imgGetter = new HtmlImage(HtmlImage.SHARED, false, false, false);
-                return imgGetter.fetchDrawable(avatarURL).cast(Drawable.class);
-            }
-        } catch (final Exception e) {
-            Log.w("Error when retrieving user avatar", e);
-        }
-        return null;
-    }
-
-    /**
      * Retrieve the home location
      *
      * @return a Single containing the home location, or IOException
@@ -412,6 +377,8 @@ public class GCLogin extends AbstractLogin {
             final GCMemberState memberState = GCMemberState.fromString(serverParameters.userInfo.userType);
             Log.d("Setting member status to " + memberState);
             Settings.setGCMemberStatus(memberState);
+
+            AvatarUtils.changeAvatar(GCConnector.getInstance(), serverParameters.userInfo.avatarUrl);
 
         } catch (final IOException e) {
             Settings.setGcCustomDate(GCConstants.DEFAULT_GC_DATE);

main/src/cgeo/geocaching/connector/gc/GCConstants.java
@@ -80,8 +80,6 @@ public final class GCConstants {
     /** Use replaceAll("[,.]","") on the resulting string before converting to an int */
     static final Pattern PATTERN_CACHES_FOUND = Pattern.compile("\\swindow(?>\\.|\\[')(?:headerSettings|chromeSettings)(?>'\\])?\\s*=\\s*\\{[\\S\\s]*\"findCount\":\\s*([0-9]*)[\\S\\s]*\\}");
 
-    static final Pattern PATTERN_AVATAR_IMAGE_SERVERPARAMETERS = Pattern.compile("\"(https?:\\/\\/(img(?:cdn)?\\.geocaching\\.com|[^>\\\"]+\\.cloudfront\\.net)\\/avatar\\/[0-9a-f-]+\\.(" + IMAGE_FORMATS + "))\\\"");
-
     // Patterns for parsing trackables
 
     static final Pattern PATTERN_TRACKABLE_GUID = Pattern.compile("<a id=\"ctl00_ContentBody_lnkPrint\" aria-labelledby=\"[^\"]*\" href=\".*sheet\\.aspx\\?guid=([a-z0-9\\-]+)\"[^>]*>[^<]*</a>");

main/src/cgeo/geocaching/settings/GCAuthorizationActivity.java
@@ -4,7 +4,6 @@ import cgeo.geocaching.R;
 import cgeo.geocaching.connector.gc.GCConnector;
 import cgeo.geocaching.connector.gc.GCLogin;
 import cgeo.geocaching.enumerations.StatusCode;
-import cgeo.geocaching.ui.AvatarUtils;
 
 public class GCAuthorizationActivity extends AbstractCredentialsAuthorizationActivity {
 
@@ -35,7 +34,7 @@ public class GCAuthorizationActivity extends AbstractCredentialsAuthorizationAct
 
         final StatusCode status = GCLogin.getInstance().login(credentials);
         if (status == StatusCode.NO_ERROR) {
-            AvatarUtils.changeAvatar(GCConnector.getInstance(), GCLogin.getInstance().getAvatarUrl());
+            GCLogin.getInstance().getServerParameters(); // This will initialize some settings
         }
         return  status;
     }

main/src/cgeo/geocaching/ui/AvatarUtils.java
@@ -9,6 +9,8 @@ import android.graphics.drawable.BitmapDrawable;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.Objects;
+
 import org.apache.commons.lang3.StringUtils;
 
 /** Class to retrieve Avatar images from. This class handles e.g. in-memory image caching */
@@ -21,7 +23,9 @@ public class AvatarUtils {
     }
 
     public static void changeAvatar(@NonNull final IAvatar avatar, final String newUrl) {
-        Settings.setAvatarUrl(avatar, newUrl);
+        if (!Objects.equals(Settings.getAvatarUrl(avatar), newUrl)) {
+            Settings.setAvatarUrl(avatar, newUrl);
+        }
     }
 
 

tests/src-android/cgeo/geocaching/connector/gc/GCLoginTest.java
@@ -2,6 +2,7 @@ package cgeo.geocaching.connector.gc;
 
 import cgeo.geocaching.enumerations.StatusCode;
 import cgeo.geocaching.location.Geopoint;
+import cgeo.geocaching.ui.AvatarUtils;
 import cgeo.geocaching.utils.TextUtils;
 
 import junit.framework.TestCase;
@@ -36,7 +37,11 @@ public class GCLoginTest extends TestCase {
     }
 
     public void testAvatar() {
-        assertThat(instance.downloadAvatar()).isNotNull();
+        instance.resetServerParameters();
+        AvatarUtils.changeAvatar(GCConnector.getInstance(), null);
+        assertThat(AvatarUtils.getAvatar(GCConnector.getInstance())).isNull();
+        instance.getServerParameters(); // avatar should automatically be updated here...
+        assertThat(AvatarUtils.getAvatar(GCConnector.getInstance())).isNotNull();
     }
 
 }

commit fb9a576ed766c01dcf352fa47ea0a7282bd24dd2

use high-res version of avatar image (fix regression introduced by #12621)

main/src/cgeo/geocaching/connector/gc/GCLogin.java
@@ -378,7 +378,11 @@ public class GCLogin extends AbstractLogin {
             Log.d("Setting member status to " + memberState);
             Settings.setGCMemberStatus(memberState);
 
-            AvatarUtils.changeAvatar(GCConnector.getInstance(), serverParameters.userInfo.avatarUrl);
+            if (StringUtils.isNotBlank(serverParameters.userInfo.avatarUrl)) {
+                final String avatarUrl = serverParameters.userInfo.avatarUrl.replace("/avatar/", "/user/large/");
+                Log.d("Setting avatar to " + avatarUrl);
+                AvatarUtils.changeAvatar(GCConnector.getInstance(), avatarUrl);
+            }
 
         } catch (final IOException e) {
             Settings.setGcCustomDate(GCConstants.DEFAULT_GC_DATE);