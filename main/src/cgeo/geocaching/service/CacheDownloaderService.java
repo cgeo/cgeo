6bcd5a1a621fe8ab3f374824c06612e9ed02073a
fm-sys <64581222+fm-sys@users.noreply.github.com>
2022-02-09 18:39:45 +0100

    Allow canceling of background download (rel. to #330)

--- main/src/cgeo/geocaching/service/CacheDownloaderService.java
+++ main/src/cgeo/geocaching/service/CacheDownloaderService.java
@@ -2,16 +2,20 @@ package cgeo.geocaching.service;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.activity.ActivityMixin;
+import cgeo.geocaching.enumerations.LoadFlags;
 import cgeo.geocaching.list.StoredList;
 import cgeo.geocaching.models.Geocache;
 import cgeo.geocaching.settings.Settings;
+import cgeo.geocaching.storage.DataStore;
 import cgeo.geocaching.ui.dialog.CheckboxDialogConfig;
 import cgeo.geocaching.ui.dialog.Dialogs;
 import cgeo.geocaching.ui.notifications.NotificationChannels;
 import cgeo.geocaching.ui.notifications.Notifications;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -22,7 +26,9 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-
+/**
+ * TODO: Currently this is an ALPHA feature. Before releasing, all hardcoded strings should be localized.
+ */
 public class CacheDownloaderService extends AbstractForegroundIntentService {
     static {
         logTag = "CacheDownloaderService";
@@ -31,6 +37,8 @@ public class CacheDownloaderService extends AbstractForegroundIntentService {
     private static final String GEOCODES = "extra_geocodes";
     private static final String LIST_IDS = "extra_list_ids";
     private static final String REDOWNLOAD = "extra_redownload";
+
+    private static volatile boolean shouldStop = false;
 
     public static void downloadCaches(final Activity context, final Set<String> geocodes, final boolean defaultForceRedownload, final boolean isOffline) {
         if (geocodes.isEmpty()) {
@@ -64,10 +72,20 @@ public class CacheDownloaderService extends AbstractForegroundIntentService {
         ContextCompat.startForegroundService(context, intent);
     }
 
+    public static void requestStopService() {
+        shouldStop = true;
+    }
+
     @Override
     public NotificationCompat.Builder createInitialNotification() {
+        shouldStop = false;
+        final PendingIntent actionCancelIntent = PendingIntent.getBroadcast(this, 0,
+                new Intent(this, StopCacheDownloadServiceReceiver.class),
+                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? PendingIntent.FLAG_IMMUTABLE : 0) | PendingIntent.FLAG_UPDATE_CURRENT);
+
         return Notifications.createNotification(this, NotificationChannels.FOREGROUND_SERVICE_NOTIFICATION, R.string.caches_store_background_title)
                 .setProgress(100, 0, true)
+                .addAction(R.drawable.ic_menu_cancel, getString(android.R.string.cancel), actionCancelIntent)
                 .setOnlyAlertOnce(true);
     }
 
@@ -82,24 +100,43 @@ public class CacheDownloaderService extends AbstractForegroundIntentService {
             return;
         }
 
-        final ArrayList<String> geocodes = intent.getStringArrayListExtra(GEOCODES);
+        try {
+            final ArrayList<String> geocodes = intent.getStringArrayListExtra(GEOCODES);
 
-        final ArrayList<Integer> listIdArray = intent.getIntegerArrayListExtra(LIST_IDS);
-        final Set<Integer> listIds = listIdArray != null ? new HashSet<>(intent.getIntegerArrayListExtra(LIST_IDS)) : null;
+            final ArrayList<Integer> listIdArray = intent.getIntegerArrayListExtra(LIST_IDS);
+            final Set<Integer> listIds = listIdArray != null ? new HashSet<>(intent.getIntegerArrayListExtra(LIST_IDS)) : new HashSet<>();
+            final boolean forceRedownload = intent.getBooleanExtra(REDOWNLOAD, false);
 
-        final boolean forceRedownload = intent.getBooleanExtra(REDOWNLOAD, false);
+            int cachesDownloaded = 0;
+            while (cachesDownloaded < geocodes.size() && !shouldStop) {
+                notification.setProgress(geocodes.size(), cachesDownloaded, false);
+                notification.setContentText(geocodes.get(cachesDownloaded));
+                updateForegroundNotification();
 
+                final Set<Integer> combinedListIds = new HashSet<>(listIds);
+                final Geocache cache = DataStore.loadCache(geocodes.get(cachesDownloaded), LoadFlags.LOAD_CACHE_OR_DB);
+                if (cache != null && !cache.getLists().isEmpty()) {
+                    combinedListIds.addAll(cache.getLists());
+                }
 
-        for (int i = 0; i < geocodes.size(); i++) {
-            notification.setProgress(geocodes.size(), i, false);
-            notification.setContentText(geocodes.get(i));
-            updateForegroundNotification();
+                Geocache.storeCache(null, geocodes.get(cachesDownloaded), combinedListIds, forceRedownload, null);
 
-            Geocache.storeCache(null, geocodes.get(i), listIds, forceRedownload, null);
+                cachesDownloaded++;
+            }
+
+            if (shouldStop) {
+                showEndNotification("Download canceled, " + cachesDownloaded + "/" + geocodes.size() + " caches were downloaded");
+            } else {
+                showEndNotification(geocodes.size() + " caches downloaded");
+            }
+        } catch (Exception ignored) {
+            showEndNotification("download failed");
         }
+    }
 
+    private void showEndNotification(final String text) {
         notificationManager.notify(Settings.getUniqueNotificationId(), Notifications.createTextContentNotification(
-                this, NotificationChannels.DOWNLOADER_RESULT_NOTIFICATION, R.string.caches_store_background_title, geocodes.size() + " caches downloaded")
-                .build());
+                this, NotificationChannels.DOWNLOADER_RESULT_NOTIFICATION, R.string.caches_store_background_title, text).build());
+
     }
 }
