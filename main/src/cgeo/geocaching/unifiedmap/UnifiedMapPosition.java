3657fd9dadfc72774040224b8faed2c8d09d2372
moving-bits <github@moving-bits.net>
2022-03-20 18:25:42 +0100

    fix followMyLocation for manual moves/automated GPS updates

--- main/src/cgeo/geocaching/unifiedmap/UnifiedMapPosition.java
+++ main/src/cgeo/geocaching/unifiedmap/UnifiedMapPosition.java
@@ -6,11 +6,18 @@ public class UnifiedMapPosition {
     public double longitude;
     public int zoomLevel;
     public float bearing;
+    public boolean resetFollowMyLocation;
+
+    public UnifiedMapPosition() {
+        this(0, 0, 0, 0);
+        resetFollowMyLocation = false;
+    }
 
     public UnifiedMapPosition(final double latitude, final double longitude, final int zoomLevel, final float bearing) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.zoomLevel = zoomLevel;
         this.bearing = bearing;
+        this.resetFollowMyLocation = true;
     }
 }
