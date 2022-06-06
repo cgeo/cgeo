2528202ed269d970b7f71acdfdd81a9ec450cabf
moving-bits <github@moving-bits.net>
2022-06-06 10:40:29 +0200

    replace GC3FJ5F by GC40 in tests (fix #13082)

--- tests/src-android/cgeo/geocaching/connector/gc/GCBaseTest.java
+++ tests/src-android/cgeo/geocaching/connector/gc/GCBaseTest.java
@@ -3,7 +3,7 @@ package cgeo.geocaching.connector.gc;
 import cgeo.geocaching.SearchResult;
 import cgeo.geocaching.enumerations.LoadFlags;
 import cgeo.geocaching.models.Geocache;
-import cgeo.geocaching.test.mock.GC3FJ5F;
+import cgeo.geocaching.test.mock.GC40;
 import cgeo.geocaching.test.mock.MockedCache;
 import cgeo.test.Compare;
 
@@ -15,7 +15,7 @@ import junit.framework.TestCase;
 public class GCBaseTest extends TestCase {
 
     public static void testSearchFromMap() {
-        final MockedCache mockedCache = new GC3FJ5F();
+        final MockedCache mockedCache = new GC40();
 
         final Set<String> geocodes = new HashSet<>();
         geocodes.add(mockedCache.getGeocode());
