320e6c327e3e371ae1f15cd333f4a2d51a2788c3
moving-bits <github@moving-bits.net>
2022-06-10 14:57:45 +0200

    replace GC3FJ5F by GC40 in tests (fix #13082) (#13092)
    * replace GC3FJ5F by GC40 in tests (fix #13082)
    * temporarily disable outdated test GCBaseTest.testSearchFromMap
    
    Co-authored-by: fm-sys <64581222+fm-sys@users.noreply.github.com>

--- tests/src-android/cgeo/geocaching/connector/gc/GCBaseTest.java
+++ tests/src-android/cgeo/geocaching/connector/gc/GCBaseTest.java
@@ -1,21 +1,12 @@
 package cgeo.geocaching.connector.gc;
 
-import cgeo.geocaching.SearchResult;
-import cgeo.geocaching.enumerations.LoadFlags;
-import cgeo.geocaching.models.Geocache;
-import cgeo.geocaching.test.mock.GC3FJ5F;
-import cgeo.geocaching.test.mock.MockedCache;
-import cgeo.test.Compare;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import junit.framework.TestCase;
 
 public class GCBaseTest extends TestCase {
 
+    /* TODO: temporary disabled, see PR #13119
     public static void testSearchFromMap() {
-        final MockedCache mockedCache = new GC3FJ5F();
+        final MockedCache mockedCache = new GC40();
 
         final Set<String> geocodes = new HashSet<>();
         geocodes.add(mockedCache.getGeocode());
@@ -25,5 +16,6 @@ public class GCBaseTest extends TestCase {
 
         Compare.assertCompareCaches(mockedCache, parsedCache, false);
     }
+    */
 
 }
