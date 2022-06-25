f71b1299376aed6b15a224220ccce48f427a5e32
Tiger Oakes <contact@tigeroakes.com>
2022-06-25 13:14:51 -0700

    Add ViewModel to AboutActivity (#13182)

---
+++ main/src/cgeo/geocaching/about/SystemInformationViewModel.java
@@ -0,0 +1,34 @@
+package cgeo.geocaching.about;
+
+import cgeo.geocaching.utils.SystemInformation;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+/**
+ * Stores system information for the about page.
+ */
+public class SystemInformationViewModel extends AndroidViewModel {
+
+    private final LiveData<String> systemInformation;
+
+    public SystemInformationViewModel(@NonNull final Application application) {
+        super(application);
+
+        systemInformation = LiveDataReactiveStreams.fromPublisher(
+            Flowable.fromCallable(() -> SystemInformation.getSystemInformation(application)).subscribeOn(Schedulers.io())
+        );
+    }
+
+    @NonNull
+    public LiveData<String> getSystemInformation() {
+        return systemInformation;
+    }
+}
