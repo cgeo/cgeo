97e0eec315588a046c626c20a1ab2748340f9f49
ztNFny <ztNFny@users.noreply.github.com>
2022-08-14 18:09:16 +0200

    adapt SAF for hillshading

--- main/src/cgeo/geocaching/maps/mapsforge/v6/layers/HillShadingLayer.java
+++ main/src/cgeo/geocaching/maps/mapsforge/v6/layers/HillShadingLayer.java
@@ -1,25 +1,22 @@
 package cgeo.geocaching.maps.mapsforge.v6.layers;
 
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.hills.DemFolderAndroidContent;
+import org.mapsforge.map.layer.hills.DemFolder;
 import org.mapsforge.map.layer.hills.HillsRenderConfig;
 import org.mapsforge.map.layer.hills.MemoryCachingHgtReaderTileSource;
 import org.mapsforge.map.layer.hills.SimpleShadingAlgorithm;
+import android.content.Context;
 
-import java.io.File;
-
+import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.settings.Settings;
-import cgeo.geocaching.storage.LocalStorage;
 import cgeo.geocaching.storage.PersistableFolder;
 
 public class HillShadingLayer {
-    public static HillsRenderConfig getHillsRenderConfig() {
-        // TODO: Get folder from SAF
-        File shadingFolder = new File(PersistableFolder.OFFLINE_MAP_SHADING.getFolder().toUserDisplayableString());
 
-        // Check if folder exists and contains files
-        if (!(shadingFolder.exists() && shadingFolder.isDirectory() && shadingFolder.canRead() && shadingFolder.listFiles().length > 0)) {
-            return null;
-        }
+    public static HillsRenderConfig getHillsRenderConfig() {
+        Context context = CgeoApplication.getInstance();
+        DemFolder shadingFolder = new DemFolderAndroidContent(PersistableFolder.OFFLINE_MAP_SHADING.getUri(), context, context.getContentResolver());
 
         MemoryCachingHgtReaderTileSource hillTileSource = new MemoryCachingHgtReaderTileSource(shadingFolder, new SimpleShadingAlgorithm(Settings.getMapShadingLinearity(), Settings.getMapShadingScale()), AndroidGraphicFactory.INSTANCE);
         // avoid lines at between tiles
