b605be849ae6e12210858729b16dba6c35b667c9
moving-bits <github@moving-bits.net>
2022-10-14 21:11:50 +0200

    code cleanup: lambda expressions

--- main/src/cgeo/geocaching/activity/AbstractActivity.java
+++ main/src/cgeo/geocaching/activity/AbstractActivity.java
@@ -275,9 +275,7 @@ public abstract class AbstractActivity extends AppCompatActivity implements IAbs
                 ActivityMixin.showShortToast(this, R.string.variables_scanlisting_nopatternfound);
             } else {
                 SimpleDialog.of(this).setTitle(TextParam.id(R.string.variables_scanlisting_choosepattern_title))
-                        .selectMultiple(patterns, (s, i) -> TextParam.text("`" + s.first + " | " + s.second + "`").setMarkdown(true), null, set -> {
-                            cache.addCalculatedWaypoints(set, LocalizationUtils.getString(R.string.calccoord_generate_waypointnameprefix));
-                        });
+                        .selectMultiple(patterns, (s, i) -> TextParam.text("`" + s.first + " | " + s.second + "`").setMarkdown(true), null, set -> cache.addCalculatedWaypoints(set, LocalizationUtils.getString(R.string.calccoord_generate_waypointnameprefix)));
             }
         } else {
             showToast(res.getQuantityString(R.plurals.extract_waypoints_result, 0));
