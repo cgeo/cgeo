efe911ac4c51317a058d9a0336fa22f35492d87c
murggel <murggel@web.de>
2023-01-09 23:39:22 +0100

    add unit test for merging coordinates

--- main/src/test/java/cgeo/geocaching/models/WaypointTest.java
+++ main/src/test/java/cgeo/geocaching/models/WaypointTest.java
@@ -129,6 +129,24 @@ public class WaypointTest {
     }
 
     @Test
+    public void testMergeStageWPWithLocalCoords() {
+        final Waypoint local = new Waypoint("STAGE", WaypointType.STAGE, false);
+        local.setCoords(new Geopoint("N 45°49.739 E 9°45.038"));
+        final Waypoint server = new Waypoint("STAGE", WaypointType.STAGE, false);
+        server.merge(local);
+        assertThat(server.getCoords()).isEqualTo(new Geopoint("N 45°49.739 E 9°45.038"));
+    }
+
+    @Test
+    public void testMergeStageWPWithServerCoords() {
+        final Waypoint local = new Waypoint("STAGE", WaypointType.STAGE, false);
+        final Waypoint server = new Waypoint("STAGE", WaypointType.STAGE, false);
+        server.setCoords(new Geopoint("N 45°49.739 E 9°45.038"));
+        server.merge(local);
+        assertThat(server.getCoords()).isEqualTo(new Geopoint("N 45°49.739 E 9°45.038"));
+    }
+
+    @Test
     public void testMergeNote() {
         final Waypoint local = new Waypoint("Stage 1", WaypointType.STAGE, false);
         local.setNote("Old Note");
