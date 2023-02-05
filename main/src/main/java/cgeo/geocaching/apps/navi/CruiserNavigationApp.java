2d50153e55b792190edf73ede3252be7080f403f
moving-bits <github@moving-bits.net>
2023-02-05 15:04:59 +0100

    Provide destination name to Cruiser (if available)

--- main/src/main/java/cgeo/geocaching/apps/navi/CruiserNavigationApp.java
+++ main/src/main/java/cgeo/geocaching/apps/navi/CruiserNavigationApp.java
@@ -2,6 +2,8 @@ package cgeo.geocaching.apps.navi;
 
 import cgeo.geocaching.R;
 import cgeo.geocaching.location.Geopoint;
+import cgeo.geocaching.models.Geocache;
+import cgeo.geocaching.models.Waypoint;
 import cgeo.geocaching.sensors.GeoData;
 import cgeo.geocaching.sensors.LocationDataProvider;
 
@@ -9,6 +11,9 @@ import android.content.Context;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class CruiserNavigationApp extends AbstractPointNavigationApp {
 
@@ -20,13 +25,33 @@ public class CruiserNavigationApp extends AbstractPointNavigationApp {
 
     @Override
     public void navigate(final @NonNull Context context, final @NonNull Geopoint coords) {
+        navigate(context, coords, null);
+    }
+
+    @Override
+    public void navigate(@NonNull final Context context, @NonNull final Geocache cache) {
+        final Geopoint coords = cache.getCoords();
+        assert coords != null; // asserted by caller
+        navigate(context, coords, cache.getName());
+    }
+
+    @Override
+    public void navigate(@NonNull final Context context, @NonNull final Waypoint waypoint) {
+        final Geopoint coords = waypoint.getCoords();
+        assert coords != null; // asserted by caller
+        navigate(context, coords, waypoint.getName());
+    }
+
+    private void navigate(@NonNull final Context context, @NonNull final Geopoint coords, @Nullable final String info) {
         final GeoData geo = LocationDataProvider.getInstance().currentGeo();
         final Intent intent = new Intent();
         intent.setAction(ACTION);
         intent.setPackage(getString(R.string.package_cruiser));
         intent.putExtra("LATITUDE", new double[]{geo.getLatitude(), coords.getLatitude()});
         intent.putExtra("LONGITUDE", new double[]{geo.getLongitude(), coords.getLongitude()});
+        if (StringUtils.isNotBlank(info)) {
+            intent.putExtra("NAME", new String[] {info});
+        }
         context.startActivity(intent);
     }
-
 }
