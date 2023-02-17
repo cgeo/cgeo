c1de4c663a08243ae715ecbcd8a7494fb9cbd665
Eddie <andreas-kopp@gmx.net>
2023-02-18 02:15:01 +0800

    fix #13979: catch OutOfMemoryError on opening very large track files (#13981)
    Co-authored-by: eddiemuc <eddiemuc@cgeo.org>

--- main/src/main/java/cgeo/geocaching/utils/EnvironmentUtils.java
+++ main/src/main/java/cgeo/geocaching/utils/EnvironmentUtils.java
@@ -1,7 +1,12 @@
 package cgeo.geocaching.utils;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Build;
 import android.os.Environment;
+import static android.content.Context.ACTIVITY_SERVICE;
+
+import androidx.annotation.Nullable;
 
 public class EnvironmentUtils {
     private static final boolean DEFAULT_TRANSPARENT_BACKGROUND;
@@ -24,5 +29,25 @@ public class EnvironmentUtils {
 
     public static boolean defaultBackgroundTransparent() {
         return DEFAULT_TRANSPARENT_BACKGROUND;
+    }
+
+    @Nullable
+    public static ActivityManager.MemoryInfo getMemoryInfo(final Context context) {
+        if (context == null) {
+            return null;
+        }
+        final ActivityManager activityManager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        if (activityManager == null) {
+            return null;
+        }
+        final ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        return memoryInfo;
+    }
+
+    public static long getFreeMemory(final Context context) {
+        final ActivityManager.MemoryInfo memInfo = getMemoryInfo(context);
+        return memInfo == null ? -1 : memInfo.availMem;
+
     }
 }
