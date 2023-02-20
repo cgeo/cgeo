7cff3477e4416f04ba33024e75290f9c1d233643
moving-bits <github@moving-bits.net>
2023-02-20 21:26:49 +0100

    Update map list on receiving map file (fix #14003)

--- main/src/main/java/cgeo/geocaching/downloader/AbstractMapDownloader.java
+++ main/src/main/java/cgeo/geocaching/downloader/AbstractMapDownloader.java
@@ -1,16 +1,20 @@
 package cgeo.geocaching.downloader;
 
+import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.R;
 import cgeo.geocaching.maps.mapsforge.MapsforgeMapProvider;
 import cgeo.geocaching.models.Download;
 import cgeo.geocaching.storage.ContentStorage;
 import cgeo.geocaching.storage.PersistableFolder;
 import cgeo.geocaching.utils.FileUtils;
+import static cgeo.geocaching.Intents.ACTION_INVALIDATE_MAPLIST;
 
+import android.content.Intent;
 import android.net.Uri;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.util.List;
 
@@ -28,6 +32,7 @@ abstract class AbstractMapDownloader extends AbstractDownloader {
     protected void onSuccessfulReceive(final Uri result) {
         // update offline maps
         MapsforgeMapProvider.getInstance().updateOfflineMaps(result);
+        LocalBroadcastManager.getInstance(CgeoApplication.getInstance()).sendBroadcast(new Intent(ACTION_INVALIDATE_MAPLIST));
     }
 
     /**
