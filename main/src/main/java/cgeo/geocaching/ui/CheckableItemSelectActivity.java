654e8ba6989d69baff149697ef7e04bfedd43847
moving-bits <github@moving-bits.net>
2023-03-15 10:08:54 +0100

    Two row-layout for CheckableItemSelectActivity (fix #14087)

--- main/src/main/java/cgeo/geocaching/ui/CheckableItemSelectActivity.java
+++ main/src/main/java/cgeo/geocaching/ui/CheckableItemSelectActivity.java
@@ -1,13 +1,16 @@
 package cgeo.geocaching.ui;
 
+import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.Intents;
 import cgeo.geocaching.R;
 import cgeo.geocaching.activity.AbstractActionBarActivity;
+import cgeo.geocaching.databinding.CheckableItemSelectActivityBinding;
 import cgeo.geocaching.databinding.InfoItemBinding;
 import cgeo.geocaching.models.InfoItem;
 import cgeo.geocaching.settings.Settings;
 import cgeo.geocaching.ui.recyclerview.AbstractRecyclerViewHolder;
 import cgeo.geocaching.ui.recyclerview.ManagedListAdapter;
+import cgeo.geocaching.utils.DisplayUtils;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -37,35 +40,28 @@ public class CheckableItemSelectActivity extends AbstractActionBarActivity {
         }
     }
 
-    private static final class InfoItemListAdapter extends ManagedListAdapter<Element, InfoItemViewHolder> {
+    private static final class InfoItemListAdapter extends ManagedListAdapter<InfoItem, InfoItemViewHolder> {
 
         private InfoItemListAdapter(final RecyclerView recyclerView) {
             super(new ManagedListAdapter.Config(recyclerView).setSupportDragDrop(true));
         }
 
-        private void fillViewHolder(final InfoItemViewHolder holder, final Element item) {
-            holder.binding.title.setText(item.item.getTitleResId());
-            if (item.isSelected) {
-                holder.binding.remove.setVisibility(View.VISIBLE);
-                holder.binding.remove.setOnClickListener((view) -> {
-                    item.isSelected = false;
-                    fillViewHolder(holder, item);
-                });
-                holder.binding.add.setVisibility(View.GONE);
+        private void fillViewHolder(final InfoItemViewHolder holder, final InfoItem item) {
+            if (holder.getItemViewType() != 0) {
+                holder.binding.title.setText(item.getTitleResId());
+                holder.binding.drag.setVisibility(View.VISIBLE);
             } else {
-                holder.binding.add.setVisibility(View.VISIBLE);
-                holder.binding.add.setOnClickListener((view) -> {
-                    item.isSelected = true;
-                    fillViewHolder(holder, item);
-                });
-                holder.binding.remove.setVisibility(View.GONE);
+                holder.binding.title.setText(R.string.disabled_elements);
             }
         }
 
         @NonNull
         @Override
         public InfoItemViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
-            final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.info_item, parent, false);
+            final View view = LayoutInflater.from(parent.getContext()).inflate(viewType == 0 ? R.layout.info_header : R.layout.info_item, parent, false);
+            if (viewType != 0) {
+                view.setPaddingRelative(DisplayUtils.getPxFromDp(CgeoApplication.getInstance().getResources(), 20f, 1f), 0, 0, 0);
+            }
             final InfoItemViewHolder viewHolder = new InfoItemViewHolder(view);
             registerStartDrag(viewHolder, viewHolder.binding.drag);
             return viewHolder;
@@ -75,15 +71,10 @@ public class CheckableItemSelectActivity extends AbstractActionBarActivity {
         public void onBindViewHolder(@NonNull final InfoItemViewHolder holder, final int position) {
             fillViewHolder(holder, getItem(position));
         }
-    }
 
-    private static final class Element {
-        public InfoItem item;
-        public boolean isSelected;
-
-        Element(final InfoItem item, final boolean isSelected) {
-            this.item = item;
-            this.isSelected = isSelected;
+        @Override
+        public int getItemViewType(final int position) {
+            return getItem(position) == null ? 0 : 1;
         }
     }
 
@@ -92,6 +83,9 @@ public class CheckableItemSelectActivity extends AbstractActionBarActivity {
         super.onCreate(savedInstanceState);
         setTheme();
         setUpNavigationEnabled(true);
+        final CheckableItemSelectActivityBinding binding = CheckableItemSelectActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.headerSelected.title.setText(R.string.active_elements);
 
         final Bundle bundle = getIntent().getExtras();
         setTitle(bundle.getInt(Intents.EXTRA_TITLE));
@@ -100,24 +94,19 @@ public class CheckableItemSelectActivity extends AbstractActionBarActivity {
 
         ArrayList<InfoItem> external = null;
         try {
-            Class myClass = null;
-            myClass = Class.forName(infoItemClass);
-            final Field f = myClass.getDeclaredField(bundle.getString(Intents.EXTRA_FIELD));
+            final Field f = Class.forName(infoItemClass).getDeclaredField(bundle.getString(Intents.EXTRA_FIELD));
             external = (ArrayList<InfoItem>) f.get(null);
         } catch (ClassNotFoundException | IllegalAccessException | NoSuchFieldException ignore) {
-            //
         }
         assert (external != null);
 
-        final RecyclerView listView = new RecyclerView(this, null);
-        setContentView(listView);
-
         final ArrayList<Integer> includedItems = Settings.getInfoItems(prefKey, 0);
 
-        final ArrayList<Element> allItems = new ArrayList<>();
+        final ArrayList<InfoItem> allItems = new ArrayList<>();
         for (int i : includedItems) {
-            allItems.add(new Element(InfoItem.getById(i, external), true));
+            allItems.add(InfoItem.getById(i, external));
         }
+        allItems.add(null); // marker for: disabled elements from here
         for (InfoItem item : external) {
             boolean found = false;
             for (int i : includedItems) {
@@ -127,11 +116,11 @@ public class CheckableItemSelectActivity extends AbstractActionBarActivity {
                 }
             }
             if (!found) {
-                allItems.add(new Element(item, false));
+                allItems.add(item);
             }
         }
 
-        infoItemListAdapter = new InfoItemListAdapter(listView);
+        infoItemListAdapter = new InfoItemListAdapter(binding.recyclerView);
         infoItemListAdapter.setItems(allItems);
     }
 
@@ -147,10 +136,11 @@ public class CheckableItemSelectActivity extends AbstractActionBarActivity {
     @Override
     public void onBackPressed() {
         final ArrayList<Integer> selected = new ArrayList<>();
-        for (Element item : infoItemListAdapter.getItems()) {
-            if (item.isSelected) {
-                selected.add(item.item.getId());
+        for (InfoItem item : infoItemListAdapter.getItems()) {
+            if (item == null) {
+                break;
             }
+            selected.add(item.getId());
         }
         Settings.setInfoItems(prefKey, selected);
         finish();
