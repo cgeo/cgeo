78643204dd0c36a856f564ce655be9bf2acd1412
moving-bits <github@moving-bits.net>
2023-08-20 20:32:59 +0200

    Revert "Update GM to latest renderer (when available) (fix #12023)"
    This reverts commit 004e2e55d9abd5c16fd19986f58da3086f903f1f.

--- main/src/main/java/cgeo/geocaching/unifiedmap/tileproviders/AbstractGoogleTileProvider.java
+++ main/src/main/java/cgeo/geocaching/unifiedmap/tileproviders/AbstractGoogleTileProvider.java
@@ -3,15 +3,12 @@ package cgeo.geocaching.unifiedmap.tileproviders;
 import cgeo.geocaching.CgeoApplication;
 import cgeo.geocaching.unifiedmap.AbstractMapFragment;
 import cgeo.geocaching.unifiedmap.googlemaps.GoogleMapsFragment;
-import cgeo.geocaching.utils.Log;
 
 import androidx.annotation.StringRes;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapsSdkInitializedCallback;
 
-public class AbstractGoogleTileProvider extends AbstractTileProvider implements OnMapsSdkInitializedCallback {
+public class AbstractGoogleTileProvider extends AbstractTileProvider {
 
     final int mapType;
 
@@ -29,23 +26,7 @@ public class AbstractGoogleTileProvider extends AbstractTileProvider implements 
 
     @Override
     public AbstractMapFragment createMapFragment() {
-        MapsInitializer.initialize(CgeoApplication.getInstance(), MapsInitializer.Renderer.LATEST, this);
         return new GoogleMapsFragment();
     }
 
-    @Override
-    public void onMapsSdkInitialized(final MapsInitializer.Renderer renderer) {
-        switch (renderer) {
-            case LATEST:
-                Log.d("GMv2: The latest version of the renderer is used.");
-                break;
-            case LEGACY:
-                Log.d("GMv2: The legacy version of the renderer is used.");
-                break;
-            default:
-                // to make Codacy happy...
-                Log.w("GMv2: Unknown renderer version used, neither LATEST nor LEGACY.");
-                break;
-        }
-    }
 }
