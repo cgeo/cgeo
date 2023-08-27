3fa95de71709632f09cdb4edc336af8664a7c31d
moving-bits <github@moving-bits.net>
2023-08-27 12:45:19 +0200

    Remove unused local var in SettingsActivity

--- main/src/main/java/cgeo/geocaching/settings/SettingsActivity.java
+++ main/src/main/java/cgeo/geocaching/settings/SettingsActivity.java
@@ -83,7 +83,6 @@ public class SettingsActivity extends AppCompatActivity implements PreferenceFra
     public static final int RESTART_NEEDED = 2;
 
     public static final String STATE_CSAH = "csah";
-    public static final String STATE_CSAH_PN = "csahpn";
     public static final String STATE_BACKUPUTILS = "backuputils";
 
     private BackupUtils backupUtils = null;
