610edc74d09652c43c7c2349ae69d536502154d1
moving-bits <github@moving-bits.net>
2023-09-26 22:40:05 +0200

    Prepare version number for next release

--- main/src/main/java/cgeo/geocaching/utils/BranchDetectionHelper.java
+++ main/src/main/java/cgeo/geocaching/utils/BranchDetectionHelper.java
@@ -9,7 +9,7 @@ public class BranchDetectionHelper {
 
     // should contain version names of active bugfix releases since last feature release, oldest first
     // empty the part within curly brackets when creating a new release branch from master
-    public static final String[] BUGFIX_VERSION_NAME = new String[]{ "2023.08.24", "2023.09.22-RC" };
+    public static final String[] BUGFIX_VERSION_NAME = new String[]{ "2023.08.24", "2023.09.26" };
 
     private BranchDetectionHelper() {
         // utility class
