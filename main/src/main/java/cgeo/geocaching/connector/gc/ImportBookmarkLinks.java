0b7d1ebe616276605096844f7242657b3402893f
Morten Bruhn <11851157+mortenbruhn@users.noreply.github.com>
2023-11-01 18:34:55 +0100

    Named capturing groups

--- main/src/main/java/cgeo/geocaching/connector/gc/ImportBookmarkLinks.java
+++ main/src/main/java/cgeo/geocaching/connector/gc/ImportBookmarkLinks.java
@@ -84,11 +84,11 @@ public class ImportBookmarkLinks extends AppCompatActivity {
     static UrlToIdParser defaultBookmarkListUrlToIdParser() {
         return new AggregatedUrlToIdParser(
                 new SingleUrlToIdParser(
-                        "^https?://(?:www\\.)?geocaching\\.com/plan/lists/([A-Z0-9]+)",
+                        "^https?://(?:www\\.)?geocaching\\.com/plan/lists/(?<identifier>[A-Z0-9]+)",
                         1
                 ),
                 new SingleUrlToIdParser(
-                        "^https?://(?:www\\.)?coord\\.info/([A-Z0-9]+)",
+                        "^https?://(?:www\\.)?coord\\.info/(?<identifier>[A-Z0-9]+)",
                         1
                 )
         );
