90c48824a8e5d6badd33905ab2b0505d738509dd
murggel <murggel@web.de>
2023-12-11 01:51:16 +0100

    Remove Geocache.isPastEvent

--- main/src/main/java/cgeo/geocaching/models/Geocache.java
+++ main/src/main/java/cgeo/geocaching/models/Geocache.java
@@ -516,17 +516,6 @@ public class Geocache implements IWaypoint {
         return isEventCache() && hidden != null;
     }
 
-    public boolean isPastEvent() {
-        final Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-        assert hidden != null; // Android Studio compiler issue
-        return hidden.compareTo(cal.getTime()) < 0;
-    }
-
     public boolean isEventCache() {
         return cacheType.isEvent();
     }
