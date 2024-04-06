5f8bc44a5576deb0cc3b9df8e9f1bc2cc893f62f
moving-bits <github@moving-bits.net>
2024-04-06 13:58:42 +0200

    UnifiedMap VTM: Disable auto rotation by tapping on compass rose

--- main/src/main/java/cgeo/geocaching/unifiedmap/mapsforgevtm/MapsforgeVtmFragment.java
+++ main/src/main/java/cgeo/geocaching/unifiedmap/mapsforgevtm/MapsforgeVtmFragment.java
@@ -18,6 +18,7 @@ import cgeo.geocaching.unifiedmap.tileproviders.AbstractTileProvider;
 import cgeo.geocaching.utils.AngleUtils;
 import cgeo.geocaching.utils.GroupedList;
 import cgeo.geocaching.utils.ImageUtils;
+import static cgeo.geocaching.storage.extension.OneTimeDialogs.DialogType.MAP_AUTOROTATION_DISABLE;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
@@ -177,8 +178,12 @@ public class MapsforgeVtmFragment extends AbstractMapFragment {
             matrix.setRotate(bearing, rotationWidth / 2.0f, rotationHeight / 2.0f);
             compassrose.setImageBitmap(Bitmap.createBitmap(rotationIndicator, 0, 0, rotationWidth, rotationHeight, matrix, true));
             compassrose.setOnClickListener(v -> {
+                final boolean isRotated = getCurrentBearing() != 0f;
                 setBearing(0.0f);
                 repaintRotationIndicator(0.0f);
+                if (isRotated && (Settings.getMapRotation() == Settings.MAPROTATION_AUTO_LOWPOWER || Settings.getMapRotation() == Settings.MAPROTATION_AUTO_PRECISE)) {
+                    Dialogs.advancedOneTimeMessage(getContext(), MAP_AUTOROTATION_DISABLE, getString(MAP_AUTOROTATION_DISABLE.messageTitle), getString(MAP_AUTOROTATION_DISABLE.messageText), "", true, null, () -> Settings.setMapRotation(Settings.MAPROTATION_MANUAL));
+                }
             });
         }
 
