bf187009dfa4cd86a9f4c5d7d47b5a080fd4ca9a
fm-sys <64581222+fm-sys@users.noreply.github.com>
2024-05-12 21:30:43 +0200

    developer app id gradle script enhancements

--- main/src/androidTest/java/cgeo/geocaching/ExampleInstrumentationTest.java
+++ main/src/androidTest/java/cgeo/geocaching/ExampleInstrumentationTest.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 //@RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentationTest {
@@ -13,8 +13,6 @@ public class ExampleInstrumentationTest {
     public void useAppContext() {
         // Context of the app under test.
         final Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("cgeo.geocaching", appContext.getPackageName());
+        assertTrue("cgeo.geocaching".equals(appContext.getPackageName()) || "cgeo.geocaching.developer".equals(appContext.getPackageName()));
     }
-
 }
-
