d8bde0ce1729dbb0a2e2abfbae698e6c35d09136
moving-bits <github@moving-bits.net>
2024-09-28 21:24:54 +0200

    Remove exceptions declarations which are too specific

--- main/src/main/java/cgeo/geocaching/network/Tls12SocketFactory.java
+++ main/src/main/java/cgeo/geocaching/network/Tls12SocketFactory.java
@@ -43,12 +43,12 @@ public class Tls12SocketFactory extends SSLSocketFactory {
     }
 
     @Override
-    public Socket createSocket(final String host, final int port) throws IOException, UnknownHostException {
+    public Socket createSocket(final String host, final int port) throws IOException {
         return patch(delegate.createSocket(host, port));
     }
 
     @Override
-    public Socket createSocket(final String host, final int port, final InetAddress localHost, final int localPort) throws IOException, UnknownHostException {
+    public Socket createSocket(final String host, final int port, final InetAddress localHost, final int localPort) throws IOException {
         return patch(delegate.createSocket(host, port, localHost, localPort));
     }
 
