aabaf088846b0091e88e6217e819726745cae019
ztNFny <ztNFny@users.noreply.github.com>
2025-01-18 00:14:18 +0100

    add parser for DD,DDDDDDD format (#16486)
    * add parser for DD,DDDDDDD format
    * tweak parser for less false positives
    * Improve regexp and add more test
    * Support "+" as sign for coordinates
    
    Co-authored-by: murggel <murggel@web.de>

--- main/src/main/java/cgeo/geocaching/location/GeopointParser.java
+++ main/src/main/java/cgeo/geocaching/location/GeopointParser.java
@@ -22,10 +22,10 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class GeopointParser {
 
-    private static final Pattern PATTERN_BAD_BLANK_COMMA = Pattern.compile("(\\d), (\\d{2,})");
-    private static final Pattern PATTERN_BAD_BLANK_DOT = Pattern.compile("(\\d)\\. (\\d{2,})");
+    private static final Pattern PATTERN_BAD_BLANK_COMMA = Pattern.compile("(\\d), ([-+]?\\d{2,})");
+    private static final Pattern PATTERN_BAD_BLANK_DOT = Pattern.compile("(\\d)\\. ([-+]?\\d{2,})");
 
-    private static final List<AbstractParser> parsers = Arrays.asList(new MinDecParser(), new MinParser(), new DegParser(), new DMSParser(), new ShortDMSParser(), new DegDecParser(), new ShortDegDecParser(), new UTMParser());
+    private static final List<AbstractParser> parsers = Arrays.asList(new MinDecParser(), new MinParser(), new DegParser(), new DMSParser(), new ShortDMSParser(), new DegDecParser(), new ShortDegDecParser(), new UTMParser(), new DegDecCommaParser());
 
     private GeopointParser() {
         // utility class
@@ -404,6 +404,35 @@ public class GeopointParser {
         @Nullable
         public Double parse(@NonNull final List<String> groups) {
             final String group1 = groups.get(0);
+            return createCoordinate("", group1, "", "");
+        }
+    }
+
+    /**
+     * Parser for DegDec format: DD,DDDDDDD°.
+     */
+    private static final class DegDecCommaParser extends AbstractLatLonParser {
+        //                                        (     1     ) , (    2   )
+        private static final String STRING_LAT = "([-+]?\\d{1,2}+),(\\d{5,}+)°?";
+
+        //                                        (     1     ) , (    2   )
+        private static final String STRING_LON = "([-+]?\\d{1,3}+),(\\d{5,}+)\\b°?";
+        private static final String STRING_SEPARATOR = ",";
+        private static final Pattern PATTERN_LAT = Pattern.compile(STRING_LAT, Pattern.CASE_INSENSITIVE);
+        private static final Pattern PATTERN_LON = Pattern.compile(STRING_LON, Pattern.CASE_INSENSITIVE);
+        private static final Pattern PATTERN_LATLON = Pattern.compile(STRING_LAT + STRING_SEPARATOR + STRING_LON, Pattern.CASE_INSENSITIVE);
+
+        DegDecCommaParser() {
+            super(PATTERN_LAT, PATTERN_LON, PATTERN_LATLON);
+        }
+
+        /**
+         * @see AbstractLatLonParser#parse(List)
+         */
+        @Override
+        @Nullable
+        public Double parse(@NonNull final List<String> groups) {
+            final String group1 = groups.get(0) + "." + groups.get(1);
             return createCoordinate("", group1, "", "");
         }
     }
