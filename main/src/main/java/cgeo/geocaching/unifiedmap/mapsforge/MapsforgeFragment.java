eca9c36332631e2cdcda7a24867d5ec2e3c05549
moving-bits <github@moving-bits.net>
2025-04-06 00:21:17 +0200

    Restart UnifiedMap Mapsforge on language switch (fix #16806)

--- main/src/main/java/cgeo/geocaching/unifiedmap/mapsforge/MapsforgeFragment.java
+++ main/src/main/java/cgeo/geocaching/unifiedmap/mapsforge/MapsforgeFragment.java
@@ -427,6 +427,7 @@ public class MapsforgeFragment extends AbstractMapFragment implements Observer {
     @Override
     public void setPreferredLanguage(final String language) {
         currentTileProvider.setPreferredLanguage(language);
+        requireActivity().recreate();
     }
 
 
