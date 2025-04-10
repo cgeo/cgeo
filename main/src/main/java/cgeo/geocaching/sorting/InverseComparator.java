64075127371bc7d9edd7019b6d3d6351b1f36979
murggel <murggel@web.de>
2025-04-10 03:08:26 +0200

    Do beforeSort

--- main/src/main/java/cgeo/geocaching/sorting/InverseComparator.java
+++ main/src/main/java/cgeo/geocaching/sorting/InverseComparator.java
@@ -31,7 +31,15 @@ public class InverseComparator implements CacheComparator {
 
     @Override
     public void sort(final List<Geocache> list) {
+        if (originalComparator instanceof AbstractCacheComparator) {
+            ((AbstractCacheComparator) originalComparator).beforeSort(list);
+        }
+
         Collections.sort(list, this);
+
+        if (originalComparator instanceof AbstractCacheComparator) {
+            ((AbstractCacheComparator) originalComparator).afterSort(list);
+        }
     }
 
     @Override
