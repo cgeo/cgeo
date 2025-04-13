44f0bb1be3480357e66fc945bdc40054251f6e0e
moving-bits <github@moving-bits.net>
2025-04-13 19:43:39 +0200

    Altitude conversion for MSL values (fix #16724)

--- main/src/main/java/cgeo/geocaching/utils/GeoHeightUtils.java
+++ main/src/main/java/cgeo/geocaching/utils/GeoHeightUtils.java
@@ -1,14 +1,28 @@
 package cgeo.geocaching.utils;
 
+import cgeo.geocaching.CgeoApplication;
+import cgeo.geocaching.location.Geopoint;
 import cgeo.geocaching.location.Units;
 import cgeo.geocaching.sensors.GeoData;
 
 import android.location.Location;
 import android.os.Build;
 
+import androidx.core.location.LocationCompat;
+import androidx.core.location.altitude.AltitudeConverterCompat;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class GeoHeightUtils {
     private static final double[] altitudeReadings = {0.0d, 0.0d, 0.0d, 0.0d, 0.0d};
     private static int altitudeReadingPos = 0;
+
+    // msl correction (WGS84 model to actual form)
+    private static final float MAX_DISTANCE = 0.1f; // max allowed distance in km to accept last msl reading
+    private static double mslCorrection = Double.NaN;
+    private static Geopoint mslLastReading;
+    private static AtomicBoolean mslRetrievalOngoing = new AtomicBoolean(false);
 
     private GeoHeightUtils() {
         // utility class
@@ -36,11 +50,29 @@ public class GeoHeightUtils {
 
     /** returns altitude in meters; prefers msl altitude if available */
     public static double getAltitude(final Location geo) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-            if (geo.hasMslAltitude()) {
-                return geo.getMslAltitudeMeters();
+        if (Double.isNaN(mslCorrection) || (mslLastReading != null && mslLastReading.distanceTo(new Geopoint(geo.getLatitude(), geo.getLongitude())) > MAX_DISTANCE)) {
+            if (!mslRetrievalOngoing.get()) {
+                // trigger download of msl correction, if not yet available
+                mslRetrievalOngoing.set(true);
+                final Location dummy = new Location(geo);
+                dummy.setAltitude(0d);
+                AndroidRxUtils.networkScheduler.scheduleDirect(() -> {
+                    try {
+                        AltitudeConverterCompat.addMslAltitudeToLocation(CgeoApplication.getInstance().getApplicationContext(), dummy);
+                        if (LocationCompat.hasMslAltitude(dummy)) {
+                            mslCorrection = LocationCompat.getMslAltitudeMeters(dummy);
+                            mslLastReading = new Geopoint(geo.getLatitude(), geo.getLongitude());
+                        }
+                    } catch (IOException ignore) {
+                        // ignore
+                    } finally {
+                        mslRetrievalOngoing.set(false);
+                    }
+                });
             }
+            return geo.getAltitude();
+        } else {
+            return geo.getAltitude() + mslCorrection;
         }
-        return geo.getAltitude();
     }
 }
