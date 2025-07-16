6ad29944ffc9032ebf391eba2968d2cff0e16868
moving-bits <github@moving-bits.net>
2025-07-16 22:50:58 +0200

    Remove unused import

--- main/src/main/java/cgeo/geocaching/InstallWizardActivity.java
+++ main/src/main/java/cgeo/geocaching/InstallWizardActivity.java
@@ -25,7 +25,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
