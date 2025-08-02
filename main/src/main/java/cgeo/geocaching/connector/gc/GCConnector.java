1b23d873c5126eea6138ab2fd39838ab37242bb6
murggel <murggel@web.de>
2025-08-02 16:45:20 +0200

    Fix condition for deleting logs

--- main/src/main/java/cgeo/geocaching/connector/gc/GCConnector.java
+++ main/src/main/java/cgeo/geocaching/connector/gc/GCConnector.java
@@ -220,15 +220,15 @@ public class GCConnector extends AbstractConnector implements ISearchByGeocode, 
 
     @Override
     public boolean canEditLog(final Geocache cache, final LogEntry logEntry) {
-        //needs to be log author
+        // needs to be online and needs to be log author
         return !StringUtils.isBlank(logEntry.serviceLogId) && logEntry.author.equalsIgnoreCase(getUserName());
     }
 
     @Override
     public boolean canDeleteLog(final Geocache cache, final LogEntry logEntry) {
-        //needs to be log author or cache owner
+        //needs to be online and needs to be log author or cache owner
         return !StringUtils.isBlank(logEntry.serviceLogId) &&
-            logEntry.author.equalsIgnoreCase(getUserName()) || cache.getOwnerUserId().equals(getUserName());
+                (logEntry.author.equalsIgnoreCase(getUserName()) || cache.getOwnerUserId().equals(getUserName()));
     }
 
     @Override
