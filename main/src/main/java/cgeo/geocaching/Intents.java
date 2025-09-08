e5665a10f51fd1969608afa3b3fc0db851bbf4d9
murggel <murggel@web.de>
2025-09-08 18:59:35 +0200

    fix #11763: Allow multi-selection for pocket queries

--- main/src/main/java/cgeo/geocaching/Intents.java
+++ main/src/main/java/cgeo/geocaching/Intents.java
@@ -60,8 +60,7 @@ public class Intents {
     public static final String EXTRA_TRACKING_CODE = PREFIX + "tracking_code";
     public static final String EXTRA_USERNAME = PREFIX + "username";
     public static final String EXTRA_WAYPOINT_ID = PREFIX + "waypoint_id";
-    public static final String EXTRA_POCKET_GUID = PREFIX + "pocket_guid";
-    public static final String EXTRA_POCKET_HASH = PREFIX + "pocket_hash";
+    public static final String EXTRA_POCKET_LIST = PREFIX + "pocket_list";
 
     private static final String PREFIX_ACTION = "cgeo.geocaching.intent.action.";
     public static final String ACTION_GEOCACHE = PREFIX_ACTION + "GEOCACHE";
