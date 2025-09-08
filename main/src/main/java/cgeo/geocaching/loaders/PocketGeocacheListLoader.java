ea060575c6c6c82f7fe5d2cba1bf22a300cee517
murggel <murggel@web.de>
2025-09-08 18:59:35 +0200

    Support list of GCList

--- main/src/main/java/cgeo/geocaching/loaders/PocketGeocacheListLoader.java
+++ main/src/main/java/cgeo/geocaching/loaders/PocketGeocacheListLoader.java
@@ -3,29 +3,38 @@ package cgeo.geocaching.loaders;
 import cgeo.geocaching.SearchResult;
 import cgeo.geocaching.connector.gc.GCConnector;
 import cgeo.geocaching.connector.gc.GCParser;
+import cgeo.geocaching.models.GCList;
 import cgeo.geocaching.settings.Settings;
 
 import android.app.Activity;
 
-public class PocketGeocacheListLoader extends AbstractSearchLoader {
-    private final String shortGuid;
-    private final String pqHash;
+import java.util.List;
 
-    public PocketGeocacheListLoader(final Activity activity, final String shortGuid, final String pqHash) {
+public class PocketGeocacheListLoader extends AbstractSearchLoader {
+    private final List<GCList> pocketQueries;
+
+    public PocketGeocacheListLoader(final Activity activity, final List<GCList> pocketQueries) {
         super(activity);
-        this.shortGuid = shortGuid;
-        this.pqHash = pqHash;
+        this.pocketQueries = pocketQueries;
     }
 
     @Override
     public SearchResult runSearch() {
 
         if (Settings.isGCConnectorActive()) {
-            return GCParser.searchByPocketQuery(GCConnector.getInstance(), shortGuid, pqHash);
+            final SearchResult combinedResult = new SearchResult();
+            for (final GCList pocketQuery : pocketQueries) {
+                if (pocketQuery.isBookmarkList()) {
+                    // final SearchResult bmResult = GCParser.searchByBookmarkList(GCConnector.getInstance(), pocketQuery.getGuid());
+                    // combinedResult.addSearchResult(bmResult);
+                } else {
+                    final SearchResult pqResult = GCParser.searchByPocketQuery(GCConnector.getInstance(), pocketQuery.getShortGuid(), pocketQuery.getPqHash());
+                    combinedResult.addSearchResult(pqResult);
+                }
+            }
+            return combinedResult;
         }
 
         return new SearchResult();
-
     }
-
 }
