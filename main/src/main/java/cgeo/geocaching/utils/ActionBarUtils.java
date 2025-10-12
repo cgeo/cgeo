d1046d70c658bfe9cdeb09b84ef7c04349fb0426
murggel <murggel@web.de>
2025-10-13 00:18:01 +0200

    rel #17545: adapt subtitle-color

--- main/src/main/java/cgeo/geocaching/utils/ActionBarUtils.java
+++ main/src/main/java/cgeo/geocaching/utils/ActionBarUtils.java
@@ -7,13 +7,20 @@ import cgeo.geocaching.settings.Settings;
 import cgeo.geocaching.ui.ViewUtils;
 
 import android.app.Activity;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.Window;
 
+import androidx.annotation.ColorRes;
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class ActionBarUtils {
 
@@ -91,5 +98,41 @@ public class ActionBarUtils {
             windowInsetsController.setAppearanceLightStatusBars(false);
             windowInsetsController.setAppearanceLightNavigationBars(false);
         }
+    }
+
+    public static void setSubtitle(@NonNull final AbstractActionBarActivity activity, @NonNull final CharSequence subtitleText) {
+        final ActionBar supportActionBar = activity.getSupportActionBar();
+        if (supportActionBar == null || StringUtils.isEmpty(subtitleText)) {
+            return;
+        }
+
+        final SpannableString titleString = getSpannedTitle(subtitleText, R.color.colorTextHintActionBar);
+        supportActionBar.setSubtitle(titleString);
+    }
+
+    public static void setTitle(@NonNull final AbstractActionBarActivity activity, @NonNull final CharSequence titleText) {
+        final ActionBar supportActionBar = activity.getSupportActionBar();
+        if (supportActionBar == null || StringUtils.isEmpty(titleText)) {
+            return;
+        }
+
+        final SpannableString titleString = getSpannedTitle(titleText, R.color.colorTextActionBar);
+        supportActionBar.setTitle(titleString);
+    }
+
+    // @todo remove after switching map ActionBar to Toolbar
+    // workaround for colored ActionBar titles/subtitles
+    // Checking for an existing span of the given class
+    private static SpannableString getSpannedTitle(final CharSequence spanText, final @ColorRes int colorRes) {
+        // // If a Spanned is already present, check whether a ForegroundColorSpan covers the entire text
+        if (TextUtils.hasSpanCoveringWholeText(spanText, ForegroundColorSpan.class)) {
+            return new SpannableString(spanText);
+        }
+
+        // Create new span with actionbar text color
+        final SpannableString titleString = new SpannableString(spanText);
+        final int color = ColorUtils.colorFromResource(colorRes);
+        titleString.setSpan(new ForegroundColorSpan(color), 0, titleString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return titleString;
     }
 }
