b3bb782efa52e835595a09bb6a7f4c9b47514e04
moving-bits <github@moving-bits.net>
2025-12-21 18:18:54 +0100

    Add NoMap tileprovider (fix #17436)
    (cherry picked from commit 78bcd66a374b04cc463b06efc3bd9fdd3a2ed7b7)

---
+++ main/src/main/java/cgeo/geocaching/unifiedmap/tileproviders/NoMapMapsforgeTileProvider.java
@@ -0,0 +1,22 @@
+package cgeo.geocaching.unifiedmap.tileproviders;
+
+import cgeo.geocaching.CgeoApplication;
+import cgeo.geocaching.R;
+import cgeo.geocaching.unifiedmap.mapsforge.MapsforgeFragment;
+
+import android.net.Uri;
+
+import androidx.core.util.Pair;
+
+import org.mapsforge.map.view.MapView;
+
+public class NoMapMapsforgeTileProvider extends AbstractMapsforgeTileProvider {
+    NoMapMapsforgeTileProvider() {
+        super(CgeoApplication.getInstance().getString(R.string.map_source_nomap), Uri.parse(""), 0, 18, new Pair<>("", false));
+    }
+
+    @Override
+    public void addTileLayer(final MapsforgeFragment fragment, final MapView map) {
+        // nothing to do
+    }
+}
