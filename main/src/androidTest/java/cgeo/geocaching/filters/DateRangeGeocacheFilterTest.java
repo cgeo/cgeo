d72e0615c9cd5d942373540d8f0639299a8a2972
eddiemuc <eddiemuc@cgeo.org>
2025-12-27 14:30:55 +0100

    #17614: Add 'Ignore year' option to date filters

--- main/src/androidTest/java/cgeo/geocaching/filters/DateRangeGeocacheFilterTest.java
+++ main/src/androidTest/java/cgeo/geocaching/filters/DateRangeGeocacheFilterTest.java
@@ -45,6 +45,88 @@ public class DateRangeGeocacheFilterTest {
         assertSingle(c -> c.setHidden(d), dd -> dd.setMinMaxDate(dDaySameLater, null), true);
     }
 
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    public void ignoreYearSameDayDifferentYears() throws Exception {
+        // Test that same day in different years matches when ignoreYear is true
+        final Date date2020 = FORMATTER.parse("2020-04-06");
+        final Date date2021 = FORMATTER.parse("2021-04-06");
+        final Date date2022 = FORMATTER.parse("2022-04-06");
+
+        assertSingle(c -> c.setHidden(date2020), dd -> {
+            dd.setMinMaxDate(date2021, date2021);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, true);
+        assertSingle(c -> c.setHidden(date2022), dd -> {
+            dd.setMinMaxDate(date2021, date2021);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, true);
+    }
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    public void ignoreYearDifferentDays() throws Exception {
+        // Test that different days don't match even with ignoreYear
+        final Date april6 = FORMATTER.parse("2022-04-06");
+        final Date april7 = FORMATTER.parse("2022-04-07");
+
+        assertSingle(c -> c.setHidden(april6), dd -> {
+            dd.setMinMaxDate(april7, april7);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, false);
+    }
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    public void ignoreYearRangeWithinYear() throws Exception {
+        // Test a range that doesn't wrap around year boundary
+        final Date march1 = FORMATTER.parse("2022-03-01");
+        final Date june30 = FORMATTER.parse("2022-06-30");
+        final Date april15 = FORMATTER.parse("2021-04-15");
+
+        // april15 should match because it's between march1 and june30 (ignoring year)
+        assertSingle(c -> c.setHidden(april15), dd -> {
+            dd.setMinMaxDate(march1, june30);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, true);
+
+        final Date february15 = FORMATTER.parse("2022-02-15");
+        // february15 should NOT match
+        assertSingle(c -> c.setHidden(february15), dd -> {
+            dd.setMinMaxDate(march1, june30);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, false);
+    }
+
+    @Test
+    @SuppressWarnings("PMD.JUnitTestsShouldIncludeAssert")
+    public void ignoreYearRangeWrappingYearBoundary() throws Exception {
+        // Test a range that wraps around year boundary (e.g., Nov to Feb)
+        final Date november1 = FORMATTER.parse("2022-11-01");
+        final Date february28 = FORMATTER.parse("2022-02-28");
+
+        // December should match
+        final Date december15 = FORMATTER.parse("2021-12-15");
+        assertSingle(c -> c.setHidden(december15), dd -> {
+            dd.setMinMaxDate(november1, february28);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, true);
+
+        // January should match
+        final Date january15 = FORMATTER.parse("2022-01-15");
+        assertSingle(c -> c.setHidden(january15), dd -> {
+            dd.setMinMaxDate(november1, february28);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, true);
+
+        // March should NOT match
+        final Date march15 = FORMATTER.parse("2022-03-15");
+        assertSingle(c -> c.setHidden(march15), dd -> {
+            dd.setMinMaxDate(november1, february28);
+            dd.getDateFilter().setIgnoreYear(true);
+        }, false);
+    }
+
     private void assertSingle(final Action1<Geocache> cacheSetter, final Action1<DateRangeGeocacheFilter> filterSetter, final Boolean expectedResult) {
         GeocacheFilterTestUtils.testSingle(GeocacheFilterType.HIDDEN, cacheSetter, filterSetter, expectedResult);
     }
