1a0ff8bccf78d85c46aca05e072618201045769a
Dave <dave.thiss@gmail.com>
2026-01-24 10:35:44 +0000

    Now uses getOriginalWaypoint as requested

--- main/src/main/java/cgeo/geocaching/export/GpxSerializer.java
+++ main/src/main/java/cgeo/geocaching/export/GpxSerializer.java
@@ -211,20 +211,15 @@ public final class GpxSerializer {
                 }
             }
         }
-        if (cache.hasUserModifiedCoords()) {
-            if (cache.hasWaypoints()) {
-                for (final Waypoint wp : cache.getWaypoints()) {
-                    if (wp.getWaypointType() == WaypointType.ORIGINAL) {
-                        if (wp.getCoords() != null) {
-                            gpx.startTag(NS_GSAK, "LatBeforeCorrect");
-                            gpx.text(Double.toString(wp.getCoords().getLatitude()));
-                            gpx.endTag(NS_GSAK, "LatBeforeCorrect");
-                            gpx.startTag(NS_GSAK, "LonBeforeCorrect");
-                            gpx.text(Double.toString(wp.getCoords().getLongitude()));
-                            gpx.endTag(NS_GSAK, "LonBeforeCorrect");
-                        }
-                    }
-                }
+        if (cache.hasUserModifiedCoords() && cache.hasWaypoints()) {
+            final Waypoint wp = cache.getOriginalWaypoint();
+            if (wp != null && wp.getCoords() != null) {
+                gpx.startTag(NS_GSAK, "LatBeforeCorrect");
+                gpx.text(Double.toString(wp.getCoords().getLatitude()));
+                gpx.endTag(NS_GSAK, "LatBeforeCorrect");
+                gpx.startTag(NS_GSAK, "LonBeforeCorrect");
+                gpx.text(Double.toString(wp.getCoords().getLongitude()));
+                gpx.endTag(NS_GSAK, "LonBeforeCorrect");
             }
         }
         gpx.endTag(NS_GSAK, "wptExtension");
