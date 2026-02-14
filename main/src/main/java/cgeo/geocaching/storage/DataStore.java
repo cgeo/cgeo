88ba0c5ff8a3379e488288d4281cd1acf1ec2319
murggel <murggel@web.de>
2026-02-14 22:49:36 +0100

    Add test

--- main/src/main/java/cgeo/geocaching/storage/DataStore.java
+++ main/src/main/java/cgeo/geocaching/storage/DataStore.java
@@ -2915,7 +2915,7 @@ public class DataStore {
                     values.put("geocode", geocode);
                 } else {
                     final String spottedGeocode = trackable.getSpottedCacheGeocode();
-                    if (spottedGeocode != null) {
+                    if (StringUtils.isNotBlank(spottedGeocode)) {
                         values.put("geocode", spottedGeocode);
                     }
                 }
