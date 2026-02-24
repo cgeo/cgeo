676254665ca04a5f1c16aa112cec87fa385a44f7
bekuno <bekuno@sags-per-mail.de>
2026-02-24 22:16:00 +0100

    replace deprecated IOUtils method closeQuietly
    from
    `import org.apache.commons.compress.utils.IOUtils`
    to `import org.apache.commons.io.IOUtils`

--- main/src/main/java/cgeo/geocaching/network/Network.java
+++ main/src/main/java/cgeo/geocaching/network/Network.java
@@ -46,7 +46,7 @@ import okhttp3.Request;
 import okhttp3.Request.Builder;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.apache.commons.compress.utils.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Strings;
 import org.apache.commons.lang3.tuple.ImmutablePair;
