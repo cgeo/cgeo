b4cafc356404feeab1f08d71229d1b43c78093ff
Joel <40491616+Joelsuperstar@users.noreply.github.com>
2026-05-24 23:21:06 +0200

    Fix Codacy import order in coordinates calculator dialog

--- main/src/main/java/cgeo/geocaching/ui/dialog/CoordinatesCalculateGlobalDialog.java
+++ main/src/main/java/cgeo/geocaching/ui/dialog/CoordinatesCalculateGlobalDialog.java
@@ -57,9 +57,9 @@ import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Set;
-import java.util.Locale;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
