a7e42d395b1b74cdd80e25c140dd1fe531a6d4a0
eddiemuc <eddiemuc@cgeo.org>
2026-06-18 08:57:02 +0200

    rel to #18249: fix changed GC Log API

--- main/src/androidTest/java/cgeo/geocaching/test/mock/GC2JVEH.java
+++ main/src/androidTest/java/cgeo/geocaching/test/mock/GC2JVEH.java
@@ -86,7 +86,7 @@ public class GC2JVEH extends MockedCache {
     @NonNull
     @Override
     public String getDescription() {
-        return "<img src=\"http://img.geocaching.com/cache/large/1711f8a1-796a-405b-82ba-8685f2e9f024.jpg\">";
+        return "<img src=\"http://img.geocaching.com/cache/large/1711f8a1-796a-405b-82ba-8685f2e9f024.jpg\"";
     }
 
     @NonNull
