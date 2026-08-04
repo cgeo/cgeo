5576e219dba4e80ded6ee54b782a84a99ffe13f3
ztNFny <ztNFny@users.noreply.github.com>
2026-08-04 11:57:45 +0200

    fix (re)setting corrected coordinates

--- main/src/main/java/cgeo/geocaching/connector/gc/GCParser.java
+++ main/src/main/java/cgeo/geocaching/connector/gc/GCParser.java
@@ -1723,30 +1723,36 @@ public final class GCParser {
 
     @NonNull
     static Single<Boolean> editModifiedCoordinates(@NonNull final Geocache cache, final Geopoint wpt) {
-        final String userToken = getUserToken(cache);
-        if (StringUtils.isEmpty(userToken)) {
+        final String token = getRequestVerificationToken(cache);
+        if (StringUtils.isEmpty(token)) {
             return Single.just(false);
         }
 
-        final ObjectNode jo = new ObjectNode(JsonUtils.factory);
-        final ObjectNode dto = jo.putObject("dto").put("ut", userToken);
-        if (wpt != null) {
-            dto.putObject("data").put("lat", wpt.getLatitudeE6() / 1E6).put("lng", wpt.getLongitudeE6() / 1E6);
+        final Parameters headers = new Parameters("__requestverificationtoken", token);
+        final ObjectNode jo = new ObjectNode(JsonUtils.factory).put("cacheId", cache.getCacheId());
+
+        final Single<Response> response;
+        try {
+            if (wpt != null) {
+                jo.put("lat", wpt.getLatitudeE6() / 1E6).put("lng", wpt.getLongitudeE6() / 1E6);
+                response = Network.postJsonRequest("https://www.geocaching.com/seek/geocache.usercoordinate", headers, jo);
+            } else {
+                response = Network.deleteJsonRequest("https://www.geocaching.com/seek/geocache.usercoordinate", headers, jo);
+            }
+        } catch (final Exception e) {
+            Log.e("GCParser.editModifiedCoordinates - cannot modify coordinates", e);
+            return Single.just(false);
         }
 
-        final String uriSuffix = wpt != null ? "SetUserCoordinate" : "ResetUserCoordinate";
-
-        final String uriPrefix = "https://www.geocaching.com/seek/cache_details.aspx/";
-
-        return Network.completeWithSuccess(Network.postJsonRequest(uriPrefix + uriSuffix, jo))
-            .toSingle(() -> {
-                Log.i("GCParser.editModifiedCoordinates - edited on GC.com");
-                return true;
-            })
-            .onErrorReturn((throwable) -> {
-                Log.e("GCParser.deleteModifiedCoordinates - cannot delete modified coords", throwable);
-                return false;
-            });
+        return Network.completeWithSuccess(response)
+                .toSingle(() -> {
+                    Log.i("GCParser.editModifiedCoordinates - edited on GC.com");
+                    return true;
+                })
+                .onErrorReturn((throwable) -> {
+                    Log.e("GCParser.deleteModifiedCoordinates - cannot delete modified coords", throwable);
+                    return false;
+                });
     }
 
     @NonNull
